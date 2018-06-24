b07880b33458058db8c958bd1be007ad7dcd6cb9
liteng <930372551@qq.com>
2018-06-24 09:09:07 +0800

    场景编辑区继续优化。

--- src/editor/ViewportInfo.js
+++ src/editor/ViewportInfo.js
@@ -5,39 +5,49 @@
  * @author mrdoob / http://mrdoob.com/
  */
 function ViewportInfo(app, container) {
-    var container = new UI2.Div({
-        parent: container.dom,
+    this.parent = container;
+
+    this.container = new UI2.Div({
+        parent: this.parent.dom,
         style: 'position: absolute; left: 10px; bottom: 10px; font-size: 12px; color: #fff;'
     });
 
     // 物体数
-    container.add(new UI2.Text({ text: '物体' }));
-    container.add(new UI2.Text({
+    this.objectsLabel = new UI2.Text({ text: '物体' });
+    this.objectsText = new UI2.Text({
         id: 'objectsText',
         text: '0',
         style: 'margin-left: 6px'
-    }));
-    container.add(new UI2.Break());
+    });
+
+    this.container.add(this.objectsLabel);
+    this.container.add(this.objectsText);
+    this.container.add(new UI2.Break());
 
     // 顶点数
-    container.add(new UI2.Text({ text: '顶点' }));
-    container.add(new UI2.Text({
+    this.verticesLabel = new UI2.Text({ text: '顶点' });
+    this.verticesText = new UI2.Text({
         id: 'verticesText',
         text: '0',
         style: 'margin-left: 6px'
-    }));
-    container.add(new UI2.Break());
+    });
+
+    this.container.add(this.verticesLabel);
+    this.container.add(this.verticesText);
+    this.container.add(new UI2.Break());
 
     // 三角形数
-    container.add(new UI2.Text({ text: '三角形' }));
-    container.add(new UI2.Text({
+    this.trianglesLabel = new UI2.Text({ text: '三角形' });
+    this.trianglesText = new UI2.Text({
         id: 'trianglesText',
         text: '0',
         style: 'margin-left: 6px'
-    }));
-    container.add(new UI2.Break());
+    });
+    this.container.add(this.trianglesLabel);
+    this.container.add(this.trianglesText);
+    this.container.add(new UI2.Break());
 
-    container.render();
+    this.container.render();
 };
 
 export default ViewportInfo;
