60049e2146a721093c70698fc277839bb6f6b6dc
liteng <930372551@qq.com>
2018-07-05 07:54:02 +0800

    模态框。

--- src/ui/Modal.js
+++ src/ui/Modal.js
@@ -8,6 +8,11 @@ import XType from './XType';
  */
 function Modal(options) {
     Container.call(this, options);
+    options = options || {};
+
+    this.id = options.id || null;
+    this.width = options.width || 500;
+    this.height = options.height || 300;
 };
 
 Modal.prototype = Object.create(Container.prototype);
@@ -15,6 +20,10 @@ Modal.prototype.constructor = Modal;
 
 Modal.prototype.render = function () {
     this.dom = document.createElement('div');
+
+    if (this.id) {
+        this.dom.id = this.id;
+    }
 
     this.dom.style = 'position: absolute; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); '
         + 'display: none; align-items: center; justify-content: center;';
@@ -24,17 +33,17 @@ Modal.prototype.render = function () {
     this.dom.addEventListener('click', this.hide.bind(this));
 
     this.container = document.createElement('div');
-    this.container.dom.style.width = '200px';
-    this.container.dom.style.padding = '20px';
+    this.container.dom.style.width = this.width + 'px';
+    this.container.dom.style.height = this.height + 'px';
+    this.container.dom.style.padding = '8px';
     this.container.dom.style.backgroundColor = '#ffffff';
     this.container.dom.style.boxShadow = '0px 5px 10px rgba(0,0,0,0.5)';
 
     this.dom.appendChild(this.container);
 };
 
-Modal.prototype.show = function (content) {
-    this.container.innerHTML = content;
-    this.dom.style.display = 'flex';
+Modal.prototype.show = function () {
+    this.dom.style.display = 'block';
     return this;
 };
 
