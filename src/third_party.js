6863077dbcc924ad6086a7e3aa1aff4982266bf0
liteng <930372551@qq.com>
2018-07-08 17:47:55 +0800

    1、将所有Three.js第三方模块打包到ShadowEditor.js中。 2、将网页通过style和script引用的第三方文件统一复制到assets文件夹中。

--- src/third_party.js
+++ src/third_party.js
@@ -23,6 +23,16 @@ import '../third_party/loaders/TGALoader.js';
 import '../third_party/loaders/UTF8Loader.js';
 import '../third_party/loaders/VRMLLoader.js';
 import '../third_party/loaders/VTKLoader.js';
+import '../third_party/loaders/ctm/CTMLoader.js';
+import '../third_party/exporters/OBJExporter.js';
+import '../third_party/exporters/STLExporter.js';
+import '../third_party/loaders/deprecated/SceneLoader.js';
+
+import '../third_party/renderers/Projector.js';
+import '../third_party/renderers/CanvasRenderer.js';
+import '../third_party/renderers/RaytracingRenderer.js';
+import '../third_party/renderers/SoftwareRenderer.js';
+import '../third_party/renderers/SVGRenderer.js';
 
 import '../third_party/effects/VREffect.js';
 import '../third_party/controls/VRControls.js';
