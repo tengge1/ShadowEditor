76004a813e7f354ca44878a9264998c8dfda41db
liteng <930372551@qq.com>
2018-07-18 12:12:39 +0800

    CircleGeometry。

--- src/editor/sidebar/geometry/CircleGeometryPanel.js
+++ src/editor/sidebar/geometry/CircleGeometryPanel.js
@@ -1,88 +1,31 @@
-﻿import SetGeometryCommand from '../../../command/SetGeometryCommand';
-import UI from '../../../ui/UI';
+﻿import Control from '../../../ui/Control';
+import XType from '../../../ui/XType';
+import SetGeometryCommand from '../../../command/SetGeometryCommand';
 
 /**
  * 圆形几何体面板
  * @author mrdoob / http://mrdoob.com/
  */
-function CircleGeometryPanel(editor, object) {
-    var container = new UI.Row();
+function CircleGeometryPanel(options) {
+    Control.call(this, options);
+    this.app = options.app;
+    this.object = options.object;
+};
 
+CircleGeometryPanel.prototype = Object.create(Control.prototype);
+CircleGeometryPanel.prototype.constructor = CircleGeometryPanel;
+
+CircleGeometryPanel.prototype.render = function () {
+    var editor = this.app.editor;
+    var object = this.object;
     var geometry = object.geometry;
     var parameters = geometry.parameters;
 
-    // radius
-
-    var radiusRow = new UI.Row();
-
-    var radius = new UI.Number({
-        value: parameters.radius,
-        onChange: update
-    });
-
-    radiusRow.add(new UI.Label({
-        text: '半径'
-    }));
-
-    radiusRow.add(radius);
-
-    container.add(radiusRow);
-
-    // segments
-
-    var segmentsRow = new UI.Row();
-
-    var segments = new UI.Integer({
-        value: parameters.segments,
-        range: [3, Infinity],
-        onChange: update
-    });
-
-    segmentsRow.add(new UI.Label({
-        text: '段数'
-    }));
-
-    segmentsRow.add(segments);
-
-    container.add(segmentsRow);
-
-    // thetaStart
-
-    var thetaStartRow = new UI.Row();
-
-    var thetaStart = new UI.Number({
-        value: parameters.thetaStart,
-        onChange: update
-    });
-
-    thetaStartRow.add(new UI.Label({
-        text: '开始弧度'
-    }));
-
-    thetaStartRow.add(thetaStart);
-
-    container.add(thetaStartRow);
-
-    // thetaLength
-
-    var thetaLengthRow = new UI.Row();
-    var thetaLength = new UI.Number({
-        value: parameters.thetaLength,
-        onChange: update,
-        value: Math.PI * 2
-    });
-
-    thetaLengthRow.add(new UI.Label({
-        text: '结束弧度'
-    }));
-
-    thetaLengthRow.add(thetaLength);
-
-    container.add(thetaLengthRow);
-
-    //
-
     function update() {
+        var radius = XType.getControl('circleGeometryRadius');
+        var segments = XType.getControl('circleGeometrySegments');
+        var thetaStart = XType.getControl('circleGeometryThetaStart');
+        var thetaLength = XType.getControl('circleGeometryThetaLength');
 
         editor.execute(new SetGeometryCommand(object, new THREE[geometry.type](
             radius.getValue(),
@@ -93,9 +36,58 @@ function CircleGeometryPanel(editor, object) {
 
     }
 
-    container.render();
+    var container = XType.create({
+        xtype: 'row',
+        children: [{ // radius
+            xtype: 'row',
+            children: [{
+                xtype: 'label',
+                text: '半径'
+            }, {
+                xtype: 'number',
+                id: 'circleGeometryRadius',
+                value: parameters.radius,
+                onChange: update
+            }]
+        }, { // segments
+            xtype: 'row',
+            children: [{
+                xtype: 'label',
+                text: '段数'
+            }, {
+                xtype: 'int',
+                id: 'circleGeometrySegments',
+                value: parameters.segments,
+                range: [3, Infinity],
+                onChange: update
+            }]
+        }, { // thetaStart
+            xtype: 'row',
+            children: [{
+                xtype: 'label',
+                text: '开始弧度'
+            }, {
+                xtype: 'number',
+                id: 'circleGeometryThetaStart',
+                value: parameters.thetaStart,
+                onChange: update
+            }]
+        }, { // thetaLength
+            xtype: 'row',
+            children: [{
+                xtype: 'label',
+                text: '结束弧度'
+            }, {
+                xtype: 'number',
+                id: 'circleGeometryThetaLength',
+                value: parameters.thetaLength,
+                onChange: update,
+                value: Math.PI * 2
+            }]
+        }]
+    });
 
-    return container;
+    container.render();
 };
 
 export default CircleGeometryPanel;
