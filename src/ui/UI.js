5c7dd043a07e8aafb6838c5478294ce5dc9ee8b5
liteng <930372551@qq.com>
2018-07-19 21:32:22 +0800

    UI类。

--- src/ui/UI.js
+++ src/ui/UI.js
@@ -1,62 +1,132 @@
-import XType from './XType';
 import Control from './Control';
-import Html from './Html';
-import Container from './Container';
-
-import Div from './Div';
-import Break from './Break';
-import Button from './Button';
-import IconButton from './IconButton';
-import Boolean from './Boolean';
-import Checkbox from './Checkbox';
-import CloseButton from './CloseButton';
-import Color from './Color';
-import HorizontalRule from './HorizontalRule';
-import Input from './Input';
-import Integer from './Integer';
-import Number from './Number';
-import Row from './Row';
-import Select from './Select';
-import Span from './Span';
-import Text from './Text';
-import TextArea from './TextArea';
-import Label from './Label';
-
-import Modal from './Modal';
-import Texture from './Texture';
-import Outliner from './Outliner';
 
 /**
- * 所有UI控件封装
+ * UI类
  */
-const UI = {
-    XType: XType,
-    Control: Control,
-    Html: Html,
-    Container: Container,
+function UICls() {
+    this.xtypes = {};
+    this.objects = {};
+}
 
-    Div: Div,
-    Break: Break,
-    Button: Button,
-    IconButton: IconButton,
-    Boolean: Boolean,
-    Checkbox: Checkbox,
-    CloseButton: CloseButton,
-    Color: Color,
-    HorizontalRule: HorizontalRule,
-    Input: Input,
-    Integer: Integer,
-    Number: Number,
-    Row: Row,
-    Select: Select,
-    Span: Span,
-    Text: Text,
-    TextArea: TextArea,
-    Label: Label,
-
-    Texture: Texture,
-    Modal: Modal,
-    Outliner: Outliner
+/**
+ * 添加xtype
+ * @param {*} name xtype字符串
+ * @param {*} cls xtype对应类
+ */
+UICls.prototype.addXType = function (name, cls) {
+    if (this.xtypes[name] === undefined) {
+        this.xtypes[name] = cls;
+    } else {
+        console.warn(`UICls: xtype named ${name} has already been added.`);
+    }
 };
+
+/**
+ * 删除xtype
+ * @param {*} name xtype字符串
+ */
+UICls.prototype.removeXType = function (name) {
+    if (this.xtypes[name] !== undefined) {
+        delete this.xtypes[name];
+    } else {
+        console.warn(`UICls: xtype named ${name} is not defined.`);
+    }
+};
+
+/**
+ * 获取xtype
+ * @param {*} name xtype字符串
+ */
+UICls.prototype.getXType = function (name) {
+    if (this.xtypes[name] === undefined) {
+        console.warn(`UICls: xtype named ${name} is not defined.`);
+    }
+    return this.xtypes[name];
+};
+
+/**
+ * 添加一个对象到缓存
+ * @param {*} id 对象id
+ * @param {*} obj 对象
+ * @param {*} scope 对象作用域（默认为global）
+ */
+UICls.prototype.add = function (id, obj, scope = "global") {
+    var key = `${scope}:${id}`;
+    if (this.objects[key] !== undefined) {
+        console.warn(`UICls: object named ${id} has already been added.`);
+    }
+    this.objects[key] = obj;
+};
+
+/**
+ * 从缓存中移除一个对象
+ * @param {*} id 对象id
+ * @param {*} scope 对象作用域（默认为global）
+ */
+UICls.prototype.remove = function (id, scope = 'global') {
+    var key = `${scope}:${id}`;
+    if (this.objects[key] === undefined) {
+        console.warn(`UICls: object named ${id} is not defined.`);
+    } else {
+        delete this.objects[key];
+    }
+};
+
+/**
+ * 从缓存中获取一个对象
+ * @param {*} id 控件id
+ * @param {*} scope 对象作用域（默认为global）
+ */
+UICls.prototype.get = function (id, scope = 'global') {
+    var key = `${scope}:${id}`;
+    if (this.objects[key] === undefined) {
+        console.warn(`UICls: object named ${id} is not defined.`);
+    }
+    return this.objects[key];
+};
+
+/**
+ * 通过json配置创建UI实例，并自动将包含id的控件添加到缓存
+ * @param {*} config xtype配置
+ * @param {*} scope 对象作用域（默认为global）
+ */
+UICls.prototype.create = function (config, scope = 'global') {
+    if (config instanceof Control) { // config是Control实例
+        if (config.id) {
+            scope = config.scope || scope;
+            this.objects[`${scope}:${config.id}`] = config;
+        }
+        return config;
+    }
+
+    // config是json配置
+    if (config == null || config.xtype == null) {
+        throw 'UICls: config is undefined.';
+    }
+
+    if (config.xtype === undefined) {
+        throw 'UICls: config.xtype is undefined.';
+    }
+
+    var cls = this.xtypes[config.xtype];
+    if (cls == null) {
+        throw `UICls: xtype named ${config.xtype} is undefined.`;
+    }
+
+    var control = new cls(config);
+    scope = config.scope || scope;
+    if (config.id && this.objects[`${scope}:${config.id}`] !== undefined) {
+        console.warn(`UICls: control named ${control.id} has already be added.`);
+    } else if (control.id) {
+        this.objects[`${scope}:${config.id}`] = control;
+    }
+
+    return control;
+};
+
+/**
+ * UICls唯一一个实例
+ */
+const UI = new UICls();
 
 export default UI;
