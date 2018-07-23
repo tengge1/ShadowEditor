09d94ef31da1765fb1f30564942f0b5c69d67d37
liteng <930372551@qq.com>
2018-07-23 12:13:53 +0800

    svg关闭按钮更换为iconfont。

--- src/ui/CloseButton.js
+++ src/ui/CloseButton.js
@@ -17,14 +17,18 @@ CloseButton.prototype = Object.create(Control.prototype);
 CloseButton.prototype.constructor = CloseButton;
 
 CloseButton.prototype.render = function () {
-    this.dom = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    this.dom.setAttribute('width', 32);
-    this.dom.setAttribute('height', 32);
+    this.dom = document.createElement('div');
 
     // TODO: 由于按钮默认白色，在白色背景上按钮将不可见！
     if (this.style) {
         this.dom.style = this.style;
     }
+
+    this.dom.style.width = '32px';
+    this.dom.style.height = '32px';
+    this.dom.style.display = 'flex';
+    this.dom.style.alignItems = 'center';
+    this.dom.style.justifyContent = 'center';
 
     this.parent.appendChild(this.dom);
 
@@ -32,11 +36,12 @@ CloseButton.prototype.render = function () {
         this.dom.addEventListener('click', this.onClick.bind(this));
     }
 
-    this.path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    this.path.setAttribute('d', 'M 12,12 L 22,22 M 22,12 12,22');
-    this.path.setAttribute('stroke', '#fff');
+    this.icon = document.createElement('i');
+    this.icon.className = 'iconfont icon-close';
+    this.icon.style.fontSize = '24px';
+    this.icon.style.color = '#fff';
 
-    this.dom.appendChild(this.path);
+    this.dom.appendChild(this.icon);
 };
 
 export default CloseButton;
