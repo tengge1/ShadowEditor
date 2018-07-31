97fa1c948820d0ef7300cd2ab89722aa3f94d736
liteng <930372551@qq.com>
2018-07-31 12:32:40 +0800

    添加文字。

--- src/event/menu/add/AddTextEvent.js
+++ src/event/menu/add/AddTextEvent.js
@@ -1,7 +1,8 @@
 import MenuEvent from '../MenuEvent';
 import AddObjectCommand from '../../../command/AddObjectCommand';
+import UI from '../../../ui/UI';
 
-var ID = 1;
+var ID = -1;
 
 /**
  * 添加文本事件
@@ -23,7 +24,37 @@ AddTextEvent.prototype.stop = function () {
 };
 
 AddTextEvent.prototype.onAddText = function () {
+    UI.prompt('请输入', null, '一些文字', (event, value) => {
+        this.drawText(value);
+    });
+};
 
+AddTextEvent.prototype.drawText = function (text) {
+    var canvas = document.createElement('canvas');
+
+    var ctx = canvas.getContext('2d');
+    ctx.font = '24px 微软雅黑';
+    ctx.textBaseline = 'top';
+
+    var padding = 8;
+    var textMetrics = ctx.measureText(text);
+    canvas.width = (textMetrics.width + padding * 2);
+    canvas.height = (24 + padding * 2);
+
+    ctx.fillStyle = 'rgba(0,0,0,0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(255,255,255,1)';
+    ctx.fillText(text, padding, padding);
+
+    var geometry = new THREE.PlaneBufferGeometry(canvas.width / 10, canvas.height / 10);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        map: new THREE.CanvasTexture(canvas)
+    });
+
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.name = text;
+    this.app.editor.scene.add(mesh);
 };
 
 export default AddTextEvent;
