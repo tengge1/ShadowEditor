82192219f28096d8367ece4828304a58c83b70b0
liteng <930372551@qq.com>
2018-08-06 12:12:15 +0800

    BufferGeometrySerializer

--- ShadowEditor.Web/src/serialization/geometry/BufferGeometrySerializer.js
+++ ShadowEditor.Web/src/serialization/geometry/BufferGeometrySerializer.js
@@ -13,37 +13,41 @@ BufferGeometrySerializer.prototype.constructor = BufferGeometrySerializer;
 BufferGeometrySerializer.prototype.toJSON = function (obj) {
     var json = BaseSerializer.prototype.toJSON.call(this, obj);
 
+    json.attributes = obj.attributes;
     json.boundingBox = obj.boundingBox;
     json.boundingSphere = obj.boundingSphere;
-    json.colors = obj.colors;
-    json.faces = obj.faces;
-    json.faceVertexUvs = obj.faceVertexUvs;
-    json.id = obj.id;
-    json.isGeometry = obj.isGeometry;
-    json.lineDistances = obj.lineDistances;
-    json.morphTargets = obj.morphTargets;
-    json.morphNormals = obj.morphNormals;
-    json.name = geometry.name;
-    json.skinWeights = obj.skinWeights;
-    json.skinIndices = obj.skinIndices;
+    json.drawRange = obj.drawRange;
+    json.groups = obj.groups;
+    json.index = obj.index;
+    json.morphAttributes = obj.morphAttributes;
+    json.name = obj.name;
+    json.parameters = obj.parameters;
+    json.type = obj.type;
+    json.userData = obj.userData;
     json.uuid = obj.uuid;
-    json.vertices = obj.vertices;
-    json.verticesNeedUpdate = obj.verticesNeedUpdate;
-    json.elementsNeedUpdate = obj.elementsNeedUpdate;
-    json.uvsNeedUpdate = obj.uvsNeedUpdate;
-    json.normalsNeedUpdate = obj.normalsNeedUpdate;
-    json.colorsNeedUpdate = obj.colorsNeedUpdate;
-    json.groupsNeedUpdate = obj.groupsNeedUpdate;
-    json.lineDistancesNeedUpdate = obj.lineDistancesNeedUpdate;
-
-    json.type = geometry.type;
-    json.userData = geometry.userData;
 
     return json;
 };
 
 BufferGeometrySerializer.prototype.fromJSON = function (json, parent) {
+    var obj = parent === undefined ? new THREE.BufferGeometry() : parent;
 
+    BaseSerializer.prototype.fromJSON.call(this, obj);
+
+    obj.attributes = json.attributes;
+    obj.boundingBox = json.boundingBox;
+    obj.boundingSphere = json.boundingSphere;
+    obj.drawRange = json.drawRange;
+    obj.groups = json.groups;
+    obj.index = json.index;
+    obj.morphAttributes = json.morphAttributes;
+    obj.name = json.name;
+    obj.parameters = json.parameters;
+    obj.type = json.type;
+    obj.userData = json.userData;
+    obj.uuid = json.uuid;
+
+    return obj;
 };
 
 export default BufferGeometrySerializer;
