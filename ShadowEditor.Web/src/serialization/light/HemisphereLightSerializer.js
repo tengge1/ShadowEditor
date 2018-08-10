0a3cb54a28fb7ddf693e962bcd98081dcac82126
liteng <930372551@qq.com>
2018-08-10 21:35:51 +0800

    修复半球光bug。

--- ShadowEditor.Web/src/serialization/light/HemisphereLightSerializer.js
+++ ShadowEditor.Web/src/serialization/light/HemisphereLightSerializer.js
@@ -2,7 +2,7 @@ import BaseSerializer from '../BaseSerializer';
 import LightSerializer from './LightSerializer';
 
 /**
- * HemisphereLight序列化器
+ * HemisphereLightSerializer
  */
 function HemisphereLightSerializer() {
     BaseSerializer.call(this);
@@ -14,14 +14,21 @@ HemisphereLightSerializer.prototype.constructor = HemisphereLightSerializer;
 HemisphereLightSerializer.prototype.toJSON = function (obj) {
     var json = LightSerializer.prototype.toJSON.call(this, obj);
 
+    json.isHemisphereLight = obj.isHemisphereLight;
     json.skyColor = obj.skyColor;
     json.groundColor = obj.groundColor;
 
     return json;
 };
 
-HemisphereLightSerializer.prototype.fromJSON = function (json) {
+HemisphereLightSerializer.prototype.fromJSON = function (json, parent) {
+    var obj = parent === undefined ? new THREE.HemisphereLight(json.skyColor, json.groundColor, json.intensity) : parent;
 
+    LightSerializer.prototype.fromJSON.call(this, json, obj);
+
+    obj.isHemisphereLight = json.isHemisphereLight;
+
+    return obj;
 };
 
 export default HemisphereLightSerializer;
