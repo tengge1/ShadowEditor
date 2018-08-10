5d755af74d78e4a8442a000c01a5d71cdbf35af1
liteng <930372551@qq.com>
2018-08-10 12:31:29 +0800

    聚光灯

--- ShadowEditor.Web/src/serialization/light/SpotLightSerializer.js
+++ ShadowEditor.Web/src/serialization/light/SpotLightSerializer.js
@@ -14,6 +14,7 @@ SpotLightSerializer.prototype.constructor = SpotLightSerializer;
 SpotLightSerializer.prototype.toJSON = function (obj) {
     var json = LightSerializer.prototype.toJSON.call(this, obj);
 
+    json.isSpotLight = obj.isSpotLight;
     json.distance = obj.distance;
     json.angle = obj.angle;
     json.penumbra = obj.penumbra;
@@ -22,8 +23,24 @@ SpotLightSerializer.prototype.toJSON = function (obj) {
     return json;
 };
 
-SpotLightSerializer.prototype.fromJSON = function (json) {
+SpotLightSerializer.prototype.fromJSON = function (json, parent) {
+    var obj = parent === undefined ? new THREE.SpotLight(
+        json.color,
+        json.intensity,
+        json.distance,
+        json.angle,
+        json.penumbra,
+        json.decay) : parent;
 
+    LightSerializer.prototype.fromJSON.call(this, json, obj);
+
+    obj.isSpotLight = json.isSpotLight;
+    obj.distance = json.distance;
+    obj.angle = json.angle;
+    obj.penumbra = json.penumbra;
+    obj.decay = json.decay;
+
+    return obj;
 };
 
 export default SpotLightSerializer;
