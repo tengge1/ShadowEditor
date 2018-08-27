3e1c19b89d4749dc11f46f88d743d2b3af68765e
liteng <930372551@qq.com>
2018-08-27 21:27:49 +0800

    1、帮助器状态保存场景。 2、修复纯色背景场景载入报错。

--- ShadowEditor.Web/src/serialization/core/SceneSerializer.js
+++ ShadowEditor.Web/src/serialization/core/SceneSerializer.js
@@ -36,8 +36,8 @@ SceneSerializer.prototype.fromJSON = function (json, parent) {
     Object3DSerializer.prototype.fromJSON(json, obj);
 
     if (json.background.metadata &&
-        json.background.metadata.generator === 'CubeTextureSerializer' ||
-        json.background.metadata.generator === 'TextureSerializer'
+        (json.background.metadata.generator === 'CubeTextureSerializer' ||
+            json.background.metadata.generator === 'TextureSerializer')
     ) { // 天空盒和背景图片
         obj.background = new TexturesSerializer(this.app).fromJSON(json.background);
     } else { // 纯色
