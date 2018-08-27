88a90d6fded14efcaf3ad147dc6eb26036aa4ecd
liteng <930372551@qq.com>
2018-08-27 21:34:28 +0800

    不再支持配置帮助器显示隐藏状态。

--- ShadowEditor.Web/src/Options.js
+++ ShadowEditor.Web/src/Options.js
@@ -13,13 +13,13 @@ function Options(options) {
     this.theme = options.theme || 'assets/css/light.css'; // 皮肤
 
     // 帮助器配置
-    this.showGrid = options.showGrid === undefined ? true : options.showGrid; // 是否显示网格
-    this.showCameraHelper = options.showCameraHelper === undefined ? true : options.showCameraHelper; // 是否显示相机帮助器
-    this.showPointLightHelper = options.showPointLightHelper === undefined ? true : options.showPointLightHelper; // 是否显示点光源帮助器
-    this.showDirectionalLightHelper = options.showDirectionalLightHelper === undefined ? true : options.showDirectionalLightHelper; // 是否显示平行光帮助器
-    this.showSpotLightHelper = options.showSpotLightHelper === undefined ? true : options.showSpotLightHelper; // 是否显示聚光灯帮助器
-    this.showHemisphereLightHelper = options.showHemisphereLightHelper === undefined ? true : options.showHemisphereLightHelper; // 是否显示半球光帮助器
-    this.showSkeletonHelper = options.showSkeletonHelper === undefined ? false : options.showSkeletonHelper; // 是否显示骨骼帮助器
+    this.showGrid = true; // 是否显示网格
+    this.showCameraHelper = true; // 是否显示相机帮助器
+    this.showPointLightHelper = true; // 是否显示点光源帮助器
+    this.showDirectionalLightHelper = true; // 是否显示平行光帮助器
+    this.showSpotLightHelper = true; // 是否显示聚光灯帮助器
+    this.showHemisphereLightHelper = true; // 是否显示半球光帮助器
+    this.showSkeletonHelper = false; // 是否显示骨骼帮助器
 }
 
 export default Options;
