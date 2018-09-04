6406411cf974b2c0ca4824f478eda51f26b3dfd8
liteng <930372551@qq.com>
2018-09-04 12:20:40 +0800

    删除多余引入。

--- ShadowEditor.Web/src/third_party.js
+++ ShadowEditor.Web/src/third_party.js
@@ -1,8 +1,4 @@
 import { } from './polyfills';
-import '../third_party/controls/EditorControls.js';
-import '../third_party/controls/TransformControls.js';
-
-import '../third_party/geometries/TeapotBufferGeometry.js';
 
 import '../third_party/exporters/GLTFExporter.js';
 import '../third_party/exporters/OBJExporter.js';
