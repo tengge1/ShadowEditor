5d503e3859b5367e2d29a28d52053ede322cdee6
liteng <930372551@qq.com>
2018-09-04 12:31:03 +0800

    mmd动画插件改为从js引入。

--- ShadowEditor.Web/src/third_party.js
+++ ShadowEditor.Web/src/third_party.js
@@ -1,6 +1,3 @@
-import { } from './polyfills';
-
-import '../third_party/animation/CCDIKSolver.js';
-import '../third_party/animation/MMDPhysics.js';
+import './polyfills';
 
 export { dispatch } from 'd3-dispatch';
