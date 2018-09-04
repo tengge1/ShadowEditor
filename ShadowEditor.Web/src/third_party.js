1ec83525c77fbea89e5922319f447b4274bf5127
liteng <930372551@qq.com>
2018-09-04 12:24:01 +0800

    导出器改为通过js引入。

--- ShadowEditor.Web/src/third_party.js
+++ ShadowEditor.Web/src/third_party.js
@@ -1,11 +1,5 @@
 import { } from './polyfills';
 
-import '../third_party/exporters/GLTFExporter.js';
-import '../third_party/exporters/OBJExporter.js';
-import '../third_party/exporters/PLYExporter.js';
-import '../third_party/exporters/STLBinaryExporter.js';
-import '../third_party/exporters/STLExporter.js';
-
 import '../third_party/objects/Reflector.js';
 
 import '../third_party/animation/CCDIKSolver.js';
