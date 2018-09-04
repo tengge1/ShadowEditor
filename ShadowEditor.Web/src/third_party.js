6b35d4ec090399c10d8d0ea789e3be4e5893cf7d
liteng <930372551@qq.com>
2018-09-04 12:15:08 +0800

    所有加载器改为通过js引用。

--- ShadowEditor.Web/src/third_party.js
+++ ShadowEditor.Web/src/third_party.js
@@ -4,23 +4,6 @@ import '../third_party/controls/TransformControls.js';
 
 import '../third_party/geometries/TeapotBufferGeometry.js';
 
-import '../third_party/loaders/AMFLoader.js';
-import '../third_party/loaders/AWDLoader.js';
-import '../third_party/loaders/BabylonLoader.js';
-import '../third_party/loaders/BinaryLoader.js';
-import '../third_party/loaders/ColladaLoader2.js';
-import '../third_party/loaders/FBXLoader.js';
-import '../third_party/loaders/GLTFLoader.js';
-import '../third_party/loaders/KMZLoader.js';
-import '../third_party/loaders/MD2Loader.js';
-import '../third_party/loaders/OBJLoader.js';
-import '../third_party/loaders/PLYLoader.js';
-import '../third_party/loaders/STLLoader.js';
-import '../third_party/loaders/TGALoader.js';
-import '../third_party/loaders/VTKLoader.js';
-import '../third_party/loaders/ctm/CTMLoader.js';
-import '../third_party/loaders/MMDLoader.js';
-
 import '../third_party/exporters/GLTFExporter.js';
 import '../third_party/exporters/OBJExporter.js';
 import '../third_party/exporters/PLYExporter.js';
