ec5aff14908c192e8064120b699656996a861baa
liteng <930372551@qq.com>
2018-09-05 12:09:42 +0800

    底部面板。

--- ShadowEditor.Web/src/editor/bottom/BottomPanel.js
+++ ShadowEditor.Web/src/editor/bottom/BottomPanel.js
@@ -1,5 +1,4 @@
 import Control from '../../ui/Control';
-import TimePanel from './TimePanel';
 
 /**
  * 底部面板
@@ -19,15 +18,70 @@ BottomPanel.prototype.render = function () {
         xtype: 'div',
         cls: 'sidebar bottomPanel',
         parent: this.app.container,
-        children: [
-            new TimePanel({
-                app: this.app
-            })
-        ]
+        children: [{
+            xtype: 'div',
+            cls: 'tabs',
+            children: [{
+                xtype: 'text',
+                id: 'animationTab',
+                text: '动画',
+                onClick: () => {
+                    this.selectTab('animation');
+                }
+            }, {
+                xtype: 'text',
+                id: 'logTab',
+                text: '日志',
+                onClick: () => {
+                    this.selectTab('log');
+                }
+            }]
+        }, {
+            xtype: 'div',
+            id: 'animationPanel',
+            children: [
+
+            ]
+        }, {
+            xtype: 'div',
+            id: 'logPanel',
+            children: [
+
+            ]
+        }]
     };
 
     var control = UI.create(data);
     control.render();
+
+    this.app.on(`appStarted.${this.id}`, () => {
+        this.selectTab('animation');
+    });
+};
+
+BottomPanel.prototype.selectTab = function (tabName) {
+    const animationTab = UI.get('animationTab');
+    const logTab = UI.get('logTab');
+
+    const animationPanel = UI.get('animationPanel');
+    const logPanel = UI.get('logPanel');
+
+    animationTab.dom.className = '';
+    logTab.dom.className = '';
+
+    animationPanel.dom.style.display = 'none';
+    logPanel.dom.style.display = 'none';
+
+    switch (tabName) {
+        case 'animation':
+            animationTab.dom.className = 'selected';
+            animationPanel.dom.style.display = '';
+            break;
+        case 'log':
+            logTab.dom.className = 'selected';
+            logPanel.dom.style.display = '';
+            break;
+    }
 };
 
 export default BottomPanel;
