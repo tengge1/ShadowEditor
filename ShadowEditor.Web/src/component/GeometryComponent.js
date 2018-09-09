8ec599931bcb73d3bdea1b73516df101a64463ce
liteng <930372551@qq.com>
2018-09-09 21:43:04 +0800

    车床几何体。

--- ShadowEditor.Web/src/component/GeometryComponent.js
+++ ShadowEditor.Web/src/component/GeometryComponent.js
@@ -7,6 +7,7 @@ import SphereGeometryComponent from './geometry/SphereGeometryComponent';
 import IcosahedronGeometryComponent from './geometry/IcosahedronGeometryComponent';
 import TorusGeometryComponent from './geometry/TorusGeometryComponent';
 import TorusKnotGeometryComponent from './geometry/TorusKnotGeometryComponent';
+import LatheGeometryComponent from './geometry/LatheGeometryComponent';
 
 /**
  * 几何体组件
@@ -59,7 +60,8 @@ GeometryComponent.prototype.render = function () {
         new SphereGeometryComponent({ app: this.app }),
         new IcosahedronGeometryComponent({ app: this.app }),
         new TorusGeometryComponent({ app: this.app }),
-        new TorusKnotGeometryComponent({ app: this.app })
+        new TorusKnotGeometryComponent({ app: this.app }),
+        new LatheGeometryComponent({ app: this.app })
         ]
     };
 
---
+++ ShadowEditor.Web/src/component/geometry/LatheGeometryComponent.js
@@ -0,0 +1,118 @@
+import BaseComponent from '../BaseComponent';
+import SetGeometryCommand from '../../command/SetGeometryCommand';
+
+/**
+ * 车床组件
+ * @author tengge / https://github.com/tengge1
+ * @param {*} options 
+ */
+function LatheGeometryComponent(options) {
+    BaseComponent.call(this, options);
+    this.selected = null;
+}
+
+LatheGeometryComponent.prototype = Object.create(BaseComponent.prototype);
+LatheGeometryComponent.prototype.constructor = LatheGeometryComponent;
+
+LatheGeometryComponent.prototype.render = function () {
+    var data = {
+        xtype: 'div',
+        parent: this.parent,
+        id: 'geometryPanel',
+        scope: this.id,
+        style: {
+            borderTop: 0,
+            marginTop: '8px',
+            display: 'none'
+        },
+        children: [{
+            xtype: 'row',
+            children: [{
+                xtype: 'label',
+                text: '径向分段'
+            }, {
+                xtype: 'int',
+                id: 'segments',
+                scope: this.id,
+                value: 16,
+                onChange: this.onChangeGeometry.bind(this)
+            }]
+        }, {
+            xtype: 'row',
+            children: [{
+                xtype: 'label',
+                text: '开始弧度'
+            }, {
+                xtype: 'number',
+                id: 'phiStart',
+                scope: this.id,
+                value: 0,
+                onChange: this.onChangeGeometry.bind(this)
+            }]
+        }, {
+            xtype: 'row',
+            children: [{
+                xtype: 'label',
+                text: '转过弧度'
+            }, {
+                xtype: 'number',
+                id: 'phiLength',
+                scope: this.id,
+                value: Math.PI * 2,
+                onChange: this.onChangeGeometry.bind(this)
+            }]
+        }]
+    };
+
+    var control = UI.create(data);
+    control.render();
+
+    this.app.on(`objectSelected.${this.id}`, this.onObjectSelected.bind(this));
+    this.app.on(`objectChanged.${this.id}`, this.onObjectChanged.bind(this));
+};
+
+LatheGeometryComponent.prototype.onObjectSelected = function () {
+    this.updateUI();
+};
+
+LatheGeometryComponent.prototype.onObjectChanged = function () {
+    this.updateUI();
+};
+
+LatheGeometryComponent.prototype.updateUI = function () {
+    var container = UI.get('geometryPanel', this.id);
+    var editor = this.app.editor;
+    if (editor.selected && editor.selected instanceof THREE.Mesh && editor.selected.geometry instanceof THREE.LatheBufferGeometry) {
+        container.dom.style.display = '';
+    } else {
+        container.dom.style.display = 'none';
+        return;
+    }
+
+    this.selected = editor.selected;
+
+    var segments = UI.get('segments', this.id);
+    var phiStart = UI.get('phiStart', this.id);
+    var phiLength = UI.get('phiLength', this.id);
+
+    segments.setValue(this.selected.geometry.parameters.segments);
+    phiStart.setValue(this.selected.geometry.parameters.phiStart);
+    phiLength.setValue(this.selected.geometry.parameters.phiLength);
+};
+
+LatheGeometryComponent.prototype.onChangeGeometry = function () {
+    var segments = UI.get('segments', this.id);
+    var phiStart = UI.get('phiStart', this.id);
+    var phiLength = UI.get('phiLength', this.id);
+
+    var points = this.selected.geometry.parameters.points;
+
+    this.app.editor.execute(new SetGeometryCommand(this.selected, new THREE.LatheBufferGeometry(
+        points,
+        segments.getValue(),
+        phiStart.getValue(),
+        phiLength.getValue()
+    )));
+};
+
+export default LatheGeometryComponent;
