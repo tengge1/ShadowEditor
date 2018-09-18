3fdde6a92375a903d8a22636ef513a5cf7404424
liteng <930372551@qq.com>
2018-09-19 07:53:08 +0800

    添加太阳。

--- ShadowEditor.Web/src/component/SceneComponent.js
+++ ShadowEditor.Web/src/component/SceneComponent.js
@@ -1,5 +1,6 @@
 import BaseComponent from './BaseComponent';
 import SetValueCommand from '../command/SetValueCommand';
+import AddObjectCommand from '../command/AddObjectCommand';
 
 /**
  * 场景组件
@@ -46,7 +47,8 @@ SceneComponent.prototype.render = function () {
                 options: {
                     'Color': '纯色',
                     'Image': '背景图片',
-                    'SkyBox': '立体贴图'
+                    'SkyBox': '立体贴图',
+                    'Sky': '天空'
                 },
                 onChange: this.onChangeBackgroundType.bind(this)
             }]
@@ -369,9 +371,54 @@ SceneComponent.prototype.onChangeBackgroundType = function () { // 切换背景�
             backgroundPosZRow.dom.style.display = '';
             backgroundNegZRow.dom.style.display = '';
             break;
+        case 'Sky':
+            backgroundColorRow.dom.style.display = 'none';
+            backgroundImageRow.dom.style.display = 'none';
+            backgroundPosXRow.dom.style.display = 'none';
+            backgroundNegXRow.dom.style.display = 'none';
+            backgroundPosYRow.dom.style.display = 'none';
+            backgroundNegYRow.dom.style.display = 'none';
+            backgroundPosZRow.dom.style.display = 'none';
+            backgroundNegZRow.dom.style.display = 'none';
+            this.showSky();
+            break;
     }
 
     this.update();
+};
+
+SceneComponent.prototype.showSky = function () {
+    var editor = this.app.editor;
+
+    var distance = 400000;
+
+    var sky = new THREE.Sky();
+    sky.scale.setScalar(450000);
+    editor.execute(new AddObjectCommand(sky));
+
+    var sunSphere = new THREE.Mesh(
+        new THREE.SphereBufferGeometry(20000, 16, 8),
+        new THREE.MeshBasicMaterial({ color: 0xffffff })
+    );
+
+    sunSphere.position.y = - 700000;
+    sunSphere.visible = false;
+
+    editor.execute(new AddObjectCommand(sunSphere));
+
+    var uniforms = sky.material.uniforms;
+    uniforms.turbidity.value = 10;
+    uniforms.rayleigh.value = 2;
+    uniforms.luminance.value = 1;
+    uniforms.mieCoefficient.value = 0.005;
+    uniforms.mieDirectionalG.value = 0.8;
+    var theta = Math.PI * (0.49 - 0.5);
+    var phi = 2 * Math.PI * (0.25 - 0.5);
+    sunSphere.position.x = distance * Math.cos(phi);
+    sunSphere.position.y = distance * Math.sin(phi) * Math.sin(theta);
+    sunSphere.position.z = distance * Math.sin(phi) * Math.cos(theta);
+    sunSphere.visible = true;
+    uniforms.sunPosition.value.copy(sunSphere.position);
 };
 
 SceneComponent.prototype.onChangeFogType = function () { // 切换雾类型
