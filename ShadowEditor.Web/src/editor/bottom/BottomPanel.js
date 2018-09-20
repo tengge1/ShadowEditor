59615b1859977eb304e8863ce261d9a8ae9fdafc
liteng <930372551@qq.com>
2018-09-20 21:40:21 +0800

    增加三个面板。

--- ShadowEditor.Web/src/editor/bottom/BottomPanel.js
+++ ShadowEditor.Web/src/editor/bottom/BottomPanel.js
@@ -1,6 +1,9 @@
 import Control from '../../ui/Control';
 import AnimationPanel from './AnimationPanel';
 import ModelPanel from './ModelPanel';
+import TexturePanel from './TexturePanel';
+import AudioPanel from './AudioPanel';
+import ParticlePanel from './ParticlePanel';
 import LogPanel from './LogPanel';
 
 /**
@@ -40,6 +43,27 @@ BottomPanel.prototype.render = function () {
                 }
             }, {
                 xtype: 'text',
+                id: 'textureTab',
+                text: '纹理',
+                onClick: () => {
+                    this.selectTab('texture');
+                }
+            }, {
+                xtype: 'text',
+                id: 'audioTab',
+                text: '音频',
+                onClick: () => {
+                    this.selectTab('audio');
+                }
+            }, {
+                xtype: 'text',
+                id: 'particleTab',
+                text: '粒子',
+                onClick: () => {
+                    this.selectTab('particle');
+                }
+            }, {
+                xtype: 'text',
                 id: 'logTab',
                 text: '日志',
                 onClick: () => {
@@ -66,6 +90,33 @@ BottomPanel.prototype.render = function () {
             ]
         }, {
             xtype: 'div',
+            id: 'texturePanel',
+            style: {
+                flex: 1
+            },
+            children: [
+                new TexturePanel({ app: this.app })
+            ]
+        }, {
+            xtype: 'div',
+            id: 'audioPanel',
+            style: {
+                flex: 1
+            },
+            children: [
+                new AudioPanel({ app: this.app })
+            ]
+        }, {
+            xtype: 'div',
+            id: 'particlePanel',
+            style: {
+                flex: 1
+            },
+            children: [
+                new ParticlePanel({ app: this.app })
+            ]
+        }, {
+            xtype: 'div',
             id: 'logPanel',
             children: [
                 new LogPanel({ app: this.app })
@@ -84,18 +135,30 @@ BottomPanel.prototype.render = function () {
 BottomPanel.prototype.selectTab = function (tabName) {
     var animationTab = UI.get('animationTab');
     var modelTab = UI.get('modelTab');
+    var textureTab = UI.get('textureTab');
+    var audioTab = UI.get('audioTab');
+    var particleTab = UI.get('particleTab');
     var logTab = UI.get('logTab');
 
     var animationPanel = UI.get('animationPanel');
     var modelPanel = UI.get('modelPanel');
+    var texturePanel = UI.get('texturePanel');
+    var audioPanel = UI.get('audioPanel');
+    var particlePanel = UI.get('particlePanel');
     var logPanel = UI.get('logPanel');
 
     animationTab.dom.className = '';
     modelTab.dom.className = '';
+    textureTab.dom.className = '';
+    audioTab.dom.className = '';
+    particleTab.dom.className = '';
     logTab.dom.className = '';
 
     animationPanel.dom.style.display = 'none';
     modelPanel.dom.style.display = 'none';
+    texturePanel.dom.style.display = 'none';
+    audioPanel.dom.style.display = 'none';
+    particlePanel.dom.style.display = 'none';
     logPanel.dom.style.display = 'none';
 
     switch (tabName) {
@@ -106,6 +169,18 @@ BottomPanel.prototype.selectTab = function (tabName) {
         case 'model':
             modelTab.dom.className = 'selected';
             modelPanel.dom.style.display = '';
+            break;
+        case 'texture':
+            textureTab.dom.className = 'selected';
+            texturePanel.dom.style.display = '';
+            break;
+        case 'audio':
+            audioTab.dom.className = 'selected';
+            audioPanel.dom.style.display = '';
+            break;
+        case 'particle':
+            particleTab.dom.className = 'selected';
+            particlePanel.dom.style.display = '';
             break;
         case 'log':
             logTab.dom.className = 'selected';
