7b9b8790758641b7fb68d9205662122f046bbd14
liteng <930372551@qq.com>
2018-09-24 10:46:24 +0800

    线性插值。

---
+++ ShadowEditor.Web/src/animation/interpolation/LinearInterpolation.js
@@ -0,0 +1,11 @@
+/**
+ * 线性插值
+ * @author TweenJS / https://www.createjs.com/tweenjs
+ * @param {*} t 时间
+ * @link https://github.com/CreateJS/TweenJS/blob/master/src/tweenjs/Ease.js
+ */
+function LinearInterpolation(t) {
+    return t;
+};
+
+export default LinearInterpolation;
