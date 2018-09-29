d1f82a1dd0334aa3e6fc9a9972297f157607bdef
liteng <930372551@qq.com>
2018-09-29 12:37:47 +0800

    几何体封装。

--- ShadowEditor.Web/src/editor/menubar/AddMenu.js
+++ ShadowEditor.Web/src/editor/menubar/AddMenu.js
@@ -1,6 +1,5 @@
 import UI from '../../ui/UI';
 import AddObjectCommand from '../../command/AddObjectCommand';
-import StringUtils from '../../utils/StringUtils';
 
 import Group from '../../object/geometry/Group';
 import Plane from '../../object/geometry/Plane';
@@ -14,6 +13,7 @@ import TorusKnot from '../../object/geometry/TorusKnot';
 import Teapot from '../../object/geometry/Teapot';
 import Lathe from '../../object/geometry/Lathe';
 import Sprite from '../../object/geometry/Sprite';
+import Text from '../../object/geometry/Text';
 
 import PointLight from '../../object/light/PointLight';
 import HemisphereLight from '../../object/light/HemisphereLight';
@@ -227,44 +227,8 @@ AddMenu.prototype.addSprite = function () {
 
 AddMenu.prototype.addText = function () {
     UI.prompt('请输入', null, '一些文字', (event, value) => {
-        this.drawText(value);
+        this.app.editor.execute(new AddObjectCommand(new Text(value)));
     });
-};
-
-AddMenu.prototype.drawText = function (text) {
-    var canvas = document.createElement('canvas');
-
-    var fontSize = 64;
-
-    var ctx = canvas.getContext('2d');
-    ctx.font = `${fontSize}px sans-serif`;
-
-    var textMetrics = ctx.measureText(text);
-    canvas.width = StringUtils.makePowOfTwo(textMetrics.width);
-    canvas.height = fontSize;
-    ctx.textBaseline = 'hanging';
-    ctx.font = `${fontSize}px sans-serif`; // 重新设置画布大小，前面设置的ctx属性全部失效
-
-    ctx.fillStyle = 'rgba(0,0,0,0)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'rgba(255,255,255,1)';
-    ctx.fillText(text, (canvas.width - textMetrics.width) / 2, 0);
-
-    var map = new THREE.CanvasTexture(canvas, );
-
-    var geometry = new THREE.PlaneBufferGeometry(canvas.width / 10, canvas.height / 10);
-    var material = new THREE.MeshBasicMaterial({
-        color: 0xffffff,
-        map: map,
-        transparent: true
-    });
-
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.name = text;
-
-    var editor = this.app.editor;
-
-    editor.execute(new AddObjectCommand(mesh));
 };
 
 // ------------------------- 环境光 ------------------------------
