df8996716a40a2152dac6de615e0a1f3a64f72ae
liteng <930372551@qq.com>
2018-11-10 22:14:05 +0800

    添加封装后的npm包，并删除多余模块。

--- ShadowEditor.SVG/src/third_party.js
+++
@@ -1,5 +0,0 @@
-import { SvgControl, Manager } from '@tengge1/xtype.js';
-
-const SVG = new Manager();
-
-export { SvgControl, SVG };
--- ShadowEditor.UI/src/third_party.js
+++ ShadowEditor.UI/src/third_party.js
@@ -1,5 +1,1 @@
-import { Control, Manager } from '@tengge1/xtype.js';
-
-const UI = new Manager();
-
-export { Control, UI };
+export { Control, UI } from '@tengge1/xtype-html5';
