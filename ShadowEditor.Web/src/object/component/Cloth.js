0e1a8d92563dc1852aa58d591d0c677ad680617c
liteng <930372551@qq.com>
2018-11-19 07:51:46 +0800

    布料。

--- ShadowEditor.Web/src/object/component/Cloth.js
+++ ShadowEditor.Web/src/object/component/Cloth.js
@@ -36,48 +36,6 @@ function Cloth() {
 
     function init() {
 
-        container = document.createElement('div');
-        document.body.appendChild(container);
-
-        // scene
-
-        scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xcce0ff);
-        scene.fog = new THREE.Fog(0xcce0ff, 500, 10000);
-
-        // camera
-
-        camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000);
-        camera.position.set(1000, 50, 1500);
-
-        // lights
-
-        var light, materials;
-
-        scene.add(new THREE.AmbientLight(0x666666));
-
-        light = new THREE.DirectionalLight(0xdfebff, 1);
-        light.position.set(50, 200, 100);
-        light.position.multiplyScalar(1.3);
-
-        light.castShadow = true;
-
-        light.shadow.mapSize.width = 1024;
-        light.shadow.mapSize.height = 1024;
-
-        var d = 300;
-
-        light.shadow.camera.left = - d;
-        light.shadow.camera.right = d;
-        light.shadow.camera.top = d;
-        light.shadow.camera.bottom = - d;
-
-        light.shadow.camera.far = 1000;
-
-        scene.add(light);
-
-        // cloth material
-
         var loader = new THREE.TextureLoader();
         var clothTexture = loader.load('textures/patterns/circuit_pattern.png');
         clothTexture.anisotropy = 16;
