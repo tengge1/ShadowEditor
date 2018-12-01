2cddb3f29d30e3e5261a5d1117439d3f8151c387
liteng <930372551@qq.com>
2018-12-01 14:44:03 +0800

    fbx动画。

--- ShadowEditor.Web/src/loader/FBXLoader.js
+++ ShadowEditor.Web/src/loader/FBXLoader.js
@@ -11,16 +11,43 @@ function FBXLoader() {
 FBXLoader.prototype = Object.create(BaseLoader.prototype);
 FBXLoader.prototype.constructor = FBXLoader;
 
-FBXLoader.prototype.load = function (url) {
+FBXLoader.prototype.load = function (url, options) {
     return new Promise(resolve => {
         var loader = new THREE.FBXLoader();
 
         loader.load(url, obj3d => {
+            Object.assign(obj3d.userData, {
+                obj: obj3d,
+                root: obj3d,
+            });
+
+            if (obj3d.animations && obj3d.animations.length > 0) {
+                Object.assign(obj3d.userData, {
+                    animNames: obj3d.animations.map(n => n.name),
+                    scripts: [{
+                        id: null,
+                        name: `${options.Name}动画`,
+                        type: 'javascript',
+                        source: this.createScripts(options.Name),
+                        uuid: THREE.Math.generateUUID()
+                    }]
+                });
+            }
+
             resolve(obj3d);
         }, undefined, () => {
             resolve(null);
         });
     });
+};
+
+FBXLoader.prototype.createScripts = function (name) {
+    return `var mesh = this.getObjectByName('${name}');\n\n` +
+        `var obj = mesh.userData.obj;\n\n` +
+        `var root = mesh.userData.root;\n\n` +
+        `var mixer = new THREE.AnimationMixer(root);\n\n` +
+        `mixer.clipAction(obj.animations[0]).play();\n\n` +
+        `function update(clock, deltaTime) { \n    mixer.update(deltaTime); \n}`;
 };
 
 export default FBXLoader;
