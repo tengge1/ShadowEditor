fa46a0b5fffaf4acb7a4bfb62f8d071f829e00a4
liteng <930372551@qq.com>
2018-12-01 10:33:51 +0800

    ColladaLoader

--- ShadowEditor.Web/src/loader/ColladaLoader.js
+++ ShadowEditor.Web/src/loader/ColladaLoader.js
@@ -11,13 +11,29 @@ function ColladaLoader() {
 ColladaLoader.prototype = Object.create(BaseLoader.prototype);
 ColladaLoader.prototype.constructor = ColladaLoader;
 
-ColladaLoader.prototype.load = function (url) {
+ColladaLoader.prototype.load = function (url, options) {
     return new Promise(resolve => {
         var loader = new THREE.ColladaLoader();
 
         loader.load(url, collada => {
-            var obj3d = collada.scene;
-            resolve(obj3d);
+            var dae = collada.scene;
+
+            dae.traverse(child => {
+                if (child instanceof THREE.Mesh) {
+                    child.material.flatShading = true;
+                }
+            });
+
+            dae.scale.x = dae.scale.y = dae.scale.z = 10.0;
+
+            dae.updateMatrix();
+
+            Object.assign(dae.userData, {
+                obj: collada,
+                root: dae
+            });
+
+            resolve(dae);
         }, undefined, () => {
             resolve(null);
         });
