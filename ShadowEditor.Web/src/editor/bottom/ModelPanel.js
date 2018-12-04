95507a9f5c8b51bddbfbcaa2243138257d1b904f
liteng <930372551@qq.com>
2018-12-05 07:42:29 +0800

    模型面板。

--- ShadowEditor.Web/src/editor/bottom/ModelPanel.js
+++ ShadowEditor.Web/src/editor/bottom/ModelPanel.js
@@ -1,4 +1,5 @@
 import UI from '../../ui/UI';
+import Ajax from '../../utils/Ajax';
 
 /**
  * 模型面板
@@ -7,24 +8,160 @@ import UI from '../../ui/UI';
 function ModelPanel(options) {
     UI.Control.call(this, options);
     this.app = options.app;
+
+    this.firstShow = true;
+
+    this.keywords = '';
+    this.data = [];
 };
 
 ModelPanel.prototype = Object.create(UI.Control.prototype);
 ModelPanel.prototype.constructor = ModelPanel;
 
 ModelPanel.prototype.render = function () {
-    var data = {
+    this.app.on(`showBottomPanel.${this.id}`, this.onShowPanel.bind(this));
+};
+
+ModelPanel.prototype.onShowPanel = function (tabName) {
+    if (tabName !== 'model') {
+        return;
+    }
+
+    if (this.firstShow) {
+        this.firstShow = false;
+        this.renderUI();
+        this.update();
+    }
+};
+
+ModelPanel.prototype.renderUI = function () {
+    var control = UI.create({
         xtype: 'div',
         parent: this.parent,
-        cls: 'Panel',
         style: {
-            position: 'relative'
+            position: 'relative',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'row',
         },
-        children: []
-    };
+        children: [{
+            xtype: 'div',
+            style: {
+                width: '200px',
+                display: 'flex',
+                flexDirection: 'column',
+                boxSizing: 'border-box',
+                borderRight: '1px solid #ddd',
+            },
+            children: [{
+                xtype: 'div',
+                children: [{
+                    xtype: 'searchfield',
+                    id: 'search',
+                    scope: this.id,
+                    showSearchButton: false,
+                    showResetButton: true,
+                    onInput: this.onSearch.bind(this)
+                }]
+            }, {
+                xtype: 'div',
+                style: {
+                    height: 'calc(100% - 30px)'
+                },
+                children: [{
+                    xtype: 'category',
+                    options: {
+                        category1: '分类1',
+                        category2: '分类2',
+                        category3: '分类3',
+                        category4: '分类4',
+                        category5: '分类5',
+                        category6: '分类6',
+                        category7: '分类7',
+                        category8: '分类8',
+                    }
+                }]
+            }]
+        }, {
+            xtype: 'div',
+            style: {
+                width: '100%',
+                height: '100%',
+                flex: 1,
+                overflow: 'auto'
+            },
+            children: [{
+                xtype: 'imagelist',
+                id: 'images',
+                scope: this.id,
+                style: {
+                    width: '100%',
+                    height: '100%',
+                },
+                onClick: this.onClick.bind(this)
+            }]
+        }]
+    });
 
-    var control = UI.create(data);
     control.render();
+};
+
+ModelPanel.prototype.update = function () {
+    var server = this.app.options.server;
+
+    this.keywords = '';
+
+    Ajax.getJson(`${server}/api/Mesh/List`, obj => {
+        this.data = obj.Data;
+        this.onSearch('');
+    });
+};
+
+ModelPanel.prototype.onSearch = function (name) {
+    if (name.trim() === '') {
+        this.renderList(this.data);
+        return;
+    }
+
+    name = name.toLowerCase();
+
+    var list = this.data.filter(n => {
+        return n.Name.indexOf(name) > -1 ||
+            n.FirstPinYin.indexOf(name) > -1 ||
+            n.TotalPinYin.indexOf(name) > -1;
+    });
+
+    this.renderList(list);
+};
+
+ModelPanel.prototype.renderList = function (list) {
+    var images = UI.get('images', this.id);
+    images.clear();
+
+    images.children = list.map(n => {
+        return {
+            xtype: 'image',
+            src: n.Thumbnail ? n.Thumbnail : null,
+            title: n.Name,
+            data: n,
+            icon: 'icon-model',
+            cornerText: n.Type,
+            style: {
+                backgroundColor: '#eee'
+            }
+        };
+    });;
+
+    images.render();
+};
+
+ModelPanel.prototype.onClick = function (data) {
+    // if (typeof (this.onSelect) === 'function') {
+    //     this.onSelect(data);
+    // } else {
+    //     UI.msg('请在材质控件中修改纹理。');
+    // }
 };
 
 export default ModelPanel;
