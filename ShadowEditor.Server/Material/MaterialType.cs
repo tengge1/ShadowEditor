4fcd224f2ca0f296ff71a9fed07e381dc5c20479
liteng <930372551@qq.com>
2018-12-05 07:52:33 +0800

    材质类型。

---
+++ ShadowEditor.Server/Material/MaterialType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowEditor.Server.Material
+{
+    /// <summary>
+    /// 材质类型
+    /// </summary>
+    public enum MaterialType
+    {
+        unknown, // 未知类型
+
+    }
+}
