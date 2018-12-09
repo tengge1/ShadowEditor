372b0badc639f6f0677ed86cf1d5cd78ed76a507
liteng <930372551@qq.com>
2018-12-09 20:22:25 +0800

    Matrix3修改完成。

--- THREE/Math/Matrix3.cs
+++ THREE/Math/Matrix3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _Math = System.Math;
 
 namespace THREE
 {
@@ -11,6 +12,7 @@ namespace THREE
     /// @author WestLangley / http://github.com/WestLangley
     /// @author bhouston / http://clara.io
     /// @author tschw
+    /// @author tengge / https://github.com/tengge1
     /// </summary>
     public class Matrix3
     {
@@ -25,11 +27,10 @@ namespace THREE
             };
         }
 
-        isMatrix3: true,
+        public const bool isMatrix3 = true;
 
-	set: function(n11, n12, n13, n21, n22, n23, n31, n32, n33 )
+        public Matrix3 Set(double n11, double n12, double n13, double n21, double n22, double n23, double n31, double n32, double n33)
         {
-
             var te = this.elements;
 
             te[0] = n11; te[1] = n21; te[2] = n31;
@@ -37,13 +38,11 @@ namespace THREE
             te[6] = n13; te[7] = n23; te[8] = n33;
 
             return this;
+        }
 
-        },
-
-	identity: function()
+        public Matrix3 Identity()
         {
-
-            this.set(
+            this.Set(
 
                 1, 0, 0,
                 0, 1, 0,
@@ -52,19 +51,15 @@ namespace THREE
             );
 
             return this;
+        }
 
-        },
-
-	clone: function()
+        public Matrix3 Clone()
         {
+            return new Matrix3().FromArray(this.elements);
+        }
 
-            return new this.constructor().fromArray(this.elements);
-
-        },
-
-	copy: function(m )
+        public Matrix3 Copy(Matrix3 m)
         {
-
             var te = this.elements;
             var me = m.elements;
 
@@ -73,81 +68,44 @@ namespace THREE
             te[6] = me[6]; te[7] = me[7]; te[8] = me[8];
 
             return this;
+        }
 
-        },
-
-	setFromMatrix4: function(m )
+        public Matrix3 SetFromMatrix4(Matrix4 m)
         {
-
             var me = m.elements;
 
-            this.set(
-
+            this.Set(
                 me[0], me[4], me[8],
                 me[1], me[5], me[9],
                 me[2], me[6], me[10]
-
             );
 
             return this;
-
-        },
-
-	applyToBufferAttribute: function()
-        {
-
-            var v1 = new Vector3();
-
-            return function applyToBufferAttribute(attribute) {
-
-                for (var i = 0, l = attribute.count; i < l; i++)
-                {
-
-                    v1.x = attribute.getX(i);
-                    v1.y = attribute.getY(i);
-                    v1.z = attribute.getZ(i);
-
-                    v1.applyMatrix3(this);
-
-                    attribute.setXYZ(i, v1.x, v1.y, v1.z);
-
-                }
-
-                return attribute;
-
-            };
-
         }
-        (),
 
-	multiply: function(m )
+        public Matrix3 Multiply(Matrix3 m)
         {
+            return this.MultiplyMatrices(this, m);
+        }
 
-            return this.multiplyMatrices(this, m);
-
-        },
-
-	premultiply: function(m )
+        public Matrix3 Premultiply(Matrix3 m)
         {
+            return this.MultiplyMatrices(m, this);
+        }
 
-            return this.multiplyMatrices(m, this);
-
-        },
-
-	multiplyMatrices: function(a, b )
+        public Matrix3 MultiplyMatrices(Matrix3 a, Matrix3 b)
         {
-
             var ae = a.elements;
             var be = b.elements;
             var te = this.elements;
 
-            var a11 = ae[0], a12 = ae[3], a13 = ae[6];
-            var a21 = ae[1], a22 = ae[4], a23 = ae[7];
-            var a31 = ae[2], a32 = ae[5], a33 = ae[8];
+            double a11 = ae[0], a12 = ae[3], a13 = ae[6];
+            double a21 = ae[1], a22 = ae[4], a23 = ae[7];
+            double a31 = ae[2], a32 = ae[5], a33 = ae[8];
 
-            var b11 = be[0], b12 = be[3], b13 = be[6];
-            var b21 = be[1], b22 = be[4], b23 = be[7];
-            var b31 = be[2], b32 = be[5], b33 = be[8];
+            double b11 = be[0], b12 = be[3], b13 = be[6];
+            double b21 = be[1], b22 = be[4], b23 = be[7];
+            double b31 = be[2], b32 = be[5], b33 = be[8];
 
             te[0] = a11 * b11 + a12 * b21 + a13 * b31;
             te[3] = a11 * b12 + a12 * b22 + a13 * b32;
@@ -162,12 +120,10 @@ namespace THREE
             te[8] = a31 * b13 + a32 * b23 + a33 * b33;
 
             return this;
+        }
 
-        },
-
-	multiplyScalar: function(s )
+        public Matrix3 MultiplyScalar(double s)
         {
-
             var te = this.elements;
 
             te[0] *= s; te[3] *= s; te[6] *= s;
@@ -175,65 +131,48 @@ namespace THREE
             te[2] *= s; te[5] *= s; te[8] *= s;
 
             return this;
+        }
 
-        },
-
-	determinant: function()
+        public double Determinant()
         {
-
             var te = this.elements;
 
-            var a = te[0], b = te[1], c = te[2],
+            double a = te[0], b = te[1], c = te[2],
                 d = te[3], e = te[4], f = te[5],
                 g = te[6], h = te[7], i = te[8];
 
             return a * e * i - a * f * h - b * d * i + b * f * g + c * d * h - c * e * g;
+        }
 
-        },
-
-	getInverse: function(matrix, throwOnDegenerate )
+        public Matrix3 GetInverse(Matrix3 matrix, bool throwOnDegenerate = false)
         {
+            double[] me = matrix.elements,
+                te = this.elements;
 
-            if (matrix && matrix.isMatrix4)
+            double n11 = me[0], n21 = me[1], n31 = me[2],
+            n12 = me[3], n22 = me[4], n32 = me[5],
+            n13 = me[6], n23 = me[7], n33 = me[8],
+
+            t11 = n33 * n22 - n32 * n23,
+            t12 = n32 * n13 - n33 * n12,
+            t13 = n23 * n12 - n22 * n13,
+
+            det = n11 * t11 + n21 * t12 + n31 * t13;
+
+            if (det == 0)
             {
-
-                console.error("THREE.Matrix3: .getInverse() no longer takes a Matrix4 argument.");
-
-            }
-
-            var me = matrix.elements,
-                te = this.elements,
-
-                n11 = me[0], n21 = me[1], n31 = me[2],
-                n12 = me[3], n22 = me[4], n32 = me[5],
-                n13 = me[6], n23 = me[7], n33 = me[8],
-
-                t11 = n33 * n22 - n32 * n23,
-                t12 = n32 * n13 - n33 * n12,
-                t13 = n23 * n12 - n22 * n13,
-
-                det = n11 * t11 + n21 * t12 + n31 * t13;
-
-            if (det === 0)
-            {
-
                 var msg = "THREE.Matrix3: .getInverse() can't invert matrix, determinant is 0";
 
-                if (throwOnDegenerate === true)
+                if (throwOnDegenerate == true)
                 {
-
-                    throw new Error(msg);
-
+                    throw new Exception(msg);
                 }
                 else
                 {
-
-                    console.warn(msg);
-
+                    Console.WriteLine(msg);
                 }
 
-                return this.identity();
-
+                return this.Identity();
             }
 
             var detInv = 1 / det;
@@ -251,32 +190,27 @@ namespace THREE
             te[8] = (n22 * n11 - n21 * n12) * detInv;
 
             return this;
+        }
 
-        },
-
-	transpose: function()
+        public Matrix3 Transpose()
         {
-
-            var tmp, m = this.elements;
+            double tmp;
+            double[] m = this.elements;
 
             tmp = m[1]; m[1] = m[3]; m[3] = tmp;
             tmp = m[2]; m[2] = m[6]; m[6] = tmp;
             tmp = m[5]; m[5] = m[7]; m[7] = tmp;
 
             return this;
+        }
 
-        },
-
-	getNormalMatrix: function(matrix4 )
+        public Matrix3 GetNormalMatrix(Matrix4 matrix4)
         {
+            return this.SetFromMatrix4(matrix4).GetInverse(this).Transpose();
+        }
 
-            return this.setFromMatrix4(matrix4).getInverse(this).transpose();
-
-        },
-
-	transposeIntoArray: function(r )
+        public Matrix3 TransposeIntoArray(double[] r)
         {
-
             var m = this.elements;
 
             r[0] = m[0];
@@ -290,45 +224,40 @@ namespace THREE
             r[8] = m[8];
 
             return this;
+        }
 
-        },
-
-	setUvTransform: function(tx, ty, sx, sy, rotation, cx, cy )
+        public void SetUvTransform(double tx, double ty, double sx, double sy, double rotation, double cx, double cy)
         {
 
-            var c = Math.cos(rotation);
-            var s = Math.sin(rotation);
+            var c = _Math.Cos(rotation);
+            var s = _Math.Sin(rotation);
 
-            this.set(
+            this.Set(
                 sx * c, sx * s, -sx * (c * cx + s * cy) + cx + tx,
                 -sy * s, sy * c, -sy * (-s * cx + c * cy) + cy + ty,
                 0, 0, 1
             );
+        }
 
-        },
-
-	scale: function(sx, sy )
+        public Matrix3 Scale(double sx, double sy)
         {
-
             var te = this.elements;
 
             te[0] *= sx; te[3] *= sx; te[6] *= sx;
             te[1] *= sy; te[4] *= sy; te[7] *= sy;
 
             return this;
+        }
 
-        },
-
-	rotate: function(theta )
+        public Matrix3 Rotate(double theta)
         {
-
-            var c = Math.cos(theta);
-            var s = Math.sin(theta);
+            var c = _Math.Cos(theta);
+            var s = _Math.Sin(theta);
 
             var te = this.elements;
 
-            var a11 = te[0], a12 = te[3], a13 = te[6];
-            var a21 = te[1], a22 = te[4], a23 = te[7];
+            double a11 = te[0], a12 = te[3], a13 = te[6];
+            double a21 = te[1], a22 = te[4], a23 = te[7];
 
             te[0] = c * a11 + s * a21;
             te[3] = c * a12 + s * a22;
@@ -339,59 +268,46 @@ namespace THREE
             te[7] = -s * a13 + c * a23;
 
             return this;
+        }
 
-        },
-
-	translate: function(tx, ty )
+        public Matrix3 Translate(double tx, double ty)
         {
-
             var te = this.elements;
 
             te[0] += tx * te[2]; te[3] += tx * te[5]; te[6] += tx * te[8];
             te[1] += ty * te[2]; te[4] += ty * te[5]; te[7] += ty * te[8];
 
             return this;
+        }
 
-        },
-
-	equals: function(matrix )
+        public bool Equals(Matrix3 matrix)
         {
-
             var te = this.elements;
             var me = matrix.elements;
 
             for (var i = 0; i < 9; i++)
             {
 
-                if (te[i] !== me[i]) return false;
+                if (te[i] != me[i]) return false;
 
             }
 
             return true;
+        }
 
-        },
-
-	fromArray: function(array, offset )
+        public Matrix3 FromArray(double[] array, int offset = 0)
         {
-
-            if (offset === undefined) offset = 0;
-
             for (var i = 0; i < 9; i++)
             {
-
                 this.elements[i] = array[i + offset];
-
             }
 
             return this;
+        }
 
-        },
-
-	toArray: function(array, offset )
+        public double[] ToArray(double[] array = null, int offset = 0)
         {
-
-            if (array === undefined) array = [];
-            if (offset === undefined) offset = 0;
+            if (array == null) array = new double[9];
 
             var te = this.elements;
 
@@ -408,7 +324,6 @@ namespace THREE
             array[offset + 8] = te[8];
 
             return array;
-
         }
     }
 }
