0ce840a550ab287a01cd3511612f9e2bcc76f2bc
liteng <930372551@qq.com>
2018-12-18 19:36:25 +0800

    粒子模型。

---
+++ ShadowEditor.Model/Particle/ParticleType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowEditor.Model.Particle
+{
+    /// <summary>
+    /// 粒子类型
+    /// </summary>
+    public enum ParticleType
+    {
+        Light,
+
+        Fire,
+
+        Water
+    }
+}
