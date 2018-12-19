180d3b2744fd845c874a179cad9fae6d2326bba2
liteng <930372551@qq.com>
2018-12-19 20:39:07 +0800

    播放器启用后期处理。

--- ShadowEditor.Web/src/player/component/PlayerRenderer.js
+++ ShadowEditor.Web/src/player/component/PlayerRenderer.js
@@ -15,10 +15,28 @@ PlayerRenderer.prototype.create = function (scene, camera, renderer) {
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
+
+    var composer = new THREE.EffectComposer(renderer);
+
+    var renderPass = new THREE.RenderPass(scene, camera);
+    renderPass.renderToScreen = true;
+    composer.addPass(renderPass);
+
+    // var effect = new THREE.ShaderPass(THREE.DotScreenShader);
+    // effect.uniforms['scale'].value = 4;
+    // effect.renderToScreen = true;
+    // composer.addPass(effect);
+    // var effect = new THREE.ShaderPass(THREE.RGBShiftShader);
+    // effect.uniforms['amount'].value = 0.0015;
+    // effect.renderToScreen = true;
+    // composer.addPass(effect);
+
+    this.composer = composer;
 };
 
 PlayerRenderer.prototype.update = function (clock, deltaTime) {
-    this.renderer.render(this.scene, this.camera);
+    this.composer.render();
+    // this.renderer.render(this.scene, this.camera);
 };
 
 PlayerRenderer.prototype.dispose = function () {
