64ef734af9eb585fde28ddb8e828b2e30d165f51
liteng <930372551@qq.com>
2018-12-20 21:01:31 +0800

    预设体模型。

---
+++ ShadowEditor.Model/Prefab/PrefabType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowEditor.Model.Prefab
+{
+    /// <summary>
+    /// 预设体类型
+    /// </summary>
+    public enum PrefabType
+    {
+        Mesh
+    }
+}
