e1194eb36bc5273e2f11fde250f282ada87eab2b
liteng <930372551@qq.com>
2019-02-01 17:14:00 +0800

    修改。

--- ShadowEditor.Web/src/helper/shader/view_fragment.glsl
+++ ShadowEditor.Web/src/helper/shader/view_fragment.glsl
@@ -1,9 +1,10 @@
 varying vec3 vNormal;
 
-const vec3 lightPosition = vec3(0.0, 0.0, 1.0);
 const vec3 lightColor = vec3(1.0, 0.0, 0.0);
+const vec3 lightPosition = vec3(0.0, 0.0, 1.0);
+const float shiness = 0.8;
 
 void main() {
-    
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    vec3 color = lightColor * pow(max(0, cameraPosition * reflect(vNormal)), shiness);
+    gl_FragColor = vec4(color, 1.0);
 }
