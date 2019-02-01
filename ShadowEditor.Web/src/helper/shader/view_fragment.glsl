f9dd9acbaf151ff7ad8dbb79947b31ca586b5808
liteng <930372551@qq.com>
2019-02-01 17:35:33 +0800

    ViewHelper光照。

--- ShadowEditor.Web/src/helper/shader/view_fragment.glsl
+++ ShadowEditor.Web/src/helper/shader/view_fragment.glsl
@@ -1,10 +1,16 @@
+varying vec3 vPosition;
 varying vec3 vNormal;
 
-const vec3 lightColor = vec3(1.0, 0.0, 0.0);
+const vec3 color = vec3(1.0, 0.0, 0.0);
 const vec3 lightPosition = vec3(0.0, 0.0, 1.0);
+const vec3 lightColor = vec3(1.0, 0.0, 0.0);
 const float shiness = 0.8;
 
 void main() {
-    vec3 color = lightColor * pow(max(0, cameraPosition * reflect(vNormal)), shiness);
-    gl_FragColor = vec4(color, 1.0);
+    vec3 lightDirection = normalize(lightPosition - vPosition);
+    float dotL = max(dot(lightDirection, vNormal), 0.0);
+    vec3 reflectDirection = reflect(-lightDirection, vNormal);
+    float specularLightWeighting = pow(max(dot(reflectDirection, cameraPosition), 0.0), shiness);
+    vec3 specular =  color.rgb * specularLightWeighting ;
+    gl_FragColor = vec4(specular, 1.0);
 }
--- ShadowEditor.Web/src/helper/shader/view_vertex.glsl
+++ ShadowEditor.Web/src/helper/shader/view_vertex.glsl
@@ -3,6 +3,7 @@ uniform float domHeight;
 uniform float width;
 uniform float height;
 
+varying vec3 vPosition;
 varying vec3 vNormal;
 
 void main() {
@@ -11,5 +12,6 @@ void main() {
 
     gl_Position = vec4(x, y, position.z, 1.0);
 
+    vPosition = vec3(x, y, 0.0);
     vNormal = normal;
 }
