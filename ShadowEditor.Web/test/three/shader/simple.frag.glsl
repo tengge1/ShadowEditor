c0af727d613cb52121a005a63ac5a13dc31557ff
liteng <930372551@qq.com>
2019-02-01 21:53:30 +0800

    简单着色器材质。

---
+++ ShadowEditor.Web/test/three/shader/simple.frag.glsl
@@ -0,0 +1,37 @@
+precision highp float;
+precision highp int;
+
+// 物体
+uniform vec3 color;
+
+// 环境光
+uniform vec3 ambientColor;
+
+// 漫反射
+uniform vec3 lightPosition;
+uniform vec3 diffuseColor;
+
+// 镜面反射
+uniform float shininess;
+
+varying vec3 vPosition;
+varying vec3 vNormal;
+
+void main() {
+    // 环境光
+    vec3 ambient = ambientColor * color;
+
+    // 漫反射
+    vec3 normal = normalize(vNormal);
+    vec3 lightDirection = normalize(lightPosition - vPosition);
+    float dotL = max(dot(lightDirection, normal), 0.0);
+    vec3 diffuse = diffuseColor * dotL * color;
+
+    // 镜面反射
+    vec3 eyeDirection = normalize(-vPosition);
+    vec3 reflectionDirection = reflect(-lightDirection, normal);
+    float specularLightWeight = pow(max(dot(reflectionDirection, eyeDirection), 0.0), shininess);
+    vec3 specular = color * specularLightWeight;
+    
+    gl_FragColor = vec4(ambient + diffuse + specular, 1.0);
+}
---
+++ ShadowEditor.Web/test/three/shader/simple_vertex.glsl
@@ -0,0 +1,20 @@
+precision highp float;
+precision highp int;
+
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec2 uv;
+
+uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+varying vec3 vPosition;
+varying vec3 vNormal;
+
+void main() {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    
+    vPosition = modelMatrix * vec4(position, 1.0);
+    vNormal = normal;
+}
