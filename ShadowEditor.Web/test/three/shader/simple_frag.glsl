b9d806c5ff9d54045c0494b71205ed6da2bcf8a3
liteng <930372551@qq.com>
2019-02-03 12:12:44 +0800

    带环境光、漫反射、镜面反射的着色器程序终于调好。

--- ShadowEditor.Web/test/three/shader/simple_frag.glsl
+++ ShadowEditor.Web/test/three/shader/simple_frag.glsl
@@ -1,40 +1,30 @@
 precision highp float;
 precision highp int;
 
-// 物体
+uniform mat4 viewMatrix;
+
 uniform vec3 color;
-
-// 环境光
 uniform vec3 ambientColor;
-
-// 漫反射
 uniform vec3 lightPosition;
 uniform vec3 diffuseColor;
-
-// 镜面反射
 uniform float shininess;
-uniform vec3 cameraPosition;
-uniform mat4 normalMatrix;
 
 varying vec3 vPosition;
 varying vec3 vNormal;
 
 void main() {
-    // 环境光
     vec3 ambient = ambientColor * color;
-
-    // 漫反射
+    
     vec3 normal = normalize(vNormal);
-    vec3 lightDirection = normalize(lightPosition - vPosition);
+    vec3 vLightPosition = vec3(viewMatrix * vec4(lightPosition, 1.0));
+    vec3 lightDirection = normalize(vLightPosition - vPosition);
     float dotL = max(dot(lightDirection, normal), 0.0);
     vec3 diffuse = diffuseColor * dotL * color;
 
-    // 镜面反射
     vec3 eyeDirection = normalize(- vPosition);
     vec3 reflectionDirection = reflect(-lightDirection, normal);
     float specularLightWeight = pow(max(dot(reflectionDirection, eyeDirection), 0.0), shininess);
     vec3 specular = color * specularLightWeight;
-    
-    gl_FragColor = vec4(diffuse, 1.0);
-    // gl_FragColor = vec4(ambient + diffuse + specular, 1.0);
+
+    gl_FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
--- ShadowEditor.Web/test/three/shader/simple_vertex.glsl
+++ ShadowEditor.Web/test/three/shader/simple_vertex.glsl
@@ -5,17 +5,18 @@ attribute vec3 position;
 attribute vec3 normal;
 attribute vec2 uv;
 
-uniform mat4 modelMatrix;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
-uniform mat4 normalMatrix;
+uniform mat3 normalMatrix;
 
 varying vec3 vPosition;
 varying vec3 vNormal;
 
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+
+    gl_Position = projectionMatrix * mvPosition;
     
-    vPosition = vec3(modelMatrix * vec4(position, 1.0));
-    vNormal = normalize(mat3(normalMatrix) * normal);
+    vPosition = vec3(mvPosition);
+    vNormal = normalize(normalMatrix * normal);
 }
