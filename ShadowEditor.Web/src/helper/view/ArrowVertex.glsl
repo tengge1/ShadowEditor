340c6f5fd999f363f7f68de6e5bc86518c465d3a
liteng <930372551@qq.com>
2019-02-05 19:26:31 +0800

    视角帮助器显示弄好了。

--- ShadowEditor.Web/src/helper/view/ArrowVertex.glsl
+++ ShadowEditor.Web/src/helper/view/ArrowVertex.glsl
@@ -22,8 +22,11 @@ void main() {
         0.0, 0.0, 1.0, 0.0,
         1.0 - size / domWidth, 1.0 - size / domHeight, 0.0, 1.0
     );
+
+    mat4 _modelViewMatrix = modelViewMatrix;
+    _modelViewMatrix[3][2] = -12.0;
     
-    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+    vec4 mvPosition = _modelViewMatrix * vec4(position, 1.0);
 
     gl_Position = translateMatrix * projectionMatrix * mvPosition;
     
