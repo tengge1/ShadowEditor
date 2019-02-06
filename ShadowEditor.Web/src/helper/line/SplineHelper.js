e7175268f28b084f0f0032cd65bcd424b11966b3
liteng <930372551@qq.com>
2019-02-06 21:06:11 +0800

    曲线帮助器。

--- ShadowEditor.Web/src/helper/line/SplineHelper.js
+++ ShadowEditor.Web/src/helper/line/SplineHelper.js
@@ -6,13 +6,30 @@ import BaseHelper from '../BaseHelper';
  */
 function SplineHelper(spline) {
     BaseHelper.call(this, spline);
+
+    this.points = [];
+
+    this.update();
 }
 
 SplineHelper.prototype = Object.create(BaseHelper.prototype);
 SplineHelper.prototype.constructor = SplineHelper;
 
 SplineHelper.prototype.update = function () {
+    for (var i = 0; i < this.points.length; i++) {
+        this.remove(this.points[i]);
+    }
+    this.points.length = 0;
 
+    this.object.userData.points.forEach(n => {
+        var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({
+            color: 0xff0000
+        });
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.position.copy(n);
+        this.add(mesh);
+    });
 };
 
 SplineHelper.prototype.updateObject = function () {
