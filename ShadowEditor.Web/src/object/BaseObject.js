02352106aed8f71b0f55cbc624123b2087bc0e28
tengge1 <930372551@qq.com>
2019-02-24 19:30:58 +0800

    物体基类。

---
+++ ShadowEditor.Web/src/object/BaseObject.js
@@ -0,0 +1,22 @@
+/**
+ * 物体基类
+ * @constructor
+ * @description 物体不需要继承该类，该类仅仅用来说明物体上的通用方法。
+ */
+function BaseObject() {
+    THREE.Object3D.call(this);
+}
+
+BaseObject.prototype = Object.create(THREE.Object3D.prototype);
+BaseObject.prototype.constructor = BaseObject;
+
+/**
+ * 创建类前调用，用于下载创建物体所需的类
+ */
+BaseObject.prototype.load = function () {
+    return new Promise(resolve => {
+        resolve();
+    });
+};
+
+export default BaseObject;
