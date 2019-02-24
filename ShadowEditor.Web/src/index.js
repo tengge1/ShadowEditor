1670dd8108f7b499ba38a639d3406ee29054c3bb
tengge1 <930372551@qq.com>
2019-02-24 21:33:14 +0800

    导出UI相关类。

--- ShadowEditor.Web/src/index.js
+++ ShadowEditor.Web/src/index.js
@@ -1,2 +1,46 @@
 export { default as Options } from './Options';
+
+// ui
+export { default as Control } from './ui/Control';
+export { default as SvgControl } from './ui/SvgControl';
+export { default as Alert } from './ui/Alert';
+export { default as Boolean } from './ui/Boolean';
+export { default as Break } from './ui/Break';
+export { default as Button } from './ui/Button';
+export { default as Canvas } from './ui/Canvas';
+export { default as Category } from './ui/Category';
+export { default as Checkbox } from './ui/Checkbox';
+export { default as CloseButton } from './ui/CloseButton';
+export { default as Color } from './ui/Color';
+export { default as Confirm } from './ui/Confirm';
+export { default as Container } from './ui/Container';
+export { default as DataTable } from './ui/DataTable';
+export { default as Div } from './ui/Div';
+export { default as HorizontalRule } from './ui/HorizontalRule';
+export { default as Html } from './ui/Html';
+export { default as IconButton } from './ui/IconButton';
+export { default as Image } from './ui/Image';
+export { default as ImageList } from './ui/ImageList';
+export { default as ImageUploader } from './ui/ImageUploader';
+export { default as Input } from './ui/Input';
+export { default as Integer } from './ui/Integer';
+export { default as Label } from './ui/Label';
+export { default as LinkButton } from './ui/LinkButton';
+export { default as MessageBox } from './ui/MessageBox';
+export { default as Modal } from './ui/Modal';
+export { default as Number } from './ui/Number';
+export { default as Outliner } from './ui/Outliner';
+export { default as Prompt } from './ui/Prompt';
+export { default as Row } from './ui/Row';
+export { default as SearchField } from './ui/SearchField';
+export { default as Select } from './ui/Select';
+export { default as Span } from './ui/Span';
+export { default as Text } from './ui/Text';
+export { default as TextArea } from './ui/TextArea';
+export { default as Texture } from './ui/Texture';
+export { default as Timeline } from './ui/Timeline';
+export { default as ToolbarFiller } from './ui/ToolbarFiller';
+export { default as Window } from './ui/Window';
+export { default as UI } from './ui/UI';
+
 export { default as Application } from './Application';
