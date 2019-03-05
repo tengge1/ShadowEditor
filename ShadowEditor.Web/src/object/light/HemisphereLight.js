405d16f01e12c89f4d1f104d5bb4df1f5d5ba11b
tengge1 <930372551@qq.com>
2019-03-05 20:06:44 +0800

    添加光源不再添加帮助器。

--- ShadowEditor.Web/src/object/light/HemisphereLight.js
+++ ShadowEditor.Web/src/object/light/HemisphereLight.js
@@ -10,26 +10,26 @@ import fragmentShader from './shader/hemisphere_fragment.glsl';
 function HemisphereLight(skyColor, groundColor, intensity) {
     THREE.HemisphereLight.call(this, skyColor, groundColor, intensity);
 
-    var uniforms = {
-        topColor: { value: new THREE.Color(skyColor) },
-        bottomColor: { value: new THREE.Color(groundColor) },
-        offset: { value: 33 },
-        exponent: { value: 0.6 }
-    };
+    // var uniforms = {
+    //     topColor: { value: new THREE.Color(skyColor) },
+    //     bottomColor: { value: new THREE.Color(groundColor) },
+    //     offset: { value: 33 },
+    //     exponent: { value: 0.6 }
+    // };
 
-    var skyGeo = new THREE.SphereBufferGeometry(4000, 32, 15);
-    var skyMat = new THREE.ShaderMaterial({
-        vertexShader: vertexShader,
-        fragmentShader: fragmentShader,
-        uniforms: uniforms,
-        side: THREE.BackSide
-    });
+    // var skyGeo = new THREE.SphereBufferGeometry(4000, 32, 15);
+    // var skyMat = new THREE.ShaderMaterial({
+    //     vertexShader: vertexShader,
+    //     fragmentShader: fragmentShader,
+    //     uniforms: uniforms,
+    //     side: THREE.BackSide
+    // });
 
-    var sky = new THREE.Mesh(skyGeo, skyMat);
-    sky.name = L_SKY;
-    sky.userData.type = 'sky';
+    // var sky = new THREE.Mesh(skyGeo, skyMat);
+    // sky.name = L_SKY;
+    // sky.userData.type = 'sky';
 
-    this.add(sky);
+    // this.add(sky);
 }
 
 HemisphereLight.prototype = Object.create(THREE.HemisphereLight.prototype);
--- ShadowEditor.Web/src/object/light/RectAreaLight.js
+++ ShadowEditor.Web/src/object/light/RectAreaLight.js
@@ -4,25 +4,25 @@
 function RectAreaLight(color, intensity, width, height) {
     THREE.RectAreaLight.call(this, color, intensity, width, height);
 
-    // 正面
-    var rectLightMesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial());
-    rectLightMesh.scale.x = width;
-    rectLightMesh.scale.y = height;
+    // // 正面
+    // var rectLightMesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial());
+    // rectLightMesh.scale.x = width;
+    // rectLightMesh.scale.y = height;
 
-    rectLightMesh.name = L_FrontSide;
-    rectLightMesh.userData.type = 'frontSide';
+    // rectLightMesh.name = L_FrontSide;
+    // rectLightMesh.userData.type = 'frontSide';
 
-    this.add(rectLightMesh);
+    // this.add(rectLightMesh);
 
-    var rectLightMeshBack = new THREE.Mesh(new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial({ color: 0x080808 }));
-    rectLightMeshBack.scale.x = width;
-    rectLightMeshBack.scale.y = height;
-    rectLightMeshBack.rotation.y = Math.PI;
+    // var rectLightMeshBack = new THREE.Mesh(new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial({ color: 0x080808 }));
+    // rectLightMeshBack.scale.x = width;
+    // rectLightMeshBack.scale.y = height;
+    // rectLightMeshBack.rotation.y = Math.PI;
 
-    rectLightMesh.name = L_BackSide;
-    rectLightMesh.userData.type = 'backSide';
+    // rectLightMesh.name = L_BackSide;
+    // rectLightMesh.userData.type = 'backSide';
 
-    this.add(rectLightMeshBack);
+    // this.add(rectLightMeshBack);
 }
 
 RectAreaLight.prototype = Object.create(THREE.RectAreaLight.prototype);
