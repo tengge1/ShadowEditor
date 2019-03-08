b3738722c491443e1684a74b549a11988f28e78f
tengge1 <930372551@qq.com>
2019-03-08 21:57:20 +0800

    树状控件。

--- ShadowEditor.Web/src/ui/Tree.js
+++ ShadowEditor.Web/src/ui/Tree.js
@@ -8,13 +8,40 @@ import UI from './Manager';
  */
 function Tree(options = {}) {
     Control.call(this, options);
+
+    this.data = options.data || [];
 };
 
 Tree.prototype = Object.create(Control.prototype);
 Tree.prototype.constructor = Tree;
 
 Tree.prototype.render = function () {
+    this.dom = document.createElement('ul');
+    this.parent.appendChild(this.dom);
 
+    this.data.forEach(n => {
+        this._createNode(n, this.dom);
+    });
+};
+
+Tree.prototype._createNode = function (data, dom) {
+    var li = document.createElement('li');
+    dom.appendChild(li);
+
+    Object.assign(li, {
+        value: data.value,
+    });
+
+    var a = document.createElement('a');
+    a.setAttribute('href', 'javascript:;');
+    a.innerHTML = data.text || '';
+    li.appendChild(a);
+
+    if (Array.isArray(data.children)) {
+        data.children.forEach(n => {
+            this._createNode(n, li);
+        });
+    }
 };
 
 UI.addXType('tree', Tree);
