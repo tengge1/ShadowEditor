503f0dd878c556d7ed7a04e1d72f39edc7688909
tengge1 <930372551@qq.com>
2019-03-30 15:58:14 +0800

    修复地球底图偏上bug。

--- ShadowEditor.Web/src/gis/render/shader/tiled_vertex.glsl
+++ ShadowEditor.Web/src/gis/render/shader/tiled_vertex.glsl
@@ -6,6 +6,7 @@ uniform int z;
 
 varying vec2 vUV;
 
+// 必应地图参数
 #define EARTH_RADIUS 6378137.0
 #define MIN_LATITUDE -85.05112878
 #define MAX_LATITUDE 85.05112878
@@ -14,6 +15,7 @@ varying vec2 vUV;
 #define PI 3.141592653589793
 
 void main() {
+    // 每个瓦片位置
     float size = pow(2.0, float(z));
     float dlon = (MAX_LONGITUDE - MIN_LONGITUDE) / size;
     float dlat = (MAX_LATITUDE - MIN_LATITUDE) / size;
@@ -23,8 +25,10 @@ void main() {
     float right = left + dlon;
     float bottom = top - dlat;
 
-    float lon = left + (right - left) * position.x;
-    float lat = top - (top - bottom) * position.y;
+    // 瓦片上每个小格位置
+    // +0.5的原因是：position范围是-0.5到0.5
+    float lon = left + (right - left) * (0.5 + position.x);
+    float lat = top - (top - bottom) * (0.5 + position.y);
 
     lon = lon * PI / 180.0;
     lat = lat * PI / 180.0;
