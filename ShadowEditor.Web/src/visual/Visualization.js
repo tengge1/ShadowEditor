c1cbece4378e6fce3023c9f8d1ab054117f002df
tengge1 <930372551@qq.com>
2019-04-30 18:34:53 +0800

    Visualization.js

---
+++ ShadowEditor.Web/src/visual/Visualization.js
@@ -0,0 +1,31 @@
+/**
+ * 可视化
+ */
+function Visualization() {
+
+}
+
+/**
+ * 将所有控件渲染到svgDom里面
+ * @param {SVGSVGElement} svgDom svg根元素
+ */
+VisualControl.prototype.render = function (svgDom) {
+
+};
+
+/**
+ * svg控件转json
+ */
+Visualization.prototype.toJson = function () {
+    return {};
+};
+
+/**
+ * json转svg控件
+ * @param {*} json 
+ */
+Visualization.prototype.fromJson = function (json) {
+
+};
+
+export default VisualControl;
