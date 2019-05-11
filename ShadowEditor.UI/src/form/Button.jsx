1e583f200eeb76f6c686dd3cc49c58dad8867390
tengge1 <930372551@qq.com>
2019-05-11 15:01:14 +0800

    Button.jsx

--- ShadowEditor.UI/src/form/Button.jsx
+++ ShadowEditor.UI/src/form/Button.jsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 
 /**
  * 按钮
@@ -6,14 +7,26 @@ import classNames from 'classnames/bind';
  * @property {String} className 样式类
  * @property {Object} style 样式
  * @property {String} children 内容
+ * @property {Function} onClick 点击事件
  */
 class Button extends React.Component {
     render() {
-        const { className, style, children } = this.props;
-        return <button className={classNames('Button', className)} style={style}>
+        const { className, style, children, onClick } = this.props;
+        return <button className={classNames('Button', className)} style={style} onClick={onClick}>
             {children}
         </button>;
     }
 }
+
+Button.propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+    children: PropTypes.element,
+    onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+    children: '',
+};
 
 export default Button;
