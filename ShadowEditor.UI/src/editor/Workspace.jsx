1ed4678c1016f32a9dbc955ec90935b5043fee8e
tengge1 <930372551@qq.com>
2019-05-13 20:50:45 +0800

    工具栏优化。

--- ShadowEditor.UI/src/editor/Workspace.jsx
+++ ShadowEditor.UI/src/editor/Workspace.jsx
@@ -30,6 +30,7 @@ class Workspace extends React.Component {
         const { className, style } = this.props;
 
         return <VBoxLayout className={classNames('Workspace', className)} style={style}>
+            <Button className={'primary'}>Test Button</Button>
         </VBoxLayout>;
     }
 }
