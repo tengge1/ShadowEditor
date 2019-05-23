be7c7efe40074e12eba541b733f089edcf7d4771
tengge1 <930372551@qq.com>
2019-05-23 20:21:40 +0800

    Content

--- ShadowEditor.UI/src/layout/Content.jsx
+++ ShadowEditor.UI/src/layout/Content.jsx
@@ -1,18 +1,32 @@
+import './css/Content.css';
 import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
 
 /**
  * 内容
  * @author tengge / https://github.com/tengge1
- * @property {String} className 样式类
- * @property {Object} style 样式
- * @property {String} children 内容
  */
 class Content extends React.Component {
     render() {
-        const { className, style, children } = this.props;
+        const { className, style, children, ...others } = this.props;
 
-        return <div className={classNames('Content', className)} style={style}>{children}</div>;
+        return <div
+            className={classNames('Content', className)}
+            style={style}
+            {...others}>{children}</div>;
     }
 }
+
+Content.propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+    children: PropTypes.node,
+};
+
+Content.defaultProps = {
+    className: null,
+    style: null,
+    children: null,
+};
 
 export default Content;
