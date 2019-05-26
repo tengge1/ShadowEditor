70e52d632f8d1f901552bd1acc70b998d4875c6b
tengge1 <930372551@qq.com>
2019-05-26 09:40:40 +0800

    Canvas

--- ShadowEditor.UI/src/media/Canvas.jsx
+++ ShadowEditor.UI/src/media/Canvas.jsx
@@ -5,20 +5,26 @@ import PropTypes from 'prop-types';
 /**
  * 画布
  * @author tengge / https://github.com/tengge1
- * @property {String} className 样式类
- * @property {Object} style 样式
  */
 class Canvas extends React.Component {
     render() {
-        const { className, style } = this.props;
+        const { className, style, ...others } = this.props;
 
-        return <canvas className={classNames('Canvas', className)} style={style}></canvas>;
+        return <canvas
+            className={classNames('Canvas', className)}
+            style={style}
+            {...others}></canvas>;
     }
 }
 
 Canvas.propTypes = {
     className: PropTypes.string,
     style: PropTypes.object,
+};
+
+Canvas.defaultProps = {
+    className: null,
+    style: null,
 };
 
 export default Canvas;
