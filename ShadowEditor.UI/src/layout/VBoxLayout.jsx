5fde987a196522267a936ef551ad41fc3f76bed8
tengge1 <930372551@qq.com>
2019-05-26 09:14:23 +0800

    VBoxLayout

--- ShadowEditor.UI/src/layout/VBoxLayout.jsx
+++ ShadowEditor.UI/src/layout/VBoxLayout.jsx
@@ -5,22 +5,28 @@ import PropTypes from 'prop-types';
 /**
  * 竖直布局
  * @author tengge / https://github.com/tengge1
- * @property {String} className 样式类
- * @property {Object} style 样式
- * @property {String} children 内容
  */
 class VBoxLayout extends React.Component {
     render() {
-        const { className, style, children } = this.props;
+        const { className, style, children, ...others } = this.props;
 
-        return <div className={classNames('VBoxLayout', className)} style={style}>{children}</div>;
+        return <div
+            className={classNames('VBoxLayout', className)}
+            style={style}
+            {...others}>{children}</div>;
     }
 }
 
 VBoxLayout.propTypes = {
     className: PropTypes.string,
     style: PropTypes.object,
-    children: PropTypes.element,
+    children: PropTypes.node,
+};
+
+VBoxLayout.defaultProps = {
+    className: null,
+    style: null,
+    children: null,
 };
 
 export default VBoxLayout;
