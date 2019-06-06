cfc4f52b3bee4fe0582935a1e67f21b25e48043c
tengge1 <930372551@qq.com>
2019-06-06 20:16:25 +0800

    简化代码。

--- ShadowEditor.Web/test/js/EffectComposer.js
+++ ShadowEditor.Web/test/js/EffectComposer.js
@@ -1,87 +1,45 @@
 /**
  * @author alteredq / http://alteredqualia.com/
  */
-THREE.EffectComposer = function (renderer, renderTarget) {
-
+THREE.EffectComposer = function (renderer) {
 	this.renderer = renderer;
 
 	var size = renderer.getDrawingBufferSize(new THREE.Vector2());
 
-	renderTarget = new THREE.WebGLRenderTarget(size.width, size.height, {
+	var renderTarget = new THREE.WebGLRenderTarget(size.width, size.height, {
 		minFilter: THREE.LinearFilter,
 		magFilter: THREE.LinearFilter,
 		format: THREE.RGBAFormat,
 		stencilBuffer: false
 	});
 
-	renderTarget.texture.name = 'EffectComposer.rt1';
-
-	this.renderTarget1 = renderTarget;
-	this.renderTarget2 = renderTarget.clone();
-	this.renderTarget2.texture.name = 'EffectComposer.rt2';
-
-	this.writeBuffer = this.renderTarget1;
-	this.readBuffer = this.renderTarget2;
+	this.writeBuffer = renderTarget;
+	this.readBuffer = renderTarget.clone();
 
 	this.passes = [];
-
-	this._previousFrameTime = Date.now();
-
 };
 
 Object.assign(THREE.EffectComposer.prototype, {
 
 	addPass: function (pass) {
-
 		this.passes.push(pass);
-
-		var size = this.renderer.getDrawingBufferSize(new THREE.Vector2());
-		pass.setSize(size.width, size.height);
-
 	},
 
-	render: function (deltaTime) {
-
-		if (deltaTime === undefined) {
-
-			deltaTime = (Date.now() - this._previousFrameTime) * 0.001;
-
-		}
-
-		this._previousFrameTime = Date.now();
-
-		var currentRenderTarget = this.renderer.getRenderTarget();
-
+	render: function () {
 		var pass, i, il = this.passes.length;
 
 		for (i = 0; i < il; i++) {
-
 			pass = this.passes[i];
-
-			pass.render(this.renderer, this.writeBuffer, this.readBuffer, deltaTime);
+			pass.render(this.renderer, this.writeBuffer, this.readBuffer);
 		}
-
-		this.renderer.setRenderTarget(currentRenderTarget);
 	}
 });
 
 
-THREE.Pass = function () {
-	// if set to true, the pass is processed by the composer
-	this.enabled = true;
-
-	// if set to true, the pass clears its buffer before rendering
-	this.clear = false;
-
-	// if set to true, the result of the pass is rendered to screen
-	this.renderToScreen = false;
-};
+THREE.Pass = function () { };
 
 Object.assign(THREE.Pass.prototype, {
+	render: function (renderer, writeBuffer, readBuffer) {
 
-	setSize: function (width, height) { },
-
-	render: function (renderer, writeBuffer, readBuffer, deltaTime, maskActive) {
-		console.error('THREE.Pass: .render() must be implemented in derived pass.');
 	}
 });
--- ShadowEditor.Web/test/js/OutlinePass.js
+++ ShadowEditor.Web/test/js/OutlinePass.js
@@ -1,24 +1,17 @@
 /**
  * @author spidersharma / http://eduperiment.com/
  */
-
 THREE.OutlinePass = function (resolution, scene, camera, selectedObjects) {
 
 	this.renderScene = scene;
 	this.renderCamera = camera;
 	this.selectedObjects = selectedObjects;
-	this.visibleEdgeColor = new THREE.Color(1, 1, 1);
-	this.hiddenEdgeColor = new THREE.Color(0.1, 0.04, 0.02);
-	this.edgeGlow = 0.0;
-	this.usePatternTexture = false;
-	this.edgeThickness = 1.0;
-	this.edgeStrength = 3.0;
-	this.downSampleRatio = 2;
-	this.pulsePeriod = 0;
+
+	this.downSampleRatio = 1;
 
 	THREE.Pass.call(this);
 
-	this.resolution = (resolution !== undefined) ? new THREE.Vector2(resolution.x, resolution.y) : new THREE.Vector2(256, 256);
+	this.resolution = new THREE.Vector2(resolution.x, resolution.y);
 
 	var pars = {
 		minFilter: THREE.LinearFilter,
@@ -26,25 +19,16 @@ THREE.OutlinePass = function (resolution, scene, camera, selectedObjects) {
 		format: THREE.RGBAFormat
 	};
 
-	var resx = Math.round(this.resolution.x / this.downSampleRatio);
-	var resy = Math.round(this.resolution.y / this.downSampleRatio);
+	this.resx = Math.round(this.resolution.x / this.downSampleRatio);
+	this.resy = Math.round(this.resolution.y / this.downSampleRatio);
 
-	this.maskBufferMaterial = new THREE.MeshBasicMaterial({
-		color: 0xffffff
-	});
-	this.maskBufferMaterial.side = THREE.DoubleSide;
 	this.renderTargetMaskBuffer = new THREE.WebGLRenderTarget(this.resolution.x, this.resolution.y, pars);
-	this.renderTargetMaskBuffer.texture.name = "OutlinePass.mask";
 	this.renderTargetMaskBuffer.texture.generateMipmaps = false;
 
 	this.edgeDetectionMaterial = this.getEdgeDetectionMaterial();
-	this.renderTargetEdgeBuffer1 = new THREE.WebGLRenderTarget(resx, resy, pars);
-	this.renderTargetEdgeBuffer1.texture.name = "OutlinePass.edge1";
-	this.renderTargetEdgeBuffer1.texture.generateMipmaps = false;
 
-	// copy material
-	if (THREE.CopyShader === undefined)
-		console.error("THREE.OutlinePass relies on THREE.CopyShader");
+	this.renderTargetEdgeBuffer1 = new THREE.WebGLRenderTarget(this.resx, this.resy, pars);
+	this.renderTargetEdgeBuffer1.texture.generateMipmaps = false;
 
 	var copyShader = THREE.CopyShader;
 
@@ -61,9 +45,6 @@ THREE.OutlinePass = function (resolution, scene, camera, selectedObjects) {
 		transparent: true
 	});
 
-	this.enabled = true;
-	this.needsSwap = false;
-
 	this.oldClearColor = new THREE.Color();
 	this.oldClearAlpha = 1;
 
@@ -71,50 +52,13 @@ THREE.OutlinePass = function (resolution, scene, camera, selectedObjects) {
 	this.scene = new THREE.Scene();
 
 	this.quad = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), null);
-	this.quad.frustumCulled = false; // Avoid getting clipped
+	this.quad.frustumCulled = false;
 	this.scene.add(this.quad);
-
-	this.textureMatrix = new THREE.Matrix4();
-
-	function replaceDepthToViewZ(string, camera) {
-		var type = camera.isPerspectiveCamera ? 'perspective' : 'orthographic';
-
-		return string.replace(/DEPTH_TO_VIEW_Z/g, type + 'DepthToViewZ');
-	}
 };
 
 THREE.OutlinePass.prototype = Object.assign(Object.create(THREE.Pass.prototype), {
 
 	constructor: THREE.OutlinePass,
-
-	setSize: function (width, height) {
-		this.renderTargetMaskBuffer.setSize(width, height);
-
-		var resx = Math.round(width / this.downSampleRatio);
-		var resy = Math.round(height / this.downSampleRatio);
-
-		this.renderTargetEdgeBuffer1.setSize(resx, resy);
-	},
-
-	changeVisibilityOfSelectedObjects: function (bVisible) {
-
-		function gatherSelectedMeshesCallBack(object) {
-			if (object.isMesh) {
-				if (bVisible) {
-					object.visible = object.userData.oldVisible;
-					delete object.userData.oldVisible;
-				} else {
-					object.userData.oldVisible = object.visible;
-					object.visible = bVisible;
-				}
-			}
-		}
-
-		for (var i = 0; i < this.selectedObjects.length; i++) {
-			var selectedObject = this.selectedObjects[i];
-			selectedObject.traverse(gatherSelectedMeshesCallBack);
-		}
-	},
 
 	render: function (renderer, writeBuffer, readBuffer, deltaTime) {
 
@@ -129,14 +73,11 @@ THREE.OutlinePass.prototype = Object.assign(Object.create(THREE.Pass.prototype),
 		renderer.setRenderTarget(this.renderTargetMaskBuffer);
 		renderer.clear();
 		renderer.render(this.renderScene, this.renderCamera);
-		this.renderScene.overrideMaterial = null;
 
 		// 3. Apply Edge Detection Pass
 		this.quad.material = this.edgeDetectionMaterial;
 		this.edgeDetectionMaterial.uniforms["maskTexture"].value = this.renderTargetMaskBuffer.texture;
-		this.edgeDetectionMaterial.uniforms["texSize"].value = new THREE.Vector2(this.renderTargetMaskBuffer.width / 2, this.renderTargetMaskBuffer.height / 2);
-		this.edgeDetectionMaterial.uniforms["visibleEdgeColor"].value = this.visibleEdgeColor;
-		this.edgeDetectionMaterial.uniforms["hiddenEdgeColor"].value = this.visibleEdgeColor;
+		this.edgeDetectionMaterial.uniforms["texSize"].value.set(this.resx, this.resy);
 		renderer.setRenderTarget(this.renderTargetEdgeBuffer1);
 		renderer.clear();
 		renderer.render(this.scene, this.camera);
