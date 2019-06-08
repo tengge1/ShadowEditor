b80ba7249ea019aec0180d7fc92d4f59f0a24760
tengge1 <930372551@qq.com>
2019-06-08 22:24:38 +0800

    工具栏代码优化。

--- ShadowEditor.UI/src/editor/EditorUI.jsx
+++ ShadowEditor.UI/src/editor/EditorUI.jsx
@@ -2,7 +2,7 @@ import BorderLayout from '../layout/BorderLayout.jsx';
 
 import EditorMenuBar from './menu/EditorMenuBar.jsx';
 import EditorStatusBar from './status/EditorStatusBar.jsx';
-import EToolbar from './EToolbar.jsx';
+import EditorToolbar from './toolbar/EditorToolbar.jsx';
 import EWorkspace from './EWorkspace.jsx';
 import ESideBar from './ESideBar.jsx';
 
@@ -15,7 +15,7 @@ class EditorUI extends React.Component {
         return <BorderLayout>
             <EditorMenuBar region={'north'}></EditorMenuBar>
             <EditorStatusBar region={'south'}></EditorStatusBar>
-            <EToolbar region={'west'}></EToolbar>
+            <EditorToolbar region={'west'}></EditorToolbar>
             <ESideBar region={'east'} split={true}></ESideBar>
             <EWorkspace region={'center'}></EWorkspace>
         </BorderLayout>;
