a6676e943f49c613ca3c223b9e0fb11211cd7292
tengge1 <930372551@qq.com>
2019-06-17 21:14:49 +0800

    各种面板。

--- ShadowEditor.UI/src/editor/assets/AssetsPanel.jsx
+++ ShadowEditor.UI/src/editor/assets/AssetsPanel.jsx
@@ -13,7 +13,16 @@ import Panel from '../../panel/Panel.jsx';
 class AssetsPanel extends React.Component {
     render() {
         return <AccordionLayout>
-            <Panel className={'AssetsPanel'} title={'Assets Panel'} maximizable={true}>Assets Panel</Panel>
+            <Panel className={'ScenePanel'} title={'Scene'} maximizable={true}>Scene Panel</Panel>
+            <Panel className={'ModelPanel'} title={'Model'} maximizable={true}>Model Panel</Panel>
+            <Panel className={'MapPanel'} title={'Map'} maximizable={true}>Map Panel</Panel>
+            <Panel className={'MaterialPanel'} title={'Material'} maximizable={true}>Material Panel</Panel>
+            <Panel className={'AudioPanel'} title={'Audio'} maximizable={true}>Audio Panel</Panel>
+            <Panel className={'AnimationPanel'} title={'Animation'} maximizable={true}>Animation Panel</Panel>
+            <Panel className={'ParticlePanel'} title={'Particle'} maximizable={true}>Particle Panel</Panel>
+            <Panel className={'PrefabPanel'} title={'Prefab'} maximizable={true}>Prefab Panel</Panel>
+            <Panel className={'CharacterPanel'} title={'Character'} maximizable={true}>Character Panel</Panel>
+            <Panel className={'LogPanel'} title={'Log'} maximizable={true}>Log Panel</Panel>
         </AccordionLayout>;
     }
 }
