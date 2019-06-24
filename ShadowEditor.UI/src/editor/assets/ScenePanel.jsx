189477381ac8554aa3ee23a49aedb2f8514b24d3
tengge1 <930372551@qq.com>
2019-06-24 20:32:35 +0800

    SearchField

--- ShadowEditor.UI/src/editor/assets/ScenePanel.jsx
+++ ShadowEditor.UI/src/editor/assets/ScenePanel.jsx
@@ -1,15 +1,15 @@
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
+import ImageList from '../../image/ImageList.jsx';
+
 /**
  * 场景面板
  * @author tengge / https://github.com/tengge1
  */
 class ScenePanel extends React.Component {
     render() {
-        return <div>
-            Scene Panel
-        </div>;
+        return <ImageList></ImageList>;
     }
 }
 
