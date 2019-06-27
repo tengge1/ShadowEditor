ccceeaa1d0b249d961caa3daf40a9745b39df3d9
liteng <930372551@qq.com>
2019-06-28 07:24:01 +0800

    LightMenu.jsx

--- ShadowEditor.Web/src/editor2/menu/LightMenu.jsx
+++ ShadowEditor.Web/src/editor2/menu/LightMenu.jsx
@@ -8,25 +8,132 @@ class LightMenu extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event) {
-        event.stopPropagation();
-        alert('Hello, world!');
+        this.handleAddAmbientLight = this.handleAddAmbientLight.bind(this);
+        this.handleAddDirectionalLight = this.handleAddDirectionalLight.bind(this);
+        this.handleAddPointLight = this.handleAddPointLight.bind(this);
+        this.handleAddSpotLight = this.handleAddSpotLight.bind(this);
+        this.handleAddHemisphereLight = this.handleAddHemisphereLight.bind(this);
+        this.handleAddRectAreaLight = this.handleAddRectAreaLight.bind(this);
     }
 
     render() {
         const { className, style } = this.props;
 
         return <MenuItem title={L_LIGHT}>
-            <MenuItem title={L_AMBIENT_LIGHT}></MenuItem>
-            <MenuItem title={L_DIRECTIONAL_LIGHT}></MenuItem>
-            <MenuItem title={L_POINT_LIGHT}></MenuItem>
-            <MenuItem title={L_SPOT_LIGHT}></MenuItem>
-            <MenuItem title={L_HEMISPHERE_LIGHT}></MenuItem>
-            <MenuItem title={L_RECT_AREA_LIGHT}></MenuItem>
+            <MenuItem title={L_AMBIENT_LIGHT} onClick={this.handleAddAmbientLight}></MenuItem>
+            <MenuItem title={L_DIRECTIONAL_LIGHT} onClick={this.handleAddAmbientLight}></MenuItem>
+            <MenuItem title={L_POINT_LIGHT} onClick={this.handleAddAmbientLight}></MenuItem>
+            <MenuItem title={L_SPOT_LIGHT} onClick={this.handleAddAmbientLight}></MenuItem>
+            <MenuItem title={L_HEMISPHERE_LIGHT} onClick={this.handleAddAmbientLight}></MenuItem>
+            <MenuItem title={L_RECT_AREA_LIGHT} onClick={this.handleAddAmbientLight}></MenuItem>
         </MenuItem>;
+    }
+
+    // ------------------------- 环境光 ------------------------------
+
+    handleAddAmbientLight() {
+        var editor = this.app.editor;
+
+        var color = 0xaaaaaa;
+
+        var light = new THREE.AmbientLight(color);
+        light.name = L_AMBIENT_LIGHT;
+
+        editor.execute(new AddObjectCommand(light));
+    }
+
+    // ------------------------- 平行光 ------------------------------
+
+    handleAddDirectionalLight() {
+        var editor = this.app.editor;
+
+        var color = 0xffffff;
+        var intensity = 1;
+
+        var light = new THREE.DirectionalLight(color, intensity);
+        light.name = L_DIRECTIONAL_LIGHT;
+        light.castShadow = true;
+        light.shadow.mapSize.x = 2048;
+        light.shadow.mapSize.y = 2048;
+        light.shadow.camera.left = -100;
+        light.shadow.camera.right = 100;
+        light.shadow.camera.top = 100;
+        light.shadow.camera.bottom = -100;
+        light.position.set(5, 10, 7.5);
+
+        editor.execute(new AddObjectCommand(light));
+    }
+
+    // ------------------------- 点光源 ------------------------------
+
+    handleAddPointLight() {
+        var editor = this.app.editor;
+
+        var color = 0xffffff;
+        var intensity = 1;
+        var distance = 0;
+
+        var light = new PointLight(color, intensity, distance);
+        light.name = L_POINT_LIGHT;
+        light.position.y = 5;
+        light.castShadow = true;
+
+        editor.execute(new AddObjectCommand(light));
+    }
+
+    // ------------------------- 聚光灯 ------------------------------
+
+    handleAddSpotLight() {
+        var editor = this.app.editor;
+
+        var color = 0xffffff;
+        var intensity = 1;
+        var distance = 0;
+        var angle = Math.PI * 0.1;
+        var penumbra = 0;
+
+        var light = new THREE.SpotLight(color, intensity, distance, angle, penumbra);
+
+        light.name = L_SPOT_LIGHT;
+        light.castShadow = true;
+
+        light.position.set(5, 10, 7.5);
+
+        editor.execute(new AddObjectCommand(light));
+    }
+
+    // ------------------------- 半球光 ------------------------------
+
+    handleAddHemisphereLight() {
+        var editor = this.app.editor;
+        var skyColor = 0x00aaff;
+        var groundColor = 0xffaa00;
+        var intensity = 1;
+
+        var light = new HemisphereLight(skyColor, groundColor, intensity);
+        light.name = L_HEMISPHERE_LIGHT;
+
+        light.position.set(0, 10, 0);
+
+        editor.execute(new AddObjectCommand(light));
+    }
+
+    // ------------------------- 矩形光 ------------------------------
+
+    handleAddRectAreaLight() {
+        var editor = this.app.editor;
+
+        var color = 0xffffff;
+        var intensity = 1;
+        var width = 20;
+        var height = 10;
+
+        var light = new RectAreaLight(color, intensity, width, height);
+        light.name = L_RECT_AREA_LIGHT;
+
+        light.position.set(0, 6, 0);
+
+        editor.execute(new AddObjectCommand(light));
     }
 }
 
