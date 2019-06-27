eff64fe76a1ec1a3abfcc2d2baaabf39a6ad42ee
liteng <930372551@qq.com>
2019-06-28 07:36:14 +0800

    SceneMenu.jsx

--- ShadowEditor.Web/src/editor2/menu/PlayMenu.jsx
+++ ShadowEditor.Web/src/editor2/menu/PlayMenu.jsx
@@ -8,20 +8,81 @@ class PlayMenu extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event) {
-        event.stopPropagation();
-        alert('Hello, world!');
+        this.handleTogglePlay = this.handleTogglePlay.bind(this);
+        this.handlePlayFullscreen = this.handlePlayFullscreen.bind(this);
+        this.handlePlayNewWindow = this.handlePlayNewWindow.bind(this);
     }
 
     render() {
         return <MenuItem title={L_PLAY}>
-            <MenuItem title={L_PLAY}></MenuItem>
-            <MenuItem title={L_PLAY_FULLSCREEN}></MenuItem>
-            <MenuItem title={L_PLAY_NEW_WINDOW}></MenuItem>
+            <MenuItem title={L_PLAY} onClick={this.handleTogglePlay}></MenuItem>
+            <MenuItem title={L_PLAY_FULLSCREEN} onClick={this.handlePlayFullscreen}></MenuItem>
+            <MenuItem title={L_PLAY_NEW_WINDOW} onClick={this.handlePlayNewWindow}></MenuItem>
         </MenuItem>;
+    }
+
+    handleTogglePlay() {
+        if (this.isPlaying) {
+            this.stopPlay();
+        } else {
+            this.startPlay();
+        }
+    }
+
+    handleStartPlay() { // 启动播放
+        if (this.isPlaying) {
+            return;
+        }
+
+        this.isPlaying = true;
+
+        var play = UI.get('mPlay', this.id);
+        play.dom.innerHTML = L_STOP;
+
+        // 将场景数据转换为字符串
+        var jsons = (new Converter()).toJSON({
+            options: this.app.options,
+            scene: this.app.editor.scene,
+            camera: this.app.editor.camera,
+            renderer: this.app.editor.renderer,
+            scripts: this.app.editor.scripts,
+            animations: this.app.editor.animations,
+            visual: this.app.editor.visual,
+        });
+
+        this.app.player.start(JSON.stringify(jsons));
+    }
+
+    handleStopPlay() { // 停止播放
+        if (!this.isPlaying) {
+            return;
+        }
+
+        this.isPlaying = false;
+
+        var play = UI.get('mPlay', this.id);
+        play.dom.innerHTML = L_PLAY;
+
+        this.app.player.stop();
+    }
+
+    handlePlayFullscreen() { // 全屏播放
+        if (!this.isPlaying) {
+            this.startPlay();
+        }
+
+        UI.get('player', this.app.player.id).dom.requestFullscreen();
+    }
+
+    handlePlayNewWindow() { // 新窗口播放
+        var sceneID = this.app.editor.sceneID;
+
+        if (!sceneID) {
+            UI.msg('请先保存场景！');
+            return;
+        }
+
+        window.open(`view.html?sceneID=${sceneID}`, 'ShadowPlayer');
     }
 }
 
--- ShadowEditor.Web/src/editor2/menu/SceneMenu.jsx
+++ ShadowEditor.Web/src/editor2/menu/SceneMenu.jsx
@@ -8,24 +8,199 @@ class SceneMenu extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event) {
-        alert('Hello, world!');
+        this.handleCreateEmptyScene = this.handleCreateEmptyScene.bind(this);
+        this.handleCreateGISScene = this.handleCreateGISScene.bind(this);
+        this.handleSaveScene = this.handleSaveScene.bind(this);
+        this.handleSaveAsScene = this.handleSaveAsScene.bind(this);
+        this.handleExportScene = this.handleExportScene.bind(this);
     }
 
     render() {
         return <MenuItem title={L_SCENE}>
             <MenuItem title={L_NEW}>
-                <MenuItem title={L_EMPTY_SCENE} onClick={this.handleClick}></MenuItem>
-                <MenuItem title={L_GIS_SCENE}></MenuItem>
+                <MenuItem title={L_EMPTY_SCENE} onClick={this.handleCreateEmptyScene}></MenuItem>
+                <MenuItem title={L_GIS_SCENE} onClick={this.handleCreateGISScene}></MenuItem>
             </MenuItem>
-            <MenuItem title={L_SAVE}></MenuItem>
-            <MenuItem title={L_SAVE_AS}></MenuItem>
+            <MenuItem title={L_SAVE} onClick={this.handleSaveScene}></MenuItem>
+            <MenuItem title={L_SAVE_AS} onClick={this.handleSaveAsScene}></MenuItem>
             <MenuItemSeparator />
-            <MenuItem title={L_EXPORT_SCENE}></MenuItem>
+            <MenuItem title={L_EXPORT_SCENE} onClick={this.handleExportScene}></MenuItem>
         </MenuItem>;
+    }
+
+    // ---------------------------- 新建空场景 ---------------------------------
+
+    handleCreateEmptyScene() {
+        var editor = this.app.editor;
+
+        if (editor.sceneID == null) {
+            editor.clear();
+            editor.sceneID = null;
+            editor.sceneName = null;
+            document.title = L_NO_NAME;
+            UI.msg(L_CREATE_EMPTY_SCENE_SUCCESS);
+            return;
+        }
+
+        UI.confirm(L_CONFIRM, L_UNSAVED_WILL_LOSE_CONFIRM, (event, btn) => {
+            if (btn === 'ok') {
+                editor.clear();
+                editor.sceneID = null;
+                editor.sceneName = null;
+                this.app.options.sceneType = 'Empty';
+                document.title = L_NO_NAME;
+                this.app.editor.camera.userData.control = 'OrbitControls';
+            }
+        });
+    }
+
+    // --------------------------- 新建GIS场景 -------------------------------------
+
+    handleCreateGISScene() {
+        if (this.app.editor.gis) {
+            this.app.editor.gis.stop();
+        }
+
+        this.app.editor.gis = new GISScene(this.app);
+        this.app.editor.gis.start();
+
+        this.app.options.sceneType = 'GIS';
+
+        this.app.editor.camera.userData.control = '';
+
+        this.app.call(`sceneGraphChanged`, this);
+    }
+
+    // --------------------------- 保存场景 ----------------------------------------
+
+    handleSaveScene() { // 保存场景
+        var editor = this.app.editor;
+        var id = editor.sceneID;
+        var sceneName = editor.sceneName;
+
+        if (id) { // 编辑场景
+            this.commitSave(id, sceneName);
+        } else { // 新建场景
+            UI.prompt(L_SAVE_SCENE, L_NAME, L_NEW_SCENE, (event, name) => {
+                this.commitSave(id, name);
+            });
+        }
+    }
+
+    commitSave(id, sceneName) {
+        var editor = this.app.editor;
+
+        // 记录选中物体，以便载入时还原场景选中
+        var selected = this.app.editor.selected;
+        if (selected) {
+            this.app.options.selected = selected.uuid;
+        }
+
+        var obj = (new Converter()).toJSON({
+            options: this.app.options,
+            camera: editor.camera,
+            renderer: editor.renderer,
+            scripts: editor.scripts,
+            animations: editor.animations,
+            scene: editor.scene,
+            visual: editor.visual,
+        });
+
+        var params = {
+            Name: sceneName,
+            Data: JSON.stringify(obj)
+        };
+
+        if (id) {
+            params.ID = id;
+        }
+
+        Ajax.post(`${this.app.options.server}/api/Scene/Save`, params, result => {
+            var obj = JSON.parse(result);
+
+            if (obj.Code === 200) {
+                editor.sceneID = obj.ID;
+                editor.sceneName = sceneName;
+                document.title = sceneName;
+            }
+
+            this.app.call(`sceneSaved`, this);
+
+            UI.msg(obj.Msg);
+        });
+    }
+
+    // --------------------------- 另存为场景 -------------------------------------
+
+    handleSaveAsScene() {
+        var sceneName = this.app.editor.sceneName;
+
+        if (sceneName == null) {
+            sceneName = L_NEW_SCENE;
+        }
+
+        UI.prompt(L_SAVE_SCENE, L_NAME, sceneName, (event, name) => {
+            this.app.editor.sceneName = name;
+            document.title = name;
+            this.commitSaveAs(name);
+        });
+    }
+
+    commitSaveAs(sceneName) {
+        var editor = this.app.editor;
+
+        var obj = (new Converter()).toJSON({
+            options: this.app.options,
+            camera: editor.camera,
+            renderer: editor.renderer,
+            scripts: editor.scripts,
+            animations: editor.animations,
+            scene: editor.scene,
+            visual: editor.visual,
+        });
+
+        Ajax.post(`${this.app.options.server}/api/Scene/Save`, {
+            Name: sceneName,
+            Data: JSON.stringify(obj)
+        }, result => {
+            var obj = JSON.parse(result);
+
+            if (obj.Code === 200) {
+                editor.sceneID = obj.ID;
+                editor.sceneName = sceneName;
+                document.title = sceneName;
+            }
+
+            this.app.call(`sceneSaved`, this);
+
+            UI.msg(obj.Msg);
+        });
+    }
+
+    // -------------------------- 导出场景 --------------------------------
+
+    handleExportScene = function () {
+        var sceneID = this.app.editor.sceneID;
+
+        if (!sceneID) {
+            UI.msg('请先打开场景！');
+            return;
+        }
+
+        UI.confirm('询问', '是否导出当前场景？', (event, btn) => {
+            if (btn === 'ok') {
+                fetch(`${this.app.options.server}/api/ExportScene/Run?ID=${sceneID}`, {
+                    method: 'POST'
+                }).then(response => {
+                    if (response.ok) {
+                        response.json().then(json => {
+                            UI.msg(json.Msg);
+                            window.open(`${this.app.options.server}${json.Url}`, 'export');
+                        });
+                    }
+                });
+            }
+        });
     }
 }
 
