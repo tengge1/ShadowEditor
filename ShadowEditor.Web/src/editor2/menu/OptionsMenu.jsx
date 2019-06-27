98f865c00e200af120248c663109105fb26e1fdf
liteng <930372551@qq.com>
2019-06-28 07:28:29 +0800

    OptionsMenu.jsx

--- ShadowEditor.Web/src/editor2/menu/OptionsMenu.jsx
+++ ShadowEditor.Web/src/editor2/menu/OptionsMenu.jsx
@@ -8,24 +8,74 @@ class OptionsMenu extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event) {
-        event.stopPropagation();
-        alert('Hello, world!');
+        this.handleSurfaceOptions = this.handleSurfaceOptions.bind(this);
+        this.handleRendererOptions = this.handleRendererOptions.bind(this);
+        this.handleHelperOptions = this.handleHelperOptions.bind(this);
+        this.handleFilterOptions = this.handleFilterOptions.bind(this);
     }
 
     render() {
         const { className, style } = this.props;
 
         return <MenuItem title={L_OPTIONS}>
-            <MenuItem title={L_SURFACE}></MenuItem>
-            <MenuItem title={L_HELPERS}></MenuItem>
+            <MenuItem title={L_SURFACE} onClick={this.handleSurfaceOptions}></MenuItem>
+            <MenuItem title={L_HELPERS} onClick={this.handleRendererOptions}></MenuItem>
             <MenuItemSeparator />
-            <MenuItem title={L_RENDERER}></MenuItem>
-            <MenuItem title={L_FILTER}></MenuItem>
+            <MenuItem title={L_RENDERER} onClick={this.handleHelperOptions}></MenuItem>
+            <MenuItem title={L_FILTER} onClick={this.handleFilterOptions}></MenuItem>
         </MenuItem>;
+    }
+
+    // ---------------------------------- 外观选项 ---------------------------------------
+
+    handleSurfaceOptions() {
+        if (this.optionsWindow === undefined) {
+            this.optionsWindow = new OptionsWindow({
+                app: this.app
+            });
+            this.optionsWindow.render();
+        }
+        this.optionsWindow.show();
+        this.optionsWindow.changeTab(L_SURFACE);
+    }
+
+    // ---------------------------------- 渲染器选项 -------------------------------------
+
+    handleRendererOptions() {
+        if (this.optionsWindow === undefined) {
+            this.optionsWindow = new OptionsWindow({
+                app: this.app
+            });
+            this.optionsWindow.render();
+        }
+        this.optionsWindow.show();
+        this.optionsWindow.changeTab(L_RENDERER);
+    }
+
+    // ------------------------------- 帮助器选项 -----------------------------------------
+
+    handleHelperOptions() {
+        if (this.optionsWindow === undefined) {
+            this.optionsWindow = new OptionsWindow({
+                app: this.app
+            });
+            this.optionsWindow.render();
+        }
+        this.optionsWindow.show();
+        this.optionsWindow.changeTab(L_HELPERS);
+    }
+
+    // ------------------------------ 滤镜选项 ---------------------------------------------
+
+    handleFilterOptions() {
+        if (this.optionsWindow === undefined) {
+            this.optionsWindow = new OptionsWindow({
+                app: this.app
+            });
+            this.optionsWindow.render();
+        }
+        this.optionsWindow.show();
+        this.optionsWindow.changeTab(L_FILTER);
     }
 }
 
