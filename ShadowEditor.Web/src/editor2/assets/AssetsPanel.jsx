88a4f77db0d99e3664b04feb0193877356820a90
tengge1 <930372551@qq.com>
2019-06-28 19:45:05 +0800

    统计数。

--- ShadowEditor.Web/src/editor2/assets/AssetsPanel.jsx
+++ ShadowEditor.Web/src/editor2/assets/AssetsPanel.jsx
@@ -16,39 +16,77 @@ import LogPanel from './LogPanel.jsx';
  * @author tengge / https://github.com/tengge1
  */
 class AssetsPanel extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sceneCount: 0,
+            meshCount: 0,
+            mapCount: 0,
+            materialCount: 0,
+            audioCount: 0,
+            animationCount: 0,
+            particleCount: 0,
+            prefabCount: 0,
+            characterCount: 0,
+        };
+    }
+
     render() {
+        const { sceneCount, meshCount, mapCount, materialCount, audioCount, animationCount, particleCount, prefabCount, characterCount } = this.state;
+
         return <AccordionLayout>
-            <Accordion title={'Scene'} maximizable={true}>
+            <Accordion title={`${L_SCENE}(${sceneCount})`} maximizable={true}>
                 <ScenePanel></ScenePanel>
             </Accordion>
-            <Accordion title={'Model'} maximizable={true}>
+            <Accordion title={`${L_MODEL}(${meshCount})`} maximizable={true}>
                 <ModelPanel></ModelPanel>
             </Accordion>
-            <Accordion title={'Map'} maximizable={true}>
+            <Accordion title={`${L_MAP}(${mapCount})`} maximizable={true}>
                 <MapPanel></MapPanel>
             </Accordion>
-            <Accordion title={'Material'} maximizable={true}>
+            <Accordion title={`${L_MATERIAL}(${materialCount})`} maximizable={true}>
                 <MaterialPanel></MaterialPanel>
             </Accordion>
-            <Accordion title={'Audio'} maximizable={true}>
+            <Accordion title={`${L_AUDIO}(${audioCount})`} maximizable={true}>
                 <AudioPanel></AudioPanel>
             </Accordion>
-            <Accordion title={'Animation'} maximizable={true}>
+            <Accordion title={`${L_ANIMATION}(${animationCount})`} maximizable={true}>
                 <AnimationPanel></AnimationPanel>
             </Accordion>
-            <Accordion title={'Particle'} maximizable={true}>
+            <Accordion title={`${L_PARTICLE}(${particleCount})`} maximizable={true}>
                 <ParticlePanel></ParticlePanel>
             </Accordion>
-            <Accordion title={'Prefab'} maximizable={true}>
+            <Accordion title={`${L_PREFAB}(${prefabCount})`} maximizable={true}>
                 <PrefabPanel></PrefabPanel>
             </Accordion>
-            <Accordion title={'Character'} maximizable={true}>
+            <Accordion title={`${L_CHARACTER}(${characterCount})`} maximizable={true}>
                 <CharacterPanel></CharacterPanel>
             </Accordion>
-            <Accordion title={'Log'} maximizable={true}>
+            <Accordion title={`${L_LOG}`} maximizable={true}>
                 <LogPanel></LogPanel>
             </Accordion>
         </AccordionLayout>;
+    }
+
+    componentDidMount() {
+        fetch(`${app.options.server}/api/Assets/List`).then(response => {
+            if (response.ok) {
+                response.json().then(json => {
+                    this.setState({
+                        sceneCount: json.sceneCount,
+                        meshCount: json.meshCount,
+                        mapCount: json.mapCount,
+                        materialCount: json.materialCount,
+                        audioCount: json.audioCount,
+                        animationCount: json.animationCount,
+                        particleCount: json.particleCount,
+                        prefabCount: json.prefabCount,
+                        characterCount: json.characterCount,
+                    });
+                });
+            }
+        });
     }
 }
 
