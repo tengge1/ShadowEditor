5196c71e39890f7ae1171f59cb84105e59344519
liteng <930372551@qq.com>
2019-06-29 12:06:41 +0800

    修改。

--- ShadowEditor.Web/src/editor2/assets/ScenePanel.jsx
+++ ShadowEditor.Web/src/editor2/assets/ScenePanel.jsx
@@ -1,15 +1,31 @@
 import { classNames, PropTypes, SearchField, ImageList } from '../../third_party';
+import Ajax from '../../utils/Ajax';
+import EditWindow from '../window/EditWindow';
+import Converter from '../../serialization/Converter';
+import GISScene from '../../gis/Scene';
 
 /**
  * 场景面板
  * @author tengge / https://github.com/tengge1
  */
 class ScenePanel extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return <div className={'ScenePanel'}>
             <SearchField></SearchField>
             <ImageList></ImageList>
         </div>;
+    }
+
+    componentDidMount() {
+        Ajax.getJson(`/api/Scene/List`, obj => {
+            this.data = obj.Data;
+            search.setValue('');
+            this.onSearch();
+        });
     }
 }
 
