9cc173749d7adf6ac0a606cca6096265453fa972
tengge1 <930372551@qq.com>
2019-07-04 21:06:29 +0800

    EditWindow

---
+++ ShadowEditor.Web/src/editor2/assets/window/EditWindow.jsx
@@ -0,0 +1,15 @@
+import { classNames, PropTypes } from '../../third_party';
+
+/**
+ * 编辑窗口
+ * @author tengge / https://github.com/tengge1
+ */
+class EditWindow extends React.Component {
+    render() {
+        return <div>
+            Animation Panel
+        </div>;
+    }
+}
+
+export default EditWindow;
