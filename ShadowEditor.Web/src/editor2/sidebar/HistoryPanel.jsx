9daf33fafbab0eae49659baaebb1011d49d2d916
tengge1 <930372551@qq.com>
2019-07-09 20:56:18 +0800

    修复历史面板bug。

--- ShadowEditor.Web/src/editor2/sidebar/HistoryPanel.jsx
+++ ShadowEditor.Web/src/editor2/sidebar/HistoryPanel.jsx
@@ -28,10 +28,10 @@ class HistoryPanel extends React.Component {
             </div>
             <div className={'content'} onClick={this.handleClick}>
                 {undos.map(n => {
-                    return <div className={'undo'} value={n.id} key={n.id}>{n.name}</div>;
+                    return <div className={'undo'} value={n.id} key={n.id} onClick={this.handleClick}>{n.name}</div>;
                 })}
                 {redos.map(n => {
-                    return <div className={'redo'} value={n.id} key={n.id}>{n.name}</div>;
+                    return <div className={'redo'} value={n.id} key={n.id} onClick={this.handleClick}>{n.name}</div>;
                 })}
             </div>
         </div>;
@@ -61,17 +61,22 @@ class HistoryPanel extends React.Component {
             });
         });
 
+        undos.sort((a, b) => { return a.id - b.id; });
+        redos.sort((a, b) => { return a.id - b.id; });
+
         this.setState({ undos, redos });
     }
 
     handleClick(event) {
-        const id = event.target.value;
+        const id = event.target.getAttribute('value');
 
         if (!id) {
             return;
         }
 
-        app.editor.history.goToState(id);
+        app.editor.history.goToState(parseInt(id));
+
+        this.update();
     }
 
     handleClear() {
