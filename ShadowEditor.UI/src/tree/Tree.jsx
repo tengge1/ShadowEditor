208a15503d25f8679f8f703fd8e19feebe752b31
liteng <930372551@qq.com>
2019-07-11 07:33:38 +0800

    拖拽事件。

--- ShadowEditor.UI/src/tree/Tree.jsx
+++ ShadowEditor.UI/src/tree/Tree.jsx
@@ -17,6 +17,12 @@ class Tree extends React.Component {
         this.handleExpandNode = this.handleExpandNode.bind(this, onExpand);
         this.handleClick = this.handleClick.bind(this, onSelect);
         this.handleDoubleClick = this.handleDoubleClick.bind(this, onDoubleClick);
+
+        this.handleDrag = this.handleDrag.bind(this);
+        this.handleDragStart = this.handleDragStart.bind(this);
+        this.handleDragOver = this.handleDragOver.bind(this);
+        this.handleDragLeave = this.handleDragLeave.bind(this);
+        this.handleDrop = this.handleDrop.bind(this);
     }
 
     render() {
@@ -50,7 +56,12 @@ class Tree extends React.Component {
             value={data.value}
             key={data.value}
             onClick={this.handleClick}
-            onDoubleClick={this.handleDoubleClick}>
+            onDoubleClick={this.handleDoubleClick}
+            onDrag={this.handleDrag}
+            onDragStart={this.handleDragStart}
+            onDragOver={this.handleDragOver}
+            onDragLeave={this.handleDragLeave}
+            onDrop={this.handleDrop}>
             <i className={classNames('expand', leaf ? null : (data.expanded ? 'minus' : 'plus'))} value={data.value} onClick={this.handleExpandNode}></i>
             {checkbox}
             <i className={classNames('type', leaf ? 'node' : (data.expanded ? 'open' : 'close'))}></i>
@@ -77,6 +88,93 @@ class Tree extends React.Component {
         var value = event.target.getAttribute('value');
         if (value) {
             onDoubleClick && onDoubleClick(value, event);
+        }
+    }
+
+    // --------------------- 拖拽事件 ---------------------------
+
+    handleDrag(event) {
+        event.stopPropagation();
+        this.currentDrag = event.currentTarget;
+    }
+
+    handleDragStart(event) {
+        event.stopPropagation();
+        event.dataTransfer.setData('text', 'foo');
+    }
+
+    handleDragOver(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var target = event.currentTarget;
+
+        if (target === this.currentDrag) {
+            return;
+        }
+
+        var area = event.offsetY / target.clientHeight;
+
+        if (area < 0.25) {
+            target.classList.add('dragTop');
+        } else if (area > 0.75) {
+            target.classList.add('dragBottom');
+        } else {
+            target.classList.add('drag');
+        }
+    }
+
+    handleDragLeave(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var target = event.currentTarget;
+
+        if (target === this.currentDrag) {
+            return;
+        }
+
+        target.classList.remove('dragTop');
+        target.classList.remove('dragBottom');
+        target.classList.remove('drag');
+    }
+
+    handleDrop(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var target = event.currentTarget;
+
+        if (target === this.currentDrag) {
+            return;
+        }
+
+        target.classList.remove('dragTop');
+        target.classList.remove('dragBottom');
+        target.classList.remove('drag');
+
+        if (typeof (this.onDrag) === 'function') {
+            var area = event.offsetY / target.clientHeight;
+
+            if (area < 0.25) { // 放在当前元素前面
+                this.onDrag(
+                    this.currentDrag.data, // 拖动要素
+                    target.parentNode.parentNode.data, // 新位置父级
+                    target.data, // 新位置索引
+                ); // 拖动, 父级, 索引
+            } else if (area > 0.75) { // 放在当前元素后面
+                this.onDrag(
+                    this.currentDrag.data,
+                    target.parentNode.parentNode.data,
+                    target.nextSibling == null ? null : target.nextSibling.data, // target.nextSibling为null，说明是最后一个位置
+                );
+            } else { // 成为该元素子级
+                this.onDrag(
+                    this.currentDrag.data,
+                    target.data,
+                    null,
+                );
+            }
         }
     }
 }
