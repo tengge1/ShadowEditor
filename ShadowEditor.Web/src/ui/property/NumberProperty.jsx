64016c40dbc110270bfa120ce5342ab994e37536
liteng <930372551@qq.com>
2019-07-15 07:40:13 +0800

    NumberProperty

---
+++ ShadowEditor.Web/src/ui/property/NumberProperty.jsx
@@ -0,0 +1,50 @@
+import './css/NumberProperty.css';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+
+import Input from '../form/Input.jsx';
+
+/**
+ * 数字属性
+ * @author tengge / https://github.com/tengge1
+ */
+class NumberProperty extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this, props.onChange);
+    }
+
+    render() {
+        const { className, style, name, value } = this.props;
+
+        return <Input
+            className={classNames('NumberProperty', className)}
+            style={style}
+            name={name}
+            value={value}
+            onInput={this.handleChange}></Input>;
+    }
+
+    handleChange(onChange, value) {
+        onChange && onChange(value);
+    }
+}
+
+NumberProperty.propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+};
+
+NumberProperty.defaultProps = {
+    className: null,
+    style: null,
+    name: null,
+    value: '',
+    onChange: null,
+};
+
+export default NumberProperty;
