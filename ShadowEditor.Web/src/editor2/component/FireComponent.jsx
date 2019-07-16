55a5465883ff4615dc89e261bf2566c35e8e2546
liteng <930372551@qq.com>
2019-07-16 12:22:02 +0800

    FireComponent

--- ShadowEditor.Web/src/editor2/component/FireComponent.jsx
+++ ShadowEditor.Web/src/editor2/component/FireComponent.jsx
@@ -26,16 +26,17 @@ class FireComponent extends React.Component {
         this.handleUpdate = this.handleUpdate.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handlePreview = this.handlePreview.bind(this);
+        this.onAnimate = this.onAnimate.bind(this);
     }
 
     render() {
         const { show, expanded, width, height, depth, sliceSpacing, previewText } = this.state;
 
         return <PropertyGroup title={L_FIRE_COMPONENT} show={show} expanded={expanded} onExpand={this.handleExpand}>
-            <NumberProperty label={L_WIDTH} value={width} onChange={this.handleChange}></NumberProperty>
-            <NumberProperty label={L_HEIGHT} value={height} onChange={this.handleChange}></NumberProperty>
-            <NumberProperty label={L_DEPTH} value={depth} onChange={this.handleChange}></NumberProperty>
-            <NumberProperty label={L_SLICE_SPACING} value={sliceSpacing} onChange={this.handleChange}></NumberProperty>
+            <NumberProperty label={L_WIDTH} name={'width'} value={width} onChange={this.handleChange}></NumberProperty>
+            <NumberProperty label={L_HEIGHT} name={'height'} value={height} onChange={this.handleChange}></NumberProperty>
+            <NumberProperty label={L_DEPTH} name={'depth'} value={depth} onChange={this.handleChange}></NumberProperty>
+            <NumberProperty label={L_SLICE_SPACING} name={'sliceSpacing'} value={sliceSpacing} onChange={this.handleChange}></NumberProperty>
             <ButtonProperty text={previewText} onChange={this.handlePreview}></ButtonProperty>
         </PropertyGroup>;
     }
@@ -74,11 +75,90 @@ class FireComponent extends React.Component {
     }
 
     handleChange(value, name) {
+        let { width, height, depth, sliceSpacing } = this.state;
 
+        switch (name) {
+            case 'width':
+                width = value;
+                break;
+            case 'height':
+                height = value;
+                break;
+            case 'depth':
+                depth = value;
+                break;
+            case 'sliceSpacing':
+                sliceSpacing = value;
+                break;
+        }
+
+        VolumetricFire.texturePath = 'assets/textures/VolumetricFire/';
+
+        const editor = app.editor;
+
+        let fire = new VolumetricFire(width, height, depth, sliceSpacing, editor.camera);
+
+        fire.mesh.name = this.selected.name;
+        fire.mesh.position.copy(this.selected.position);
+        fire.mesh.rotation.copy(this.selected.rotation);
+        fire.mesh.scale.copy(this.selected.scale);
+
+        Object.assign(fire.mesh.userData, {
+            type: 'Fire',
+            fire,
+            width,
+            height,
+            depth,
+            sliceSpacing,
+        });
+
+        var index = editor.scene.children.indexOf(this.selected);
+        if (index > -1) {
+            editor.scene.children[index] = fire.mesh;
+            fire.mesh.parent = this.selected.parent;
+            this.selected.parent = null;
+            app.call(`objectRemoved`, this, this.selected);
+            app.call(`objectAdded`, this, fire.mesh);
+            editor.select(fire.mesh);
+            app.call('sceneGraphChanged', this.id);
+
+            fire.update(0);
+        }
     }
 
     handlePreview() {
+        if (this.isPlaying) {
+            this.stopPreview();
+        } else {
+            this.startPreview();
+        }
+    }
 
+    startPreview() {
+        this.isPlaying = true;
+
+        this.setState({
+            previewText: L_CANCEL,
+        });
+
+        app.on(`animate.${this.id}`, this.onAnimate);
+    }
+
+    stopPreview() {
+        this.isPlaying = false;
+
+        this.setState({
+            previewText: L_PREVIEW,
+        });
+
+        app.on(`animate.${this.id}`, null);
+    }
+
+    onAnimate(clock, deltaTime) {
+        var elapsed = clock.getElapsedTime();
+
+        var fire = this.selected.userData.fire;
+        fire.update(elapsed);
     }
 }
 
