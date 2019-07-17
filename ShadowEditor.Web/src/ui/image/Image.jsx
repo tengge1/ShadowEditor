873233b96a520d9c516eb75eba5b048fde670eac
liteng <930372551@qq.com>
2019-07-18 07:43:50 +0800

    修复bug。

--- ShadowEditor.Web/src/ui/image/Image.jsx
+++ ShadowEditor.Web/src/ui/image/Image.jsx
@@ -7,6 +7,12 @@ import PropTypes from 'prop-types';
  * @author tengge / https://github.com/tengge1
  */
 class Image extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleError = this.handleError.bind(this);
+    }
+
     render() {
         const { className, style, src, title } = this.props;
 
@@ -14,7 +20,24 @@ class Image extends React.Component {
             className={classNames('Image', className)}
             style={style}
             src={src}
-            title={title}></img>;
+            title={title}
+            onError={this.handleError}></img>;
+    }
+
+    handleError(event) {
+        let target = event.target;
+        let parent = target.parentNode;
+        parent.removeChild(target);
+
+        let img = document.createElement('div');
+        img.className = 'no-img';
+
+        let icon = document.createElement('i');
+        icon.className = 'Icon iconfont icon-scenes';
+        img.appendChild(icon);
+
+        let title = parent.children[0];
+        parent.insertBefore(img, title);
     }
 }
 
