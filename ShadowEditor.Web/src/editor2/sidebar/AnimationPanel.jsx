e43cf20fb4bac61f52e43780b7ae98a56666e85f
tengge1 <930372551@qq.com>
2019-07-28 15:18:55 +0800

    AnimationPanel

--- ShadowEditor.Web/src/editor2/sidebar/AnimationPanel.jsx
+++ ShadowEditor.Web/src/editor2/sidebar/AnimationPanel.jsx
@@ -1,5 +1,7 @@
 import './css/AnimationPanel.css';
 import { classNames, PropTypes } from '../../third_party';
+// import BasicAnimationComponent from '../../component/animation/BasicAnimationComponent';
+// import TweenAnimationComponent from '../../component/animation/TweenAnimationComponent';
 
 /**
  * 动画面板
@@ -11,6 +13,15 @@ class AnimationPanel extends React.Component {
     }
 
     render() {
+        // var data = {
+        //     xtype: 'div',
+        //     parent: this.parent,
+        //     children: [
+        //         new BasicAnimationComponent({ app: app }),
+        //         new TweenAnimationComponent({ app: app }),
+        //     ]
+        // };
+
         return <div>AnimationPanel</div>;
     }
 }
