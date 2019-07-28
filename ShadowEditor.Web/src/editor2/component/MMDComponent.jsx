c55d73c9d35fc7d4c81c841c326f622e89a28ed3
tengge1 <930372551@qq.com>
2019-07-28 10:50:24 +0800

    MMDComponent

--- ShadowEditor.Web/src/editor2/component/MMDComponent.jsx
+++ ShadowEditor.Web/src/editor2/component/MMDComponent.jsx
@@ -17,15 +17,9 @@ class MMDComponent extends React.Component {
             show: false,
             expanded: true,
 
-            reflect: false,
-            showColor: false,
-            color: '#ffffff',
-            showSize: false,
-            size: '1024',
-            showClipBias: false,
-            clipBias: 0,
-            showRecursion: false,
-            recursion: false,
+            animation: null,
+            cameraAnimation: null,
+            audio: null,
         };
 
         this.handleExpand = this.handleExpand.bind(this);
@@ -40,24 +34,22 @@ class MMDComponent extends React.Component {
     }
 
     render() {
-        const { show, expanded, reflect, showColor, color, showSize, size, showClipBias, clipBias, showRecursion, recursion } = this.state;
+        const { show, expanded, animation, cameraAnimation, audio } = this.state;
 
         if (!show) {
             return null;
         }
 
         return <PropertyGroup title={L_MMD_MODEL} show={show} expanded={expanded} onExpand={this.handleExpand}>
-            <CheckBoxProperty label={L_REFLECT} name={'reflect'} value={reflect} onChange={this.handleChange}></CheckBoxProperty>
-            <ColorProperty label={L_COLOR} name={'color'} value={color} show={showColor} onChange={this.handleChange}></ColorProperty>
-            <SelectProperty label={L_TEXTURE_SIZE} name={'size'} options={this.sizes} value={size} show={showSize} onChange={this.handleChange}></SelectProperty>
-            <NumberProperty label={L_CLIP_BIAS} name={'clipBias'} value={clipBias} show={showClipBias} onChange={this.handleChange}></NumberProperty>
-            <CheckBoxProperty label={L_RECURSION} name={'recursion'} value={recursion} show={showRecursion} onChange={this.handleChange}></CheckBoxProperty>
+            <DisplayField label={L_MODEL_ANIMATION} name={'animation'} value={animation} btnShow={true} btnText={L_SELECT} onChange={this.handleSelectAnimation}></DisplayField>
+            <DisplayField label={L_CAMERA_ANIMATION} name={'cameraAnimation'} value={cameraAnimation} btnShow={true} btnText={L_SELECT} onChange={this.handleSelectCameraAnimation}></DisplayField>
+            <DisplayField label={L_AUDIO} name={'audio'} value={audio} btnShow={true} btnText={L_SELECT} onChange={this.handleSelectAudio}></DisplayField>
         </PropertyGroup>;
     }
 
     componentDidMount() {
-        app.on(`objectSelected.ReflectorComponent`, this.handleUpdate);
-        app.on(`objectChanged.ReflectorComponent`, this.handleUpdate);
+        app.on(`objectSelected.MMDComponent`, this.handleUpdate);
+        app.on(`objectChanged.MMDComponent`, this.handleUpdate);
     }
 
     handleExpand(expanded) {
@@ -69,7 +61,7 @@ class MMDComponent extends React.Component {
     handleUpdate() {
         const editor = app.editor;
 
-        if (!editor.selected || !(editor.selected instanceof THREE.Mesh)) {
+        if (!editor.selected || !(editor.selected.userData.Type === 'pmd' || editor.selected.userData.Type === 'pmx')) {
             this.setState({
                 show: false,
             });
@@ -80,29 +72,10 @@ class MMDComponent extends React.Component {
 
         let state = {
             show: true,
+            animation: this.selected.userData.Animation ? this.selected.userData.Animation.Name : null,
+            cameraAnimation: this.selected.userData.CameraAnimation ? this.selected.userData.CameraAnimation.Name : null,
+            audio: this.selected.userData.Audio ? this.selected.userData.Audio.Name : null,
         };
-
-        if (this.selected instanceof THREE.Reflector) {
-            Object.assign(state, {
-                reflect: true,
-                showColor: true,
-                color: this.selected.userData.color,
-                showSize: true,
-                size: this.selected.userData.size,
-                showClipBias: true,
-                clipBias: this.selected.userData.clipBias,
-                showRecursion: true,
-                recursion: this.selected.userData.recursion,
-            });
-        } else {
-            Object.assign(state, {
-                reflect: false,
-                showColor: false,
-                showSize: false,
-                showClipBias: false,
-                showRecursion: false,
-            });
-        }
 
         this.setState(state);
     }
@@ -111,47 +84,49 @@ class MMDComponent extends React.Component {
 
     handleSelectAnimation() {
         app.call(`selectBottomPanel`, this, 'animation');
-        app.msg(L_CLICK_ANIMATION_PANEL);
+        app.toast(L_CLICK_ANIMATION_PANEL);
         app.on(`selectAnimation.${this.id}`, this.onSelectAnimation);
     }
 
     onSelectAnimation(data) {
         if (data.Type !== 'mmd') {
-            UI.msg(L_SELECT_MMD_ANIMATION_ONLY);
+            app.toast(L_SELECT_MMD_ANIMATION_ONLY);
             return;
         }
         app.on(`selectAnimation.${this.id}`, null);
 
         this.selected.userData.Animation = {};
         Object.assign(this.selected.userData.Animation, data);
-        this.updateUI();
+
+        app.call(`objectChanged`, this, this.selected);
     }
 
     // ---------------------------- 相机动画 -------------------------------------------
 
     handleSelectCameraAnimation() {
         app.call(`selectBottomPanel`, this, 'animation');
-        UI.msg(L_CLICK_CAMERA_ANIMATION);
+        app.toast(L_CLICK_CAMERA_ANIMATION);
         app.on(`selectAnimation.${this.id}`, this.onSelectCameraAnimation);
     }
 
     onSelectCameraAnimation(data) {
         if (data.Type !== 'mmd') {
-            UI.msg(L_SELECT_CAMERA_ANIMATION_ONLY);
+            app.toast(L_SELECT_CAMERA_ANIMATION_ONLY);
             return;
         }
         app.on(`selectAnimation.${this.id}`, null);
 
         this.selected.userData.CameraAnimation = {};
         Object.assign(this.selected.userData.CameraAnimation, data);
-        this.updateUI();
+
+        app.call(`objectChanged`, this, this.selected);
     }
 
     // ------------------------------ MMD音乐 --------------------------------------------
 
     handleSelectAudio() {
         app.call(`selectBottomPanel`, this, 'audio');
-        UI.msg(L_SELECT_MMD_AUDIO);
+        app.toast(L_SELECT_MMD_AUDIO);
         app.on(`selectAudio.${this.id}`, this.onSelectAudio.bind(this));
     }
 
@@ -160,7 +135,8 @@ class MMDComponent extends React.Component {
 
         this.selected.userData.Audio = {};
         Object.assign(this.selected.userData.Audio, data);
-        this.updateUI();
+
+        app.call(`objectChanged`, this, this.selected);
     }
 }
 
