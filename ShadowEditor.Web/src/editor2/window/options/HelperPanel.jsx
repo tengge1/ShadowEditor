8a6323170fa82ec8c9c6c273c0cf7ba0799c3a31
liteng <930372551@qq.com>
2019-08-06 12:06:52 +0800

    HelperPanel

--- ShadowEditor.Web/src/editor2/window/options/HelperPanel.jsx
+++ ShadowEditor.Web/src/editor2/window/options/HelperPanel.jsx
@@ -2,25 +2,22 @@ import './css/HelperPanel.css';
 import { classNames, PropTypes, Window, Content, TabLayout, Buttons, Button, Form, FormControl, Label, Input, Select, CheckBox } from '../../../third_party';
 
 /**
- * 渲染器窗口
+ * 帮助选项窗口
  * @author tengge / https://github.com/tengge1
  */
 class HelperPanel extends React.Component {
     constructor(props) {
         super(props);
 
-        this.shadowMapType = {
-            [-1]: L_DISABLED,
-            [THREE.BasicShadowMap]: L_BASIC_SHADOW, // 0
-            [THREE.PCFShadowMap]: L_PCF_SHADOW, // 1
-            [THREE.PCFSoftShadowMap]: L_PCF_SOFT_SHADOW // 2
-        };
-
         this.state = {
-            shadowMapType: -1,
-            gammaInput: 0,
-            gammaOutput: 0,
-            gammaFactor: 0,
+            showGrid: false,
+            showCamera: false,
+            showPointLight: false,
+            showDirectionalLight: false,
+            showSpotLight: false,
+            showHemisphereLight: false,
+            showRectAreaLight: false,
+            showSkeleton: false,
         };
 
         this.handleUpdate = this.handleUpdate.bind(this);
@@ -28,36 +25,54 @@ class HelperPanel extends React.Component {
     }
 
     render() {
-        const { shadowMapType, gammaInput, gammaOutput, gammaFactor } = this.state;
+        const { showGrid, showCamera, showPointLight, showDirectionalLight, showSpotLight, showHemisphereLight, showRectAreaLight, showSkeleton } = this.state;
 
         return <Form>
             <FormControl>
-                <Label>{L_SHADOW}</Label>
-                <Select options={this.shadowMapType} name={'shadowMapType'} value={shadowMapType} onChange={this.handleChange}></Select>
+                <Label>{L_GRID}</Label>
+                <CheckBox name={'showGrid'} value={showGrid} onChange={this.handleChange}></CheckBox>
             </FormControl>
             <FormControl>
-                <Label>{L_GAMMA_INPUT}</Label>
-                <CheckBox name={'gammaInput'} value={gammaInput} onChange={this.handleChange}></CheckBox>
+                <Label>{L_CAMERA}</Label>
+                <CheckBox name={'showCamera'} value={showCamera} onChange={this.handleChange}></CheckBox>
             </FormControl>
             <FormControl>
-                <Label>{L_GAMMA_OUTPUT}</Label>
-                <CheckBox name={'gammaOutput'} value={gammaOutput} onChange={this.handleChange}></CheckBox>
+                <Label>{L_POINT_LIGHT}</Label>
+                <CheckBox name={'showPointLight'} value={showPointLight} onChange={this.handleChange}></CheckBox>
             </FormControl>
             <FormControl>
-                <Label>{L_GAMMA_FACTOR}</Label>
-                <Input type={'number'} name={'gammaFactor'} value={gammaFactor} onChange={this.handleChange}></Input>
+                <Label>{L_DIRECTIONAL_LIGHT}</Label>
+                <CheckBox name={'showDirectionalLight'} value={showDirectionalLight} onChange={this.handleChange}></CheckBox>
+            </FormControl>
+            <FormControl>
+                <Label>{L_SPOT_LIGHT}</Label>
+                <CheckBox name={'showSpotLight'} value={showSpotLight} onChange={this.handleChange}></CheckBox>
+            </FormControl>
+            <FormControl>
+                <Label>{L_HEMISPHERE_LIGHT}</Label>
+                <CheckBox name={'showHemisphereLight'} value={showHemisphereLight} onChange={this.handleChange}></CheckBox>
+            </FormControl>
+            <FormControl>
+                <Label>{L_RECT_AREA_LIGHT}</Label>
+                <CheckBox name={'showRectAreaLight'} value={showRectAreaLight} onChange={this.handleChange}></CheckBox>
+            </FormControl>
+            <FormControl>
+                <Label>{L_SKELETON}</Label>
+                <CheckBox name={'showSkeleton'} value={showSkeleton} onChange={this.handleChange}></CheckBox>
             </FormControl>
         </Form>;
     }
 
     handleUpdate() {
-        const renderer = app.editor.renderer;
-
         this.setState({
-            shadowMapType: renderer.shadowMap.enabled ? renderer.shadowMap.type : -1,
-            gammaInput: renderer.gammaInput,
-            gammaOutput: renderer.gammaOutput,
-            gammaFactor: renderer.gammaFactor,
+            showGrid: app.storage.get('showGrid') === true,
+            showCamera: app.storage.get('showCamera') === true,
+            showPointLight: app.storage.get('showPointLight') === true,
+            showDirectionalLight: app.storage.get('showDirectionalLight') === true,
+            showSpotLight: app.storage.get('showSpotLight') === true,
+            showHemisphereLight: app.storage.get('showHemisphereLight') === true,
+            showRectAreaLight: app.storage.get('showRectAreaLight') === true,
+            showSkeleton: app.storage.get('showSkeleton') === true,
         });
     }
 
@@ -69,31 +84,50 @@ class HelperPanel extends React.Component {
             return;
         }
 
-        let renderer = app.editor.renderer;
+        const { showGrid, showCamera, showPointLight, showDirectionalLight,
+            showSpotLight, showHemisphereLight, showRectAreaLight, showSkeleton } = Object.assign({}, this.state, {
+                [name]: value,
+            });
 
-        const { shadowMapType, gammaInput, gammaOutput, gammaFactor } = Object.assign({}, this.state, {
-            [name]: value,
-        });
-
-        if (shadowMapType === -1) {
-            renderer.shadowMap.enabled = false;
-        } else {
-            renderer.shadowMap.enabled = true;
-            renderer.shadowMap.type = shadowMapType;
+        if (showGrid !== app.storage.get('showGrid')) {
+            app.storage.set('showGrid', showGrid);
+            app.call(`storageChanged`, this, 'showGrid', showGrid);
         }
 
-        renderer.gammaInput = gammaInput;
-        renderer.gammaOutput = gammaOutput;
-        renderer.gammaFactor = gammaFactor;
+        if (showCamera !== app.storage.get('showCamera')) {
+            app.storage.set('showCamera', showCamera);
+            app.call(`storageChanged`, this, 'showCamera', showCamera);
+        }
 
-        renderer.dispose();
+        if (showPointLight !== app.storage.get('showPointLight')) {
+            app.storage.set('showPointLight', showPointLight);
+            app.call(`storageChanged`, this, 'showPointLight', showPointLight);
+        }
 
-        Object.assign(app.options, {
-            shadowMapType,
-            gammaInput,
-            gammaOutput,
-            gammaFactor
-        });
+        if (showDirectionalLight !== app.storage.get('showDirectionalLight')) {
+            app.storage.set('showDirectionalLight', showDirectionalLight);
+            app.call(`storageChanged`, this, 'showDirectionalLight', showDirectionalLight);
+        }
+
+        if (showSpotLight !== app.storage.get('showSpotLight')) {
+            app.storage.set('showSpotLight', showSpotLight);
+            app.call(`storageChanged`, this, 'showSpotLight', showSpotLight);
+        }
+
+        if (showHemisphereLight !== app.storage.get('showHemisphereLight')) {
+            app.storage.set('showHemisphereLight', showHemisphereLight);
+            app.call(`storageChanged`, this, 'showHemisphereLight', showHemisphereLight);
+        }
+
+        if (showRectAreaLight !== app.storage.get('showRectAreaLight')) {
+            app.storage.set('showRectAreaLight', showRectAreaLight);
+            app.call(`storageChanged`, this, 'showRectAreaLight', showRectAreaLight);
+        }
+
+        if (showSkeleton !== app.storage.get('showSkeleton')) {
+            app.storage.set('showSkeleton', showSkeleton);
+            app.call(`storageChanged`, this, 'showSkeleton', showSkeleton);
+        }
     }
 }
 
