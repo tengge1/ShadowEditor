af31fc11a2a14f486b55a3f5e0df2b6d44180c31
tengge1 <930372551@qq.com>
2019-08-07 20:15:15 +0800

    修复火狐样式bug。

--- ShadowEditor.Web/src/ui/image/ImageList.jsx
+++ ShadowEditor.Web/src/ui/image/ImageList.jsx
@@ -65,7 +65,7 @@ class ImageList extends React.Component {
                 <IconButton icon={'right-triangle2'} title={nextPageText} onClick={this.handleNextPage}></IconButton>
                 <IconButton icon={'forward'} title={lastPageText} onClick={this.handleLastPage}></IconButton>
                 <div className={'info'}>
-                    共<span>{totalPage}</span>页
+                    共{totalPage}页
                 </div>
             </div>
         </div>;
