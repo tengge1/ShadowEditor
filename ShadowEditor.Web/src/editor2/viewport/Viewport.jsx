1d25886651c2c06f4a2dbc0658c1d663885bd8d1
tengge1 <930372551@qq.com>
2019-08-09 20:46:23 +0800

    编辑器添加性能控件。

--- ShadowEditor.Web/src/editor2/viewport/Viewport.jsx
+++ ShadowEditor.Web/src/editor2/viewport/Viewport.jsx
@@ -11,13 +11,14 @@ class Viewport extends React.Component {
     constructor(props) {
         super(props);
 
+        this.viewportRef = React.createRef();
         this.editorRef = React.createRef();
         this.svgRef = React.createRef();
         this.playerRef = React.createRef();
     }
 
     render() {
-        return <div className={'Viewport'}>
+        return <div className={'Viewport'} ref={this.viewportRef}>
             <div className={'editor'} ref={this.editorRef}></div>
             <svg className={'svg'} ref={this.svgRef}></svg>
             <ScriptEditorPanel></ScriptEditorPanel>
@@ -26,9 +27,22 @@ class Viewport extends React.Component {
     }
 
     componentDidMount() {
+        app.viewportRef = this.viewportRef.current;
         app.editorRef = this.editorRef.current;
         app.svgRef = this.svgRef.current;
         app.playerRef = this.playerRef.current;
+
+        // 性能控件
+        app.stats = new Stats();
+
+        Object.assign(app.stats.dom.style, {
+            position: 'absolute',
+            left: '8px',
+            top: '8px',
+            zIndex: 'initial',
+        });
+
+        app.viewportRef.appendChild(app.stats.dom);
 
         app.viewport = this.editorRef.current;
         app.player = new Player(this.playerRef.current, {
