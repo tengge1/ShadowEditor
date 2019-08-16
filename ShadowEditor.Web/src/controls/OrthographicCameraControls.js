851c856bcbab1b23d22b8f313a6f7d651abb9352
tengge1 <930372551@qq.com>
2019-08-16 21:58:37 +0800

    正视图平移。

--- ShadowEditor.Web/src/controls/OrthographicCameraControls.js
+++ ShadowEditor.Web/src/controls/OrthographicCameraControls.js
@@ -1,5 +1,6 @@
 /**
  * 正交相机控制器
+ * @author tengge / https://github.com/tengge1
  * @param {THREE.OrthographicCamera} camera 正交相机
  * @param {HTMLElement} domElement DOM
  */
@@ -7,15 +8,70 @@ function OrthographicCameraControls(camera, domElement) {
     this.camera = camera;
     this.domElement = domElement;
 
+    this.enabled = false;
+    this.isDown = false;
+    this.offsetXY = new THREE.Vector2();
+
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
     this.onMouseWheel = this.onMouseWheel.bind(this);
 }
 
 OrthographicCameraControls.prototype.enable = function () {
-    app.on(`mousewheel.${this.id}`, this.onMouseWheel);
+    if (this.enabled) {
+        return;
+    }
+
+    this.enabled = true;
+
+    this.domElement.addEventListener('mousedown', this.onMouseDown);
+    this.domElement.addEventListener('mousemove', this.onMouseMove);
+    this.domElement.addEventListener('mouseup', this.onMouseUp);
+    this.domElement.addEventListener('mousewheel', this.onMouseWheel);
 };
 
 OrthographicCameraControls.prototype.disable = function () {
-    app.on(`mousewheel.${this.id}`, null);
+    if (!this.enabled) {
+        return;
+    }
+
+    this.enabled = false;
+
+    this.domElement.removeEventListener('mousedown', this.onMouseDown);
+    this.domElement.removeEventListener('mousemove', this.onMouseMove);
+    this.domElement.removeEventListener('mouseup', this.onMouseUp);
+    this.domElement.removeEventListener('mousewheel', this.onMouseWheel);
+};
+
+OrthographicCameraControls.prototype.onMouseDown = function (event) {
+    this.isDown = true;
+
+    this.offsetXY.set(event.offsetX, event.offsetY);
+};
+
+OrthographicCameraControls.prototype.onMouseMove = function (event) {
+    if (!this.isDown) {
+        return;
+    }
+
+    let camera = this.camera;
+
+    let dx = event.offsetX - this.offsetXY.x;
+    let dy = event.offsetY - this.offsetXY.y;
+
+    this.camera.left -= dx;
+    this.camera.right -= dx;
+    this.camera.top += dy;
+    this.camera.bottom += dy;
+
+    this.camera.updateProjectionMatrix();
+
+    this.offsetXY.set(event.offsetX, event.offsetY);
+};
+
+OrthographicCameraControls.prototype.onMouseUp = function (event) {
+    this.isDown = false;
 };
 
 OrthographicCameraControls.prototype.onMouseWheel = function (event) {
