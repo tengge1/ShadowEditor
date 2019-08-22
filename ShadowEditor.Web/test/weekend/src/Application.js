221bdbc17f0853e35e2d2d5c2260959ef601ab06
liteng <930372551@qq.com>
2019-08-23 07:50:47 +0800

    Application

--- ShadowEditor.Web/test/weekend/src/Application.js
+++ ShadowEditor.Web/test/weekend/src/Application.js
@@ -1,6 +1,33 @@
+/**
+ * 应用程序
+ */
 class Application {
-    start() {
+    constructor(canvas) {
+        canvas.style.width = '200px';
+        canvas.style.height = '200px';
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
 
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+    }
+
+    start() {
+        const {
+            canvas,
+            context
+        } = this;
+
+        let imgData = context.createImageData(200, 200);
+
+        for (let i = 0; i < imgData.data.length; i += 4) {
+            imgData.data[i + 0] = 255;
+            imgData.data[i + 1] = 0;
+            imgData.data[i + 2] = 0;
+            imgData.data[i + 3] = 255;
+        }
+
+        context.putImageData(imgData, 0, 0);
     }
 }
 
