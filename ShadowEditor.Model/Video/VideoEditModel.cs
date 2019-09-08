329cd90192bf8e5659063197685cf0550589b20c
tengge1 <930372551@qq.com>
2019-09-08 08:25:19 +0800

    截图和视频模型。

---
+++ ShadowEditor.Model/Video/VideoEditModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowEditor.Model.Screenshot
+{
+    /// <summary>
+    /// 视频编辑模型
+    /// </summary>
+    public class VideoEditModel
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
