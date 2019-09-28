b219eb4ff6e4f01954ea36dfe1fe319b6a190f7d
tengge1 <930372551@qq.com>
2019-09-28 20:01:16 +0800

    注册和登录按钮。

--- ShadowEditor.Web/src/editor/menu/LoginMenu.jsx
+++ ShadowEditor.Web/src/editor/menu/LoginMenu.jsx
@@ -1,22 +1,36 @@
-import { classNames, PropTypes, MenuBar, MenuItem } from '../../third_party';
+import './css/LoginMenu.css';
+import { classNames, MenuItemSeparator, Button } from '../../third_party';
 import LoginWindow from '../system/LoginWindow.jsx';
 
 /**
- * 状态菜单
+ * 登录菜单
  * @author tengge / https://github.com/tengge1
  */
 class LoginMenu extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handleClick = this.handleClick.bind(this);
+        this.handleClickRegister = this.handleClickRegister.bind();
+        this.handleClickLogin = this.handleClickLogin.bind(this);
     }
 
     render() {
-        return <MenuItem title={_t(`Login`)} onClick={this.handleClick}></MenuItem>;
+        return <>
+            <MenuItemSeparator className={classNames('horizontal', 'LoginSeparator')}></MenuItemSeparator>
+            <li className={classNames('MenuItem', 'LoginMenuItem')}>
+                <Button className={'button'} onClick={this.handleClickRegister}>{_t(`Register`)}</Button>
+            </li>
+            <li className={classNames('MenuItem', 'LoginMenuItem')}>
+                <Button className={'button'} onClick={this.handleClickLogin}>{_t(`Login`)}</Button>
+            </li>
+        </>;
     }
 
-    handleClick() {
+    handleClickRegister() {
+
+    }
+
+    handleClickLogin() {
         const win = app.createElement(LoginWindow);
         app.addElement(win);
     }
