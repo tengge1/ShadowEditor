e061b116f199eea1955eec39207a2e29301cb085
tengge1 <930372551@qq.com>
2019-10-23 20:04:14 +0800

    添加组织机构。

--- ShadowEditor.Server/Controllers/System/DepartmentController.cs
+++ ShadowEditor.Server/Controllers/System/DepartmentController.cs
@@ -25,80 +25,26 @@ namespace ShadowEditor.Server.Controllers.System
         /// <summary>
         /// 获取列表
         /// </summary>
-        /// <param name="pageSize"></param>
-        /// <param name="pageNum"></param>
-        /// <param name="keyword"></param>
         /// <returns></returns>
         [HttpGet]
-        [Authority(OperatingAuthority.LIST_USER)]
-        public JsonResult List(int pageSize = 20, int pageNum = 1, string keyword = "")
+        public JsonResult List()
         {
             var mongo = new MongoHelper();
 
-            // 获取所有角色
-            var roleDocs = mongo.FindAll(Constant.RoleCollectionName).ToList();
+            var filter = Builders<BsonDocument>.Filter.Empty;
 
-            var roles = new List<RoleModel>();
+            var docs = mongo.FindAll(Constant.DepartmentCollectionName).ToList();
 
-            foreach (var doc in roleDocs)
-            {
-                roles.Add(new RoleModel
-                {
-                    ID = doc["ID"].ToString(),
-                    Name = doc["Name"].ToString(),
-                    CreateTime = doc["CreateTime"].ToLocalTime(),
-                    UpdateTime = doc["UpdateTime"].ToLocalTime(),
-                    Description = doc.Contains("Description") ? doc["Description"].ToString() : "",
-                    Status = doc["Status"].ToInt32(),
-                });
-            }
-
-            // 获取用户
-            var filter = Builders<BsonDocument>.Filter.Ne("Status", -1);
-
-            if (!string.IsNullOrEmpty(keyword))
-            {
-                var filter1 = Builders<BsonDocument>.Filter.Regex("Name", keyword);
-                filter = Builders<BsonDocument>.Filter.And(filter, filter1);
-            }
-
-            var sort = Builders<BsonDocument>.Sort.Descending("_id");
-
-            var total = mongo.Count(Constant.UserCollectionName, filter);
-            var docs = mongo.FindMany(Constant.UserCollectionName, filter)
-                .Sort(sort)
-                .Skip(pageSize * (pageNum - 1))
-                .Limit(pageSize)
-                .ToList();
-
-            var rows = new List<UserModel>();
+            var list = new List<DepartmentModel>();
 
             foreach (var doc in docs)
             {
-                var roleID = doc.Contains("RoleID") ? doc["RoleID"].ToString() : "";
-                var roleName = "";
-
-                var role = roles.Where(n => n.ID == roleID).FirstOrDefault();
-                if (role != null)
-                {
-                    roleName = role.Name;
-                }
-
-                rows.Add(new UserModel
+                list.Add(new DepartmentModel
                 {
                     ID = doc["ID"].ToString(),
-                    Username = doc["Username"].ToString(),
-                    Password = "",
+                    ParentID = doc["ParentID"].ToString(),
                     Name = doc["Name"].ToString(),
-                    RoleID = doc.Contains("RoleID") ? doc["RoleID"].ToString() : "",
-                    RoleName = roleName,
-                    Gender = doc["Gender"].ToInt32(),
-                    Phone = doc["Phone"].ToString(),
-                    Email = doc["Email"].ToString(),
-                    QQ = doc["QQ"].ToString(),
-                    CreateTime = doc["CreateTime"].ToLocalTime(),
-                    UpdateTime = doc["UpdateTime"].ToLocalTime(),
-                    Status = doc["Status"].ToInt32(),
+                    AdministratorID = doc["AdministratorID"].ToString()
                 });
             }
 
@@ -106,11 +52,7 @@ namespace ShadowEditor.Server.Controllers.System
             {
                 Code = 200,
                 Msg = "Get Successfully!",
-                Data = new
-                {
-                    total,
-                    rows,
-                },
+                Data = list
             });
         }
 
@@ -120,27 +62,8 @@ namespace ShadowEditor.Server.Controllers.System
         /// <param name="model"></param>
         /// <returns></returns>
         [HttpPost]
-        [Authority(OperatingAuthority.ADD_USER)]
-        public JsonResult Add(UserEditModel model)
+        public JsonResult Add(DepartmentEditModel model)
         {
-            if (string.IsNullOrEmpty(model.Username))
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "Username is not allowed to be empty.",
-                });
-            }
-
-            if (string.IsNullOrEmpty(model.Password))
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "Password is not allowed to be empty.",
-                });
-            }
-
             if (string.IsNullOrEmpty(model.Name))
             {
                 return Json(new
@@ -150,48 +73,17 @@ namespace ShadowEditor.Server.Controllers.System
                 });
             }
 
-            if (string.IsNullOrEmpty(model.RoleID))
-            {
-                model.RoleID = "";
-            }
-
             var mongo = new MongoHelper();
-
-            var filter = Builders<BsonDocument>.Filter.Eq("Username", model.Username);
-
-            var count = mongo.Count(Constant.UserCollectionName, filter);
-
-            if (count > 0)
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "The username is already existed.",
-                });
-            }
-
-            var now = DateTime.Now;
-
-            var salt = DateTime.Now.ToString("yyyyMMddHHmmss");
 
             var doc = new BsonDocument
             {
                 ["ID"] = ObjectId.GenerateNewId(),
-                ["Username"] = model.Username,
-                ["Password"] = MD5Helper.Encrypt(model.Password + salt),
+                ["ParentID"] = model.ParentID,
                 ["Name"] = model.Name,
-                ["RoleID"] = model.RoleID,
-                ["Gender"] = 0,
-                ["Phone"] = "",
-                ["Email"] = "",
-                ["QQ"] = "",
-                ["CreateTime"] = now,
-                ["UpdateTime"] = now,
-                ["Salt"] = salt,
-                ["Status"] = 0,
+                ["AdministratorID"] = model.AdministratorID
             };
 
-            mongo.InsertOne(Constant.UserCollectionName, doc);
+            mongo.InsertOne(Constant.DepartmentCollectionName, doc);
 
             return Json(new
             {
@@ -206,7 +98,6 @@ namespace ShadowEditor.Server.Controllers.System
         /// <param name="model"></param>
         /// <returns></returns>
         [HttpPost]
-        [Authority(OperatingAuthority.EDIT_USER)]
         public JsonResult Edit(UserEditModel model)
         {
             var objectId = ObjectId.GenerateNewId();
@@ -309,7 +200,6 @@ namespace ShadowEditor.Server.Controllers.System
         /// <param name="ID"></param>
         /// <returns></returns>
         [HttpPost]
-        [Authority(OperatingAuthority.DELETE_USER)]
         public JsonResult Delete(string ID)
         {
             var objectId = ObjectId.GenerateNewId();
@@ -356,93 +246,6 @@ namespace ShadowEditor.Server.Controllers.System
             {
                 Code = 200,
                 Msg = "Delete successfully!"
-            });
-        }
-
-        /// <summary>
-        /// 修改密码
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        [HttpPost]
-        public JsonResult ChangePassword(ChangePasswordModel model)
-        {
-            var user = UserHelper.GetCurrentUser();
-
-            if (user == null)
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "The user is not existed."
-                });
-            }
-
-            // 验证密码是否为空
-            if (model.OldPassword == null || model.OldPassword.Trim() == "")
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "Old password is not allowed to be empty."
-                });
-            }
-
-            if (model.NewPassword == null || model.NewPassword.Trim() == "")
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "New password is not allowed to be empty."
-                });
-            }
-
-            if (model.ConfirmPassword == null || model.ConfirmPassword.Trim() == "")
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "Confirm password is not allowed to be empty."
-                });
-            }
-
-            if (model.NewPassword != model.ConfirmPassword)
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "New password and confirm password is not the same."
-                });
-            }
-
-            // 验证旧密码
-            var oldPassword = MD5Helper.Encrypt(model.OldPassword + user.Salt);
-
-            if (oldPassword != user.Password)
-            {
-                return Json(new
-                {
-                    Code = 300,
-                    Msg = "Old password is not correct."
-                });
-            }
-
-            // 修改密码
-            var salt = DateTime.Now.ToString("yyyyMMddHHmmss");
-            var password = MD5Helper.Encrypt(model.NewPassword + salt);
-
-            var filter = Builders<BsonDocument>.Filter.Eq("ID", ObjectId.Parse(user.ID));
-            var update1 = Builders<BsonDocument>.Update.Set("Password", password);
-            var update2 = Builders<BsonDocument>.Update.Set("Salt", salt);
-            var update = Builders<BsonDocument>.Update.Combine(update1, update2);
-
-            var mongo = new MongoHelper();
-            mongo.UpdateOne(Constant.UserCollectionName, filter, update);
-
-            return Json(new
-            {
-                Code = 200,
-                Msg = "Password changed successfully!"
             });
         }
     }
