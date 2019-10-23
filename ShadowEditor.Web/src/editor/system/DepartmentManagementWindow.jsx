68c13d0dc41a22f9fe308e84d83cd09bb16221ad
tengge1 <930372551@qq.com>
2019-10-23 20:33:24 +0800

    添加方法。

--- ShadowEditor.Web/src/editor/system/DepartmentManagementWindow.jsx
+++ ShadowEditor.Web/src/editor/system/DepartmentManagementWindow.jsx
@@ -10,21 +10,19 @@ class DepartmentManagementWindow extends React.Component {
         super(props);
 
         this.state = {
-            roles: [],
-            authorities: [],
-            roleID: null,
-            mask: true
+            data: [],
+            selected: null,
         };
 
-        this.handleSelectRole = this.handleSelectRole.bind(this);
-        this.handleSelectAuthority = this.handleSelectAuthority.bind(this);
-        this.handleSelectAllAuthority = this.handleSelectAllAuthority.bind(this);
-        this.handleSave = this.handleSave.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleAddChild = this.handleAddChild.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
         this.handleClose = this.handleClose.bind(this);
     }
 
     render() {
-        const { roles, authorities, roleID, mask } = this.state;
+        const { data, selected } = this.state;
 
         return <Window
             className={'DepartmentManagementWindow'}
@@ -35,134 +33,44 @@ class DepartmentManagementWindow extends React.Component {
         >
             <Content>
                 <Toolbar>
-                    <Button>{_t('Add')}</Button>
-                    <Button>{_t('Add Child')}</Button>
-                    <Button>{_t('Edit')}</Button>
-                    <Button>{_t('Delete')}</Button>
+                    <Button onClick={this.handleAdd}>{_t('Add')}</Button>
+                    <Button onClick={this.handleAddChild}>{_t('Add Child')}</Button>
+                    <Button onClick={this.handleEdit}>{_t('Edit')}</Button>
+                    <Button onClick={this.handleDelete}>{_t('Delete')}</Button>
                     <ToolbarFiller />
                 </Toolbar>
                 <HBoxLayout className={'hbox'}>
-                    <DataGrid
-                        className={'roles'}
-                        data={roles}
-                        selected={roleID}
-                        mask={mask}
-                        onSelect={this.handleSelectRole}
-                        keyField={'ID'}
-                    >
-                        <Column type={'number'} />
-                        <Column field={'Name'} title={_t('Role')} renderer={this.renderRoleName} />
-                    </DataGrid>
-                    <DataGrid
-                        className={'authorities'}
-                        data={authorities}
-                        mask={mask}
-                        onSelect={this.handleSelectAuthority}
-                        onSelectAll={this.handleSelectAllAuthority}
-                        keyField={'ID'}
-                    >
-                        <Column type={'checkbox'} field={'Enabled'} />
-                        <Column field={'Name'} title={_t('Authority')} />
-                    </DataGrid>
+                    <Tree data={data} selected={selected} />
                 </HBoxLayout>
             </Content>
         </Window>;
     }
 
     componentDidMount() {
-        fetch(`${app.options.server}/api/Role/List?pageSize=10000`).then(response => {
+        fetch(`${app.options.server}/api/Department/List?pageSize=10000`).then(response => {
             response.json().then(json => {
-                this.setState({
-                    roles: json.Data.rows,
-                    mask: false
-                });
             });
         });
     }
 
-    handleSelectRole(selected) {
-        this.setState({
-            roleID: selected.ID,
-            mask: true
-        });
-        fetch(`${app.options.server}/api/OperatingAuthority/Get?roleID=${selected.ID}`).then(response => {
-            response.json().then(json => {
-                json.Data.rows.forEach(n => {
-                    n.Name = _t(n.Name);
-                });
-                this.setState({
-                    authorities: json.Data.rows,
-                    mask: false
-                });
-            });
-        });
+    handleAdd() {
+
     }
 
-    handleSelectAuthority(selected) {
-        let authorities = this.state.authorities;
-        let authority = authorities.filter(n => n.ID === selected.ID)[0];
+    handleAddChild() {
 
-        if (authority) {
-            authority.Enabled = !authority.Enabled;
-        }
-
-        this.setState({
-            authorities
-        });
     }
 
-    handleSelectAllAuthority(value, name, event) {
-        let authorities = this.state.authorities;
+    handleEdit() {
 
-        authorities.forEach(n => {
-            n.Enabled = value;
-        });
-
-        this.setState({
-            authorities
-        });
     }
 
-    handleSave() {
-        const { roleID, authorities } = this.state;
+    handleDelete() {
 
-        let body = `RoleID=${roleID}`;
-
-        authorities.forEach(n => {
-            if (n.Enabled) {
-                body += `&Authorities[]=${n.ID}`;
-            }
-        });
-
-        fetch(`${app.options.server}/api/OperatingAuthority/Save`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body
-        }).then(response => {
-            response.json().then(json => {
-                app.toast(_t(json.Msg));
-                if (json.Code === 200) {
-                    this.handleSelectRole({
-                        ID: roleID
-                    });
-                }
-            });
-        });
     }
 
     handleClose() {
         app.removeElement(this);
-    }
-
-    renderRoleName(value) {
-        if (value === 'Administrator' ||
-            value === 'User' ||
-            value === 'Guest') {
-            return _t(value);
-        }
-        return value;
     }
 }
 
