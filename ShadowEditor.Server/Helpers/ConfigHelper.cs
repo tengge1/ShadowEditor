6bb2e9eddb49ba45d3831b9615d64d7dff45684c
tengge1 <930372551@qq.com>
2019-10-25 21:26:18 +0800

    开权限只能看到自己的动画。

---
+++ ShadowEditor.Server/Helpers/ConfigHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ShadowEditor.Server.Helpers
+{
+    /// <summary>
+    /// 配置管理器
+    /// </summary>
+    public sealed class ConfigHelper
+    {
+        /// <summary>
+        /// 是否开启权限
+        /// </summary>
+        public static bool EnableAuthority
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["EnableAuthority"] == "true";
+            }
+        }
+    }
+}
