1981ff35ce7bbe589deb31b783b8b91fcf8ded0f
tengge1 <930372551@qq.com>
2019-11-27 20:18:03 +0800

    TextRenderer

--- ShadowEditor.Web/src/render/TextRenderer.js
+++ ShadowEditor.Web/src/render/TextRenderer.js
@@ -1,5 +1,24 @@
-class TextRenderer {
+import BaseRenderer from './BaseRenderer';
 
+/**
+ * 文本渲染器
+ */
+class TextRenderer extends BaseRenderer {
+    constructor() {
+        super();
+    }
+
+    create(scenes, camera, renderer, selected) {
+
+    }
+
+    render() {
+
+    }
+
+    dispose() {
+
+    }
 }
 
 export default TextRenderer;
