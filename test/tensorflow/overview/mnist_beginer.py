472e7c4124fd9b6bfec04080f14ddeb68b5bd395
tengge1 <930372551@qq.com>
2019-12-08 20:29:53 +0800

    修改。

--- test/tensorflow/overview/mnist_beginer.py
+++ test/tensorflow/overview/mnist_beginer.py
@@ -6,7 +6,7 @@ mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
