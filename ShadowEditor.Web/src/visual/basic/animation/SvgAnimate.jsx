6ff34e6ad068f768ea0511af19a4bde837d2ccf0
tengge1 <930372551@qq.com>
2019-12-08 15:09:51 +0800

    名称修改。

---
+++ ShadowEditor.Web/src/visual/basic/animation/SvgAnimate.jsx
@@ -0,0 +1,17 @@
+/**
+ * SvgAnimate
+ * @author tengge / https://github.com/tengge1
+ */
+class SvgAnimate extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <animate />;
+    }
+    toJSON() {
+
+    }
+}
+
+export default SvgAnimate;
