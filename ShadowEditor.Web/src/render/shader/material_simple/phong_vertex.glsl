6265fc11839182a16ff6a36490fe2a0355f28416
tengge1 <930372551@qq.com>
2019-12-12 20:37:19 +0800

    phong_vertex

---
+++ ShadowEditor.Web/src/render/shader/material_simple/phong_vertex.glsl
@@ -0,0 +1,7 @@
+varying vec2 vUv;
+
+void main() {
+    vec3 transformed = vec3( position );
+    vec4 mvPosition = modelViewMatrix * vec4( transformed, 1.0 );
+    gl_Position = projectionMatrix * mvPosition;
+}
