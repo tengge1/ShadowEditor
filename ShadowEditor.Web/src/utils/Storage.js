4ce8e8e525be4b09308f313c492d2d6dd693c067
tengge1 <930372551@qq.com>
2020-02-15 21:02:28 +0800

    优化本地存储代码。

--- ShadowEditor.Web/src/utils/Storage.js
+++ ShadowEditor.Web/src/utils/Storage.js
@@ -1,120 +1,97 @@
 /**
  * 存储类
  */
-function Storage() {
-    if (this.get('showGrid') === undefined) {
-        this.set('showGrid', true);
+class Storage {
+    constructor() {
+        // 向本地存储写入默认配置，并提供快捷访问方法
+        const defaultConfigs = {
+            showGrid: true,
+            showCamera: false,
+            showPointLight: true,
+            showDirectionalLight: true,
+            showSpotLight: true,
+            showHemisphereLight: true,
+            showRectAreaLight: true,
+            showSkeleton: false,
+
+            // 选中效果
+            selectMode: 'whole', // whole: 选择整体；part: 选择部分。
+            selectedColor: '#ff6600', // unity3d: #ff6600
+            selectedThickness: 4,
+
+            // 高亮效果
+            hoverEnabled: false, // 高亮效果
+            hoveredColor: '#ffff00',
+
+            addMode: 'click', // 添加模式：center: 添加到场景中心；click: 点击场景添加。
+            controlMode: 'EditorControls' // 控制器模式：EditorControls: 编辑器控制器；FreeControls: 自由控制器。
+        };
+
+        Object.entries(defaultConfigs).forEach(n => {
+            if (this.get(n[0]) === undefined) {
+                this.set(n[0], n[1]);
+            }
+
+            Object.defineProperty(this, n[0], {
+                get: () => {
+                    return this.get(n[0]);
+                },
+                set: value => {
+                    return this.set(n[0], value);
+                }
+            });
+        });
     }
 
-    if (this.get('showCamera') === undefined) {
-        this.set('showCamera', false);
+    /**
+     * 获取本地存储键值
+     * @param {String} key 键
+     * @returns {Object} 值，不存储返回undefined
+     */
+    get(key) {
+        let configs = this._getConfigs();
+        return configs[key];
     }
 
-    if (this.get('showPointLight') === undefined) {
-        this.set('showPointLight', true);
+    set(key, value) {
+        let configs = this._getConfigs();
+        configs[key] = value;
+        this._setConfigs(configs);
     }
 
-    if (this.get('showDirectionalLight') === undefined) {
-        this.set('showDirectionalLight', true);
+    setConfigs(configs) {
+        if (typeof configs !== 'object') {
+            console.warn(`Storage: configs should be an object.`);
+            return;
+        }
+        let _configs = this._getConfigs();
+        Object.keys(configs).forEach(n => {
+            _configs[n] = configs[n];
+        });
+        this._setConfigs(_configs);
     }
 
-    if (this.get('showSpotLight') === undefined) {
-        this.set('showSpotLight', true);
+    remove(key) {
+        let configs = this._getConfigs();
+        delete configs[key];
+        this._setConfigs(configs);
     }
 
-    if (this.get('showHemisphereLight') === undefined) {
-        this.set('showHemisphereLight', true);
+    clear() {
+        window.localStorage.removeItem('configs');
     }
 
-    if (this.get('showRectAreaLight') === undefined) {
-        this.set('showRectAreaLight', true);
+    _getConfigs() {
+        let configs = window.localStorage.getItem('configs');
+        if (!configs) {
+            configs = '{}';
+        }
+        return JSON.parse(configs);
     }
 
-    if (this.get('showSkeleton') === undefined) {
-        this.set('showSkeleton', false);
-    }
-
-    // 选中效果
-    if (this.get('selectMode') === undefined) {
-        this.set('selectMode', 'whole'); // whole: 选择整体；part: 选择部分。
-    }
-
-    if (this.get('selectedColor') === undefined) {
-        this.set('selectedColor', '#ff6600'); // unity3d: #ff6600
-    }
-
-    if (this.get('selectedThickness') === undefined) {
-        this.set('selectedThickness', 4);
-    }
-
-    // 高亮效果
-    if (this.get('hoverEnabled') === undefined) {
-        this.set('hoverEnabled', false);
-    }
-
-    if (this.get('hoveredColor') === undefined) {
-        this.set('hoveredColor', '#ffff00');
-    }
-
-    // 添加模式
-    if (this.get('addMode') === undefined) {
-        this.set('addMode', 'click'); // center: 添加到场景中心；click: 点击场景添加。
-    }
-
-    // 控制器模式
-    if (this.get('controlMode') === undefined) {
-        this.set('controlMode', 'EditorControls'); // EditorControls: 编辑器控制器；FreeControls: 自由控制器。
+    _setConfigs(configs) {
+        window.localStorage.setItem('configs', JSON.stringify(configs));
     }
 }
-
-Storage.prototype.get = function (key) {
-    var configs = this._getConfigs();
-    return configs[key];
-};
-
-Storage.prototype.set = function (key, value) {
-    var configs = this._getConfigs();
-    configs[key] = value;
-    this._setConfigs(configs);
-};
-
-Storage.prototype.setConfigs = function (configs) {
-    if (typeof configs !== 'object') {
-        console.warn(`Storage: configs should be an object.`);
-        return;
-    }
-
-    var _configs = this._getConfigs();
-
-    Object.keys(configs).forEach(n => {
-        _configs[n] = configs[n];
-    });
-
-    this._setConfigs(_configs);
-};
-
-Storage.prototype.remove = function (key) {
-    var configs = this._getConfigs();
-    delete configs[key];
-    this._setConfigs(configs);
-};
-
-Storage.prototype.clear = function () {
-    window.localStorage.removeItem('configs');
-};
-
-Storage.prototype._getConfigs = function () {
-    var configs = window.localStorage.getItem('configs');
-
-    if (!configs) {
-        configs = '{}';
-    }
-
-    return JSON.parse(configs);
-};
-
-Storage.prototype._setConfigs = function (configs) {
-    window.localStorage.setItem('configs', JSON.stringify(configs));
-};
 
 export default Storage;
