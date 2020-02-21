cc6acbdfe734c4cfe95540a0f004a5a1b66c58b0
tengge1 <930372551@qq.com>
2020-02-21 21:17:56 +0800

    代码优化。

--- ShadowEditor.Web/src/controls/FreeControls.js
+++ ShadowEditor.Web/src/controls/FreeControls.js
@@ -16,42 +16,23 @@ class FreeControls extends BaseControls {
         this.controls.enabled = true;
     }
 
-    /**
-     * 禁用控制器
-     */
     disable() {
         this.enabled = false;
         this.controls.enabled = false;
     }
 
-    /**
-     * 转到某个物体的视角
-     * @param {THREE.Object3D} target 目标
-     */
     focus(target) { // eslint-disable-line
 
     }
 
-    /**
-     * 不断循环调用，以便实现动画效果
-     * @param {THREE.Clock} clock 时钟
-     * @param {Number} deltaTime 间隔时间
-     */
-    update(clock, deltaTime) { // eslint-disable-line
+    update() {
         this.controls.update();
     }
 
-    /**
-     * 当前鼠标所在位置碰撞点世界坐标
-     * @param {THREE.Vector3} position 世界坐标
-     */
     setPickPosition(position) { // eslint-disable-line
 
     }
 
-    /**
-     * 析构控制器
-     */
     dispose() {
         this.controls.dispose();
         this.camera = null;
