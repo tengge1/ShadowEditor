5f13c448fb77ccce89a73bee539e7c0a0eb15962
tengge1 <930372551@qq.com>
2020-02-25 19:23:33 +0800

    小区场景模板。

--- ShadowEditor.Web/src/editor/menu/scene/DistrictSceneTemplate.js
+++ ShadowEditor.Web/src/editor/menu/scene/DistrictSceneTemplate.js
@@ -4,7 +4,51 @@ import BaseSceneTemplate from './BaseSceneTemplate';
  * 小区场景模板
  */
 class DistrictSceneTemplate extends BaseSceneTemplate {
+    create() {
+        const editor = app.editor;
 
+        // 添加场景物体
+        const amlight = new THREE.AmbientLight(0xffffff, 0.24);
+        amlight.name = _t('Ambient');
+        editor.addObject(amlight);
+
+        const dirlight = new THREE.DirectionalLight(0xffffff, 0.56);
+        dirlight.name = _t('Directional');
+        dirlight.castShadow = true;
+        dirlight.position.set(5, 10, 7.5);
+        dirlight.shadow.radius = 0;
+        dirlight.shadow.mapSize.x = 2048;
+        dirlight.shadow.mapSize.y = 2048;
+        dirlight.shadow.camera.left = -20;
+        dirlight.shadow.camera.right = 20;
+        dirlight.shadow.camera.top = 20;
+        dirlight.shadow.camera.bottom = -20;
+        dirlight.shadow.camera.near = 0.1;
+        dirlight.shadow.camera.far = 500;
+
+        editor.addObject(dirlight);
+
+        // 添加场景动画
+        editor.animations = [{
+            id: null,
+            uuid: THREE.Math.generateUUID(),
+            layer: 0,
+            layerName: _t('AnimLayer1'),
+            animations: []
+        }, {
+            id: null,
+            uuid: THREE.Math.generateUUID(),
+            layer: 1,
+            layerName: _t('AnimLayer2'),
+            animations: []
+        }, {
+            id: null,
+            uuid: THREE.Math.generateUUID(),
+            layer: 2,
+            layerName: _t('AnimLayer3'),
+            animations: []
+        }];
+    }
 }
 
 export default DistrictSceneTemplate;
