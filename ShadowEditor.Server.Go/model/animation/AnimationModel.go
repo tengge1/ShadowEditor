689b8ea45b643dc24394cb408daa0a48a58e20a6
tengge1 <930372551@qq.com>
2020-03-24 22:07:51 +0800

    go

---
+++ ShadowEditor.Server.Go/model/animation/AnimationModel.go
@@ -0,0 +1,18 @@
+package animation
+
+type AnimationModel struct {
+	ID           string
+	Name         string
+	CategoryID   string
+	CategoryName string
+	TotalPinYin  string
+	FirstPinYin  string
+	Type         string
+	Url          string
+	FileName     string
+	FileSize     string
+	FileType     string
+	SaveName     string
+	SavePath     string
+	AddTime      string
+}
