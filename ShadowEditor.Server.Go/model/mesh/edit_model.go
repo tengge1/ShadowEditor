cfe5364f93736f7861819d1114756a96d8602769
tengge1 <930372551@qq.com>
2020-03-27 21:18:02 +0800

    mesh

---
+++ ShadowEditor.Server.Go/model/mesh/edit_model.go
@@ -0,0 +1,13 @@
+package mesh
+
+// EditModel 网格编辑模型
+type EditModel struct {
+	// ID
+	ID string
+	// 名称
+	Name string
+	// 类别
+	Category string
+	// 缩略图
+	Image string
+}
---
+++ ShadowEditor.Server.Go/model/mesh/type.go
@@ -0,0 +1,50 @@
+package mesh
+
+// Type 模型类型
+type Type int
+
+const (
+	// Unknown 未知类型
+	Unknown Type = iota
+	ThreeDs
+	ThreeMf
+	Amf
+	Assimp
+	Awd
+	Binary
+	Babylon
+	Babylonmeshdata
+	Bvh
+	Ctm
+	Dae
+	Drc
+	Fbx
+	Gcode
+	Glb
+	Gltf
+	Js
+	JSON
+	ThreeGeo
+	ThreeMat
+	ThreeObj
+	ThreeScn
+	Kmz
+	Md2
+	Nrrd
+	Obj
+	Pcd
+	Pdb
+	Playcanvas
+	Ply
+	Pmd
+	Pmx
+	Prwm
+	Sea3d
+	Stl
+	Vrm
+	Vrml
+	Vtk
+	Wrl
+	Lol
+	X
+)
