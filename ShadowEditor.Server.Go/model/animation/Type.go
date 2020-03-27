444022c1d6528b294d623330c0722460c869fe6b
tengge1 <930372551@qq.com>
2020-03-27 19:34:59 +0800

    animation type

--- ShadowEditor.Server.Go/model/animation/Type.go
+++ ShadowEditor.Server.Go/model/animation/Type.go
@@ -1,8 +1,11 @@
 package animation
 
 // Type 动画类型
-type Type struct {
-	Unknown   int
-	Mmd       int
+var Type = struct {
+	// 未知类型
+	Unknown int
+	// mmd模型动画
+	Mmd int
+	// mmd相机动画
 	MmdCamera int
-}
+}{0, 1, 2}
