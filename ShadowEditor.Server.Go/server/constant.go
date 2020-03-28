9b3b89d361eef5c00f1e824b76c8a00f9d3d82ed
tengge1 <930372551@qq.com>
2020-03-28 14:22:17 +0800

    constant

--- ShadowEditor.Server.Go/server/constant.go
+++ ShadowEditor.Server.Go/server/constant.go
@@ -1,9 +1,50 @@
 package server
 
-// Constant 常量
-type Constant string
-
 const (
 	// CategoryCollectionName 类别表名
-	CategoryCollectionName Constant = "_Category"
+	CategoryCollectionName string = "_Category"
+	// SceneCollectionName 场景表名
+	SceneCollectionName string = "_Scene"
+	// MeshCollectionName 模型表名
+	MeshCollectionName string = "_Mesh"
+	// MapCollectionName 贴图表名
+	MapCollectionName string = "_Map"
+	// MaterialCollectionName 材质表名
+	MaterialCollectionName string = "_Material"
+	// AudioCollectionName 音频表名
+	AudioCollectionName string = "_Audio"
+	// AnimationCollectionName 动画表名
+	AnimationCollectionName string = "_Animation"
+	// ParticleCollectionName 粒子表名
+	ParticleCollectionName string = "_Particle"
+	// PrefabCollectionName 预设体表名
+	PrefabCollectionName string = "_Prefab"
+	// CharacterCollectionName 角色表名
+	CharacterCollectionName string = "_Character"
+	// ScreenshotCollectionName 截图表名
+	ScreenshotCollectionName string = "_Screenshot"
+	// VideoCollectionName 视频表名
+	VideoCollectionName string = "_Video"
+	// FileCollectionName 上传文件表名
+	FileCollectionName string = "_File"
+	// ConfigCollectionName 配置表
+	ConfigCollectionName string = "_Config"
+	// RoleCollectionName 角色表
+	RoleCollectionName string = "_Role"
+	// UserCollectionName 用户表
+	UserCollectionName string = "_User"
+	// OperatingAuthorityCollectionName 操作权限表
+	OperatingAuthorityCollectionName string = "_OperatingAuthority"
+	// DepartmentCollectionName 组织机构表
+	DepartmentCollectionName string = "_Department"
+	// PluginCollectionName 插件表
+	PluginCollectionName string = "_Plugin"
+	// TypefaceCollectionName 字体
+	TypefaceCollectionName string = "_Typeface"
+	// ExportSceneListCollectionName 导出场景列表
+	ExportSceneListCollectionName string = "_ExportSceneList"
+	// HistorySuffix 历史表后缀（版本控制）
+	HistorySuffix string = "_history"
+	// VersionField 历史表中版本字段
+	VersionField string = "_version"
 )
