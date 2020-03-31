4e104a73287f4ac0e307f95e866487092b9bd6aa
tengge1 <930372551@qq.com>
2020-03-31 21:39:00 +0800

    TempDir

---
+++ ShadowEditor.Server.Go/helper/directory.go
@@ -0,0 +1,11 @@
+package helper
+
+// CopyDirectory copy one directory and its content to another
+func CopyDirectory(sourceDirName, destDirName string) {
+
+}
+
+// RemoveEmptyDirectory remove empty folder under path
+func RemoveEmptyDirectory(path string) {
+
+}
---
+++ ShadowEditor.Server.Go/helper/directory_test.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCopyDirectory(t *testing.T) {
+
+}
+
+func TestRemoveEmptyDirectory(t *testing.T) {
+
+}
+
+func TestTempDir(t *testing.T) {
+	t.Log(os.TempDir())
+}
