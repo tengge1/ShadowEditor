ae65447212de4fbe885279f6646347c86fd31dbc
liteng <liteng@w-ew.com>
2020-04-02 03:52:06 +0800

    redis test

--- ShadowEditor.Server.Go/helper/redis_test.go
+++ ShadowEditor.Server.Go/helper/redis_test.go
@@ -1,1 +1,29 @@
 package helper
+
+import "testing"
+
+func TestRedis(t *testing.T) {
+	redis, err := Redis{}.Create("localhost:6379", 0)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = redis.Set("foo", []byte("bar"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	bytes, hit, err := redis.Get("foo")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !hit {
+		t.Error("redis not hit")
+		return
+	}
+	result := string(bytes)
+	if result != "bar" {
+		t.Errorf("expect bar, %v get", result)
+	}
+}
