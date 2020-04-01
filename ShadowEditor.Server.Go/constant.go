1619016ae28e67e36868deb28bdb2a5ddd30f1f4
tengge1 <930372551@qq.com>
2020-04-01 20:47:48 +0800

    constant

--- ShadowEditor.Server.Go/constant.go
+++ ShadowEditor.Server.Go/constant.go
@@ -1,50 +1,50 @@
 package shadoweditor
 
 const (
-	// CategoryCollectionName 类别表名
+	// CategoryCollectionName category collection name
 	CategoryCollectionName string = "_Category"
-	// SceneCollectionName 场景表名
+	// SceneCollectionName scene collection name
 	SceneCollectionName string = "_Scene"
-	// MeshCollectionName 模型表名
+	// MeshCollectionName mesh collection name
 	MeshCollectionName string = "_Mesh"
-	// MapCollectionName 贴图表名
+	// MapCollectionName map collection name
 	MapCollectionName string = "_Map"
-	// MaterialCollectionName 材质表名
+	// MaterialCollectionName material collection name
 	MaterialCollectionName string = "_Material"
-	// AudioCollectionName 音频表名
+	// AudioCollectionName audio collection name
 	AudioCollectionName string = "_Audio"
-	// AnimationCollectionName 动画表名
+	// AnimationCollectionName animation collection name
 	AnimationCollectionName string = "_Animation"
-	// ParticleCollectionName 粒子表名
+	// ParticleCollectionName particle collection name
 	ParticleCollectionName string = "_Particle"
-	// PrefabCollectionName 预设体表名
+	// PrefabCollectionName prefab collection name
 	PrefabCollectionName string = "_Prefab"
-	// CharacterCollectionName 角色表名
+	// CharacterCollectionName character collection name
 	CharacterCollectionName string = "_Character"
-	// ScreenshotCollectionName 截图表名
+	// ScreenshotCollectionName screenshot collection name
 	ScreenshotCollectionName string = "_Screenshot"
-	// VideoCollectionName 视频表名
+	// VideoCollectionName video collection name
 	VideoCollectionName string = "_Video"
-	// FileCollectionName 上传文件表名
+	// FileCollectionName file collection name
 	FileCollectionName string = "_File"
-	// ConfigCollectionName 配置表
+	// ConfigCollectionName config collection name
 	ConfigCollectionName string = "_Config"
-	// RoleCollectionName 角色表
+	// RoleCollectionName role collection name
 	RoleCollectionName string = "_Role"
-	// UserCollectionName 用户表
+	// UserCollectionName user collection name
 	UserCollectionName string = "_User"
-	// OperatingAuthorityCollectionName 操作权限表
+	// OperatingAuthorityCollectionName operating authority collectionname
 	OperatingAuthorityCollectionName string = "_OperatingAuthority"
-	// DepartmentCollectionName 组织机构表
+	// DepartmentCollectionName department collection name
 	DepartmentCollectionName string = "_Department"
-	// PluginCollectionName 插件表
+	// PluginCollectionName plugin collection name
 	PluginCollectionName string = "_Plugin"
-	// TypefaceCollectionName 字体
+	// TypefaceCollectionName typeface collection name
 	TypefaceCollectionName string = "_Typeface"
-	// ExportSceneListCollectionName 导出场景列表
+	// ExportSceneListCollectionName export scene list collection name
 	ExportSceneListCollectionName string = "_ExportSceneList"
-	// HistorySuffix 历史表后缀（版本控制）
+	// HistorySuffix history suffix
 	HistorySuffix string = "_history"
-	// VersionField 历史表中版本字段
+	// VersionField version field
 	VersionField string = "_version"
 )
