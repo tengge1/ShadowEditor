0ee6d43b179fe60d5d0d6344652127fd89790609
tengge1 <930372551@qq.com>
2020-04-03 20:39:52 +0800

    directory_test

--- ShadowEditor.Server.Go/helper/directory_test.go
+++ ShadowEditor.Server.Go/helper/directory_test.go
@@ -1,46 +1,46 @@
 package helper
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 )
 
+type Type int
+
+const (
+	DirType Type = iota
+	FileType
+)
+
+type FileInfo struct {
+	Name     string
+	Type     Type
+	Children []FileInfo
+}
+
 var (
-	DirTree = map[string]interface{}{
-		"foo": map[string]interface{}{
-			"type":     "dir",
-			"children": map[string]interface{}{},
-		},
-		"bar.txt": map[string]interface{}{
-			"type": "file",
+	Tree = []FileInfo{
+		{
+			Name: "Foo",
+			Type: DirType,
+			Children: []FileInfo{
+				{
+					Name: "Foo1",
+					Type: DirType,
+				}, {
+					Name: "Foo2.txt",
+					Type: FileType,
+				},
+			},
+		}, {
+			Name: "Bar.txt",
+			Type: FileType,
 		},
 	}
 )
-
-func prepareTestDirTree() error {
-	root, err := ioutil.TempDir("", "")
-	if err != nil {
-		return err
-	}
-
-	err = os.MkdirAll(filepath.Join(tmpDir, "1"))
-	if err != nil {
-		return err
-	}
-
-	err = os.MkdirAll(filepath.Join(tmpDir, tree), 0755)
-	if err != nil {
-		os.RemoveAll(tmpDir)
-		return "", err
-	}
-	return tmpDir, nil
-}
-
-func TestCopyDirectory(t *testing.T) {
-	os.TempDir()
-}
 
 func TestRemoveEmptyDirectory(t *testing.T) {
 
@@ -55,4 +55,54 @@ func TestTempDir(t *testing.T) {
 	}
 	defer os.Remove(tmpDir)
 	t.Log(tmpDir)
+}
+
+func prepareTestTree(t *testing.T) error {
+	root, err := ioutil.TempDir("", "")
+	if err != nil {
+		return err
+	}
+	t.Logf("create temp dir: %v", root)
+	_, err = os.Stat(root)
+	if !os.IsExist(err) {
+		os.MkdirAll(root, 0755)
+	}
+	return CreateNode(root, Tree)
+}
+
+func CreateNode(parent string, infos []FileInfo) error {
+	for _, info := range infos {
+		path := filepath.Join(parent, info.Name)
+		if info.Type == DirType {
+			_, err := os.Stat(path)
+			if !os.IsExist(err) {
+				os.Mkdir(path, 0755)
+			}
+			if info.Children != nil {
+				err := CreateNode(path, info.Children)
+				if err != nil {
+					return err
+				}
+			}
+		} else if info.Type == FileType {
+			_, err := os.Stat(path)
+			if !os.IsExist(err) {
+				file, err := os.Create(path)
+				if err != nil {
+					return err
+				}
+				file.Close()
+			}
+		} else {
+			return fmt.Errorf("unknown FileInfo type: %v", info.Type)
+		}
+	}
+	return nil
+}
+
+func TestCopyDirectory(t *testing.T) {
+	err := prepareTestTree(t)
+	if err != nil {
+		t.Error(err)
+	}
 }
