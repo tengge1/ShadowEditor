c211d11d191daa73bcb9e0acf4a5d2b6a69ac2dc
tengge1 <930372551@qq.com>
2020-04-04 10:57:59 +0800

    http.go

--- ShadowEditor.Server.Go/helper/http.go
+++ ShadowEditor.Server.Go/helper/http.go
@@ -1,1 +1,27 @@
 package helper
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// Get make a get request
+func Get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+// Post make a post request
+func Post(url string, data url.Values) ([]byte, error) {
+	resp, err := http.PostForm(url, data)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
--- ShadowEditor.Server.Go/helper/http_test.go
+++ ShadowEditor.Server.Go/helper/http_test.go
@@ -1,1 +1,24 @@
 package helper
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	bytes, err := Get("http://www.baidu.com")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(string(bytes))
+}
+
+func TestPost(t *testing.T) {
+	bytes, err := Post("https://passport.baidu.com/v2/api/?login", url.Values{"username": {"foo"}, "password": {"bar"}})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(string(bytes))
+}
