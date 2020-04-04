493184d1a2eb9dc900c9563e434f554bded4581a
tengge1 <930372551@qq.com>
2020-04-04 10:32:25 +0800

    download.go

--- ShadowEditor.Server.Go/helper/download.go
+++ ShadowEditor.Server.Go/helper/download.go
@@ -1,1 +1,32 @@
 package helper
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+)
+
+// Download download a file from server
+func Download(w http.ResponseWriter, path, name string) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	header := w.Header()
+	header.Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%v"`, url.QueryEscape(name)))
+	header.Set("Content-Length", strconv.Itoa(len(bytes)))
+	header.Set("Content-Type", "application/octet-stream; charset=GB2312")
+	w.Write(bytes)
+}
--- ShadowEditor.Server.Go/helper/download_test.go
+++ ShadowEditor.Server.Go/helper/download_test.go
@@ -1,1 +1,40 @@
 package helper
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestDownload(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Download(w, "./download.go", "下载")
+	}))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	strlen := res.Header.Get("Content-Length")
+
+	t.Logf("Content-Disposition: %v", res.Header.Get("Content-Disposition"))
+	t.Logf("Content-Length: %v", strlen)
+	t.Logf("Content-Type: %v", res.Header.Get("Content-Type"))
+
+	stat, err := os.Stat("./download.go")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	size := int(stat.Size())
+	length, _ := strconv.Atoi(strlen)
+
+	if size != length {
+		t.Errorf("size should be %v, get %v", size, length)
+	}
+}
