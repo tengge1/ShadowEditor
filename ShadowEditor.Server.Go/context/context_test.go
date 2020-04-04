b49ed5d40dd27d14d3af94145876d38264e70004
tengge1 <930372551@qq.com>
2020-04-04 21:28:41 +0800

    忽略一些用不到的类。

--- ShadowEditor.Server.Go/context/context_test.go
+++ ShadowEditor.Server.Go/context/context_test.go
@@ -1,1 +1,29 @@
 package context
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+	err := Create("../config.toml")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(Config)
+	Logger.Info("Some info from context_test.go")
+
+	mong, err := Mongo()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	collectionNames, err := mong.ListCollectionNames()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	for _, collectionName := range collectionNames {
+		t.Log(collectionName)
+	}
+}
