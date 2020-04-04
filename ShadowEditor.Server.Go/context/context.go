785af580e31b907df0c18c3039e94b960b72e5e9
tengge1 <930372551@qq.com>
2020-04-04 16:17:58 +0800

    logger

--- ShadowEditor.Server.Go/context/context.go
+++ ShadowEditor.Server.Go/context/context.go
@@ -9,15 +9,26 @@ import (
 var (
 	// Config config cache
 	Config *helper.ConfigModel
+	// Logger create logs
+	Logger *helper.Logger
 )
 
 // Create create a context
 func Create(path string) error {
+	// config
 	config, err := helper.GetConfig(path)
 	if err != nil {
 		return err
 	}
 	Config = config
+
+	// logger
+	logger, err := helper.NewLogger(config.Log.File)
+	if err != nil {
+		return err
+	}
+	Logger = logger
+
 	return nil
 }
 
