2a4a0a5585c68b00db9f1332c99fc11b1594c165
tengge1 <930372551@qq.com>
2020-04-04 22:00:11 +0800

    修复cmd文件夹中所有错误。

--- ShadowEditor.Server.Go/cmd/root.go
+++ ShadowEditor.Server.Go/cmd/root.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var (
-	// Used for flags.
-	cfgFile     string
-	userLicense string
+	cfgFile string
 
 	rootCmd = &cobra.Command{
-		Use:   "cobra",
+		Use:   "ShadowEditor",
 		Short: "3d scene editor based on three.js",
 		Long: `ShadowEditor is a 3d scene editor based on three.js.
 This application uses mongodb to store data.`,
@@ -28,45 +21,8 @@ func Execute() error {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
-	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
-	viper.SetDefault("license", "apache")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "./config.toml", "config file")
 
 	rootCmd.AddCommand(serveCmd)
 	rootCmd.AddCommand(versionCmd)
-}
-
-func er(msg interface{}) {
-	fmt.Println("Error:", msg)
-	os.Exit(1)
-}
-
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			er(err)
-		}
-
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cobra")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
--- ShadowEditor.Server.Go/cmd/serve.go
+++ ShadowEditor.Server.Go/cmd/serve.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/tengge1/shadoweditor/context"
@@ -10,11 +11,17 @@ import (
 
 var serveCmd = &cobra.Command{
 	Use:     "serve",
-	Short:   "Use shadoweditor server to provider data",
+	Short:   "Start shadoweditor server",
 	Aliases: []string{"server"},
-	Long:    `Use shadoweditor server to provider data.`,
+	Long:    `Use shadoweditor server to provider scene and model data.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		err := context.Create("./config.toml")
+		_, err := os.Stat(cfgFile)
+		if os.IsNotExist(err) {
+			log.Fatalf("cannot find config file: %v", cfgFile)
+			return
+		}
+
+		err = context.Create(cfgFile)
 		if err != nil {
 			log.Fatal(err)
 			return
