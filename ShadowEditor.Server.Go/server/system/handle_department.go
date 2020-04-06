818c153cea84d8bb3ee4e8f07ead13b64f54f1a7
tengge1 <930372551@qq.com>
2020-04-06 16:04:46 +0800

    /api/Department/List

--- ShadowEditor.Server.Go/server/system/handle_department.go
+++ ShadowEditor.Server.Go/server/system/handle_department.go
@@ -1,1 +1,73 @@
 package system
+
+import (
+	"net/http"
+
+	shadow "github.com/tengge1/shadoweditor"
+	"github.com/tengge1/shadoweditor/context"
+	"github.com/tengge1/shadoweditor/helper"
+	"github.com/tengge1/shadoweditor/model"
+	"github.com/tengge1/shadoweditor/model/system"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func init() {
+	department := Department{}
+	context.Mux.UsingContext().Handle(http.MethodGet, "/api/Department/Get", department.List)
+}
+
+// Department 组织机构控制器
+type Department struct {
+}
+
+// List 获取列表
+func (Department) List(w http.ResponseWriter, r *http.Request) {
+	db, err := context.Mongo()
+	if err != nil {
+		helper.Write(w, err.Error())
+		return
+	}
+
+	// 获取所有用户
+	users := []system.User{}
+
+	err = db.FindMany(shadow.UserCollectionName, bson.M{}, &users)
+	if err != nil {
+		helper.Write(w, err.Error())
+		return
+	}
+
+	// 获取所有机构
+	filter := bson.M{
+		"Status": 0,
+	}
+
+	list := []system.Department{}
+
+	err = db.FindMany(shadow.DepartmentCollectionName, filter, &list)
+	if err != nil {
+		helper.Write(w, err.Error())
+		return
+	}
+
+	for _, dept := range list {
+		adminID := dept.AdminID
+		var admin system.User
+
+		for _, user := range users {
+			if user.ID == adminID {
+				admin = user
+				break
+			}
+		}
+
+		dept.AdminID = adminID
+		dept.AdminName = admin.Name
+	}
+
+	helper.WriteJSON(w, model.Result{
+		Code: 200,
+		Msg:  "Get Successfully!",
+		Data: list,
+	})
+}
---
+++ ShadowEditor.Server.Go/server/system/handle_department_test.go
@@ -0,0 +1,35 @@
+package system
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/tengge1/shadoweditor/context"
+)
+
+func TestDepartmentList(t *testing.T) {
+	context.Create("../../config.toml")
+	context.Config.Authority.Enabled = true
+
+	department := Department{}
+
+	ts := httptest.NewServer(http.HandlerFunc(department.List))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer res.Body.Close()
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log(string(bytes))
+}
