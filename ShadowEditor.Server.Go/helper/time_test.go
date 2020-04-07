0af71f68ac8f56ebbd938b16c4107237d560076c
tengge1 <930372551@qq.com>
2020-04-07 20:30:43 +0800

    time

--- ShadowEditor.Server.Go/helper/time_test.go
+++ ShadowEditor.Server.Go/helper/time_test.go
@@ -8,4 +8,7 @@ import (
 func TestTimeToString(t *testing.T) {
 	now := time.Now()
 	t.Log(TimeToString(now, "yyyy-MM-dd HH:mm:ss"))
+	t.Log(TimeToString(now, "yyyyMMddHHmmss"))
+	t.Log(TimeToString(now, "yyyyMMdd"))
+	t.Log(TimeToString(now, "HHmmss"))
 }
