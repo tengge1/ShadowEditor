326958dde60ce9d0e543f8fe3301a909f0d70b61
liteng <liteng@mail.w-ew.com>
2020-04-08 07:50:34 +0800

    handle_operating_authority

--- ShadowEditor.Server.Go/server/system/handle_operating_authority.go
+++ ShadowEditor.Server.Go/server/system/handle_operating_authority.go
@@ -1,1 +1,27 @@
 package system
+
+import (
+	"net/http"
+
+	"github.com/tengge1/shadoweditor/context"
+)
+
+func init() {
+	authority := OperatingAuthority{}
+	context.Mux.UsingContext().Handle(http.MethodGet, "/api/OperatingAuthority/Get", authority.Get)
+	context.Mux.UsingContext().Handle(http.MethodPost, "/api/OperatingAuthority/Save", authority.Save)
+}
+
+// OperatingAuthority 操作权限管理
+type OperatingAuthority struct {
+}
+
+// Get 根据角色ID获取权限
+func (OperatingAuthority) Get(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Save 保存
+func (OperatingAuthority) Save(w http.ResponseWriter, r *http.Request) {
+
+}
