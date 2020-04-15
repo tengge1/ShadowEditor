87b3cf2602c4f6d030d27d04c7e1afeeaf0da612
tengge1 <930372551@qq.com>
2020-04-15 20:41:14 +0800

    test start

--- ShadowEditor.Server.Go/server/server_test.go
+++ ShadowEditor.Server.Go/server/server_test.go
@@ -1,1 +1,13 @@
 package server
+
+import (
+	"testing"
+
+	"github.com/tengge1/shadoweditor/context"
+)
+
+func TestStart(t *testing.T) {
+	context.Create("../config.toml")
+	// port := context.Config.Server.Port
+	go Start()
+}
