0d1750c550c3e3f5c5f8d60f8d180f3e74b3c90b
tengge1 <930372551@qq.com>
2020-04-15 20:33:13 +0800

    screenshot

--- ShadowEditor.Server.Go/server/handle_screenshot_test.go
+++ ShadowEditor.Server.Go/server/handle_screenshot_test.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/tengge1/shadoweditor/context"
+)
+
+func TestScreenshotList(t *testing.T) {
+	context.Create("../config.toml")
+
+	screenshot := Screenshot{}
+
+	ts := httptest.NewServer(http.HandlerFunc(screenshot.List))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer res.Body.Close()
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log(string(bytes))
+}
