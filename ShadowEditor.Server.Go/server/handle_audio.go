f3adc81cad15ac20d64cbab096e56dd224a3234d
tengge1 <930372551@qq.com>
2020-04-15 19:48:31 +0800

    audio list

--- ShadowEditor.Server.Go/server/handle_audio.go
+++ ShadowEditor.Server.Go/server/handle_audio.go
@@ -1,1 +1,136 @@
 package server
+
+import (
+	"net/http"
+
+	shadow "github.com/tengge1/shadoweditor"
+	"github.com/tengge1/shadoweditor/context"
+	"github.com/tengge1/shadoweditor/helper"
+	"github.com/tengge1/shadoweditor/model"
+	"github.com/tengge1/shadoweditor/model/audio"
+	"github.com/tengge1/shadoweditor/model/category"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func init() {
+	audio := Audio{}
+	context.Mux.UsingContext().Handle(http.MethodGet, "/api/Audio/List", audio.List)
+	context.Mux.UsingContext().Handle(http.MethodPost, "/api/Audio/Add", audio.Add)
+	context.Mux.UsingContext().Handle(http.MethodPost, "/api/Audio/Edit", audio.Edit)
+	context.Mux.UsingContext().Handle(http.MethodPost, "/api/Audio/Delete", audio.Delete)
+}
+
+// Audio 音频控制器
+type Audio struct {
+}
+
+// List 获取列表
+func (Audio) List(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	db, err := context.Mongo()
+	if err != nil {
+		helper.WriteJSON(w, model.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+
+	// 获取所有类别
+	filter := bson.M{
+		"Type": "Audio",
+	}
+	categories := []category.Model{}
+	db.FindMany(shadow.CategoryCollectionName, filter, &categories)
+
+	docs := bson.A{}
+
+	opts := options.FindOptions{
+		Sort: bson.M{
+			"_id": -1,
+		},
+	}
+
+	if context.Config.Authority.Enabled {
+		user, _ := context.GetCurrentUser(r)
+
+		if user != nil {
+			filter1 := bson.M{
+				"UserID": user.ID,
+			}
+
+			if user.Name == "Administrator" {
+				filter2 := bson.M{
+					"UserID": bson.M{
+						"$exists": 0,
+					},
+				}
+				filter1 = bson.M{
+					"$or": bson.A{
+						filter1,
+						filter2,
+					},
+				}
+			}
+			db.FindMany(shadow.AudioCollectionName, filter1, &docs, &opts)
+		}
+	} else {
+		db.FindAll(shadow.AudioCollectionName, &docs, &opts)
+	}
+
+	list := []audio.Model{}
+	for _, i := range docs {
+		doc := i.(primitive.D).Map()
+		categoryID := ""
+		categoryName := ""
+
+		if doc["Category"] != nil {
+			for _, category := range categories {
+				if category.ID == doc["Category"].(string) {
+					categoryID = category.ID
+					categoryName = category.Name
+					break
+				}
+			}
+		}
+
+		info := audio.Model{
+			ID:           doc["_id"].(primitive.ObjectID).Hex(),
+			Name:         doc["Name"].(string),
+			CategoryID:   categoryID,
+			CategoryName: categoryName,
+			TotalPinYin:  helper.PinYinToString(doc["TotalPinYin"]),
+			FirstPinYin:  helper.PinYinToString(doc["FirstPinYin"]),
+			Type:         doc["Type"].(string),
+			URL:          doc["Url"].(string),
+			CreateTime:   doc["CreateTime"].(primitive.DateTime).Time(),
+			UpdateTime:   doc["UpdateTime"].(primitive.DateTime).Time(),
+		}
+
+		list = append(list, info)
+	}
+
+	helper.WriteJSON(w, model.Result{
+		Code: 200,
+		Msg:  "Get Successfully!",
+		Data: list,
+	})
+}
+
+// Add 添加
+func (Audio) Add(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Edit 编辑
+func (Audio) Edit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete 删除
+func (Audio) Delete(w http.ResponseWriter, r *http.Request) {
+
+}
--- ShadowEditor.Server.Go/server/handle_audio_test.go
+++ ShadowEditor.Server.Go/server/handle_audio_test.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/tengge1/shadoweditor/context"
+)
+
+func TestAudioList(t *testing.T) {
+	context.Create("../config.toml")
+
+	audio := Audio{}
+
+	ts := httptest.NewServer(http.HandlerFunc(audio.List))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer res.Body.Close()
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log(string(bytes))
+}
