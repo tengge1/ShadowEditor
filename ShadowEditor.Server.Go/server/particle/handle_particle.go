f69e032b4bd238e32f7eb1d646de3994f97e3d72
tengge1 <930372551@qq.com>
2020-04-18 19:48:43 +0800

    particle

--- ShadowEditor.Server.Go/server/particle/handle_particle.go
+++ ShadowEditor.Server.Go/server/particle/handle_particle.go
@@ -11,7 +11,6 @@ import (
 	"github.com/tengge1/shadoweditor/context"
 	"github.com/tengge1/shadoweditor/helper"
 	"github.com/tengge1/shadoweditor/model"
-	"github.com/tengge1/shadoweditor/model/particle"
 	"github.com/tengge1/shadoweditor/server/category"
 )
 
@@ -83,7 +82,7 @@ func (Particle) List(w http.ResponseWriter, r *http.Request) {
 		db.FindAll(shadow.ParticleCollectionName, &docs, &opts)
 	}
 
-	list := []particle.Model{}
+	list := []Model{}
 	for _, i := range docs {
 		doc := i.(primitive.D).Map()
 		categoryID := ""
@@ -101,7 +100,7 @@ func (Particle) List(w http.ResponseWriter, r *http.Request) {
 
 		thumbnail, _ := doc["Thumbnail"].(string)
 
-		info := particle.Model{
+		info := Model{
 			ID:           doc["_id"].(primitive.ObjectID).Hex(),
 			Name:         doc["Name"].(string),
 			CategoryID:   categoryID,
---
+++ ShadowEditor.Server.Go/server/particle/type.go
@@ -0,0 +1,13 @@
+package particle
+
+// Type 粒子类型
+type Type string
+
+const (
+	// Light light
+	Light Type = "light"
+	// Fire fire
+	Fire Type = "fire"
+	// Water water
+	Water Type = "water"
+)
