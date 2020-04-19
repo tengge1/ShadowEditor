53a4e42aeda1f4fc36fa0455f0feab38a8efb9c5
tengge1 <930372551@qq.com>
2020-04-19 13:53:06 +0800

    anim add

--- ShadowEditor.Server.Go/server/animation/handle_animation.go
+++ ShadowEditor.Server.Go/server/animation/handle_animation.go
@@ -1,7 +1,12 @@
 package animation
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -15,11 +20,11 @@ import (
 )
 
 func init() {
-	animation := Animation{}
-	server.Mux.UsingContext().Handle(http.MethodGet, "/api/Animation/List", animation.List)
-	server.Mux.UsingContext().Handle(http.MethodPost, "/api/Animation/Add", animation.Add)
-	server.Mux.UsingContext().Handle(http.MethodPost, "/api/Animation/Edit", animation.Edit)
-	server.Mux.UsingContext().Handle(http.MethodPost, "/api/Animation/Delete", animation.Delete)
+	handler := Animation{}
+	server.Mux.UsingContext().Handle(http.MethodGet, "/api/Animation/List", handler.List)
+	server.Mux.UsingContext().Handle(http.MethodPost, "/api/Animation/Add", handler.Add)
+	server.Mux.UsingContext().Handle(http.MethodPost, "/api/Animation/Edit", handler.Edit)
+	server.Mux.UsingContext().Handle(http.MethodPost, "/api/Animation/Delete", handler.Delete)
 }
 
 // Animation 动画控制器
@@ -62,7 +67,7 @@ func (Animation) List(w http.ResponseWriter, r *http.Request) {
 				"UserID": user.ID,
 			}
 
-			if user.Name == "Administrator" {
+			if user.RoleName == "Administrator" {
 				filter2 := bson.M{
 					"UserID": bson.M{
 						"$exists": 0,
@@ -123,7 +128,144 @@ func (Animation) List(w http.ResponseWriter, r *http.Request) {
 
 // Add 添加
 func (Animation) Add(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(server.Config.Upload.MaxSize)
+	files := r.MultipartForm.File
 
+	// check upload file
+	if len(files) != 1 {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  "Please select an file.",
+		})
+		return
+	}
+
+	file := files["file"][0]
+	fileName := file.Filename
+	fileSize := file.Size
+	fileType := file.Header.Get("Content-Type")
+	fileExt := filepath.Ext(fileName)
+	fileNameWithoutExt := strings.TrimRight(fileName, fileExt)
+
+	if strings.ToLower(fileExt) != ".zip" {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  "Only zip file is allowed!",
+		})
+		return
+	}
+
+	// save file
+	now := time.Now()
+
+	savePath := fmt.Sprintf("/Upload/Animation/%v", helper.TimeToString(now, "yyyyMMddHHmmss"))
+	physicalPath := helper.MapPath(savePath)
+
+	tempPath := physicalPath + "\\temp"
+
+	if _, err := os.Stat(tempPath); os.IsNotExist(err) {
+		os.MkdirAll(tempPath, 0755)
+	}
+
+	targetPath := fmt.Sprintf("%v/%v", tempPath, fileName)
+	target, err := os.Create(targetPath)
+	if err != nil {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+	defer target.Close()
+
+	source, err := file.Open()
+	if err != nil {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+	defer source.Close()
+
+	io.Copy(target, source)
+
+	helper.UnZip(targetPath, physicalPath)
+
+	os.RemoveAll(tempPath)
+
+	// justify file type
+	entryFileName := ""
+	animationType := Unknown
+
+	infos, err := ioutil.ReadDir(physicalPath)
+	if err != nil {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+
+	for _, info := range infos {
+		if !info.IsDir() && strings.HasSuffix(strings.ToLower(info.Name()), ".vmd") {
+			entryFileName = savePath + info.Name()
+			animationType = Mmd
+			break
+		}
+	}
+
+	if animationType == Unknown {
+		os.RemoveAll(physicalPath)
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  "Unknown file type!",
+		})
+		return
+	}
+
+	// save to mongo
+	pinyin := helper.ConvertToPinYin(fileNameWithoutExt)
+
+	db, err := server.Mongo()
+	if err != nil {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+
+	doc := bson.M{
+		"ID":          primitive.NewObjectID(),
+		"AddTime":     now,
+		"FileName":    fileName,
+		"FileSize":    fileSize,
+		"FileType":    fileType,
+		"FirstPinYin": pinyin.FirstPinYin,
+		"Name":        fileNameWithoutExt,
+		"SaveName":    fileName,
+		"SavePath":    savePath,
+		"Thumbnail":   "",
+		"TotalPinYin": pinyin.TotalPinYin,
+		"Type":        animationType,
+		"Url":         entryFileName,
+	}
+
+	if server.Config.Authority.Enabled {
+		user, err := server.GetCurrentUser(r)
+
+		if err != nil && user != nil {
+			doc["UserID"] = user.ID
+		}
+	}
+
+	db.InsertOne(server.AnimationCollectionName, doc)
+
+	helper.WriteJSON(w, server.Result{
+		Code: 200,
+		Msg:  "Upload successfully!",
+	})
 }
 
 // Edit 编辑
