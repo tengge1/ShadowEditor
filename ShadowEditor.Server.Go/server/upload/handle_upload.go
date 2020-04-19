bb612fe64527ed4bf7f42e2f09b3a56c2ffcd83c
tengge1 <930372551@qq.com>
2020-04-19 21:48:21 +0800

    upload

--- ShadowEditor.Server.Go/server/upload/handle_upload.go
+++ ShadowEditor.Server.Go/server/upload/handle_upload.go
@@ -1,1 +1,141 @@
 package upload
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/tengge1/shadoweditor/helper"
+	"github.com/tengge1/shadoweditor/server"
+)
+
+func init() {
+	handler := Upload{}
+	server.Mux.UsingContext().Handle(http.MethodPost, "/api/Upload/Upload", handler.Upload)
+}
+
+// Upload 上传控制器
+type Upload struct {
+}
+
+// Upload 上传文件
+func (Upload) Upload(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(server.Config.Upload.MaxSize)
+	files := r.MultipartForm.File
+
+	// check upload file
+	if len(files) != 1 {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  "Please select an file.",
+		})
+		return
+	}
+
+	file := files["file"][0]
+	fileName := file.Filename
+	fileSize := file.Size
+	fileType := file.Header.Get("Content-Type")
+	fileExt := filepath.Ext(fileName)
+	fileNameWithoutExt := strings.TrimRight(fileName, fileExt)
+
+	if strings.ToLower(fileExt) != ".jpg" &&
+		strings.ToLower(fileExt) != ".jpeg" &&
+		strings.ToLower(fileExt) != ".png" &&
+		strings.ToLower(fileExt) != ".gif" {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  "Only jpg, png, gif format is allowed to upload!",
+		})
+		return
+	}
+
+	// save file
+	now := time.Now()
+
+	savePath := fmt.Sprintf("/Upload/File/%v", helper.TimeToString(now, "yyyyMMddHHmmss"))
+	physicalPath := helper.MapPath(savePath)
+
+	if _, err := os.Stat(physicalPath); os.IsNotExist(err) {
+		os.MkdirAll(physicalPath, 0755)
+	}
+
+	targetPath := fmt.Sprintf("%v/%v", physicalPath, fileName)
+	target, err := os.Create(targetPath)
+	if err != nil {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+	defer target.Close()
+
+	source, err := file.Open()
+	if err != nil {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+	defer source.Close()
+
+	io.Copy(target, source)
+
+	// save to mongo
+	pinyin := helper.ConvertToPinYin(fileNameWithoutExt)
+
+	db, err := server.Mongo()
+	if err != nil {
+		helper.WriteJSON(w, server.Result{
+			Code: 300,
+			Msg:  err.Error(),
+		})
+		return
+	}
+
+	doc := bson.M{
+		"AddTime":     now,
+		"FileName":    fileName,
+		"FileSize":    fileSize,
+		"FileType":    fileType,
+		"FirstPinYin": pinyin.FirstPinYin,
+		"Name":        fileNameWithoutExt,
+		"SaveName":    fileName,
+		"SavePath":    savePath,
+		"Thumbnail":   "",
+		"TotalPinYin": pinyin.TotalPinYin,
+		"Url":         savePath + fileName,
+	}
+
+	if server.Config.Authority.Enabled {
+		user, err := server.GetCurrentUser(r)
+
+		if err != nil && user != nil {
+			doc["UserID"] = user.ID
+		}
+	}
+
+	db.InsertOne(server.FileCollectionName, doc)
+
+	helper.WriteJSON(w, server.Result{
+		Code: 200,
+		Msg:  "Upload successfully!",
+		Data: FileData{fileName, fileSize, fileType, savePath + fileName},
+	})
+}
+
+// FileData means upload file data result.
+type FileData struct {
+	FileName string `json:"fileName"`
+	FileSize int64  `json:"fileSize"`
+	FileType string `json:"fileType"`
+	URL      string `json:"url"`
+}
