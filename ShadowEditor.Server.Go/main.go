cd85b692de7b997d0c21c84597021e40f8719ad3
tengge <930372551@qq.com>
2020-04-30 22:39:32 +0800

    add license to file

--- ShadowEditor.Server.Go/main.go
+++ ShadowEditor.Server.Go/main.go
@@ -1,7 +1,16 @@
+// Copyright 2017-2020. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+//
+// For more information, please visit: https://github.com/tengge1/ShadowEditor
+// You can also visit: https://gitee.com/tengge1/ShadowEditor
+
 package main
 
 import "github.com/tengge1/shadoweditor/cmd"
 
+// Here we just import and execute the root command. Keep this file tidy.
+// We can run `go build` in this folder to create the shadow editor binary.
 func main() {
 	cmd.Execute()
 }
