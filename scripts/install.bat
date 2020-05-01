1fd0327a8299dcbba7b34195465f504f449cacbf
tengge <930372551@qq.com>
2020-05-01 11:37:52 +0800

    install.bat

--- scripts/install.bat
+++ scripts/install.bat
@@ -6,10 +6,40 @@
 :: You can also visit: https://gitee.com/tengge1/ShadowEditor
 ::
 :: Download and install the third-party dependencies both for golang and nodejs.
+:: You should install `golang` and `nodejs` first. In windows, you also need to
+:: install `MinGW` to build cgo packages written in `C++`.
+
+@echo off
+
+:: Save current dir.
+echo "save current work directory"
+pushd
+
+:: The ROOT_DIR is the parent directory of the `.bat` file.
+for %%I in ("%~dp0.") do for %%J in ("%%~dpI.") do set ROOT_DIR=%%~dpnxJ
+:: The SERVER_DIR is the golang server dir.
+set SERVER_DIR=%ROOT_DIR%\server
+:: The WEB_DIR is the web dir.
+set WEB_DIR=%ROOT_DIR%\web
+
+:: Print the dir information.
+echo "root dir:" %ROOT_DIR%
+echo "server dir:" %SERVER_DIR%
+echo "web dir:" %WEB_DIR%
 
 :: install the golang dependencies.
+echo "enter" %SERVER_DIR%
+cd %SERVER_DIR%
 go env -w GO111MODULE=on
-go install ../server/
+echo "run `go install`"
+go install
 
 :: install the nodejs dependencies.
-npm install ../
+echo "enter" %WEB_DIR%
+cd %WEB_DIR%
+echo "run `npm install`"
+npm install
+
+:: Restore current dir.
+echo "restore current work directory"
+popd
