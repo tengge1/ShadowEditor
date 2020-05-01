3d9fba06bdd7d869efdc1b785bead92360fae017
tengge <930372551@qq.com>
2020-05-01 13:53:16 +0800

    test jsoniter

---
+++ server/test/json/primitive_d/primitive_d.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+func main() {
+	data := primitive.D{
+		{Key: "foo", Value: "bar"},
+		{Key: "hello", Value: "world"},
+	}
+
+	jsoniter.RegisterTypeEncoder("go.mongodb.org/mongo-driver/bson/primitive.D", PrimitiveDEncoder{})
+
+	bytes, err := jsoniter.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
+}
+
+// PrimitiveDEncoder is a custom primitive.D encoder.
+type PrimitiveDEncoder struct {
+}
+
+// Encode encode primitive.D to string.
+func (PrimitiveDEncoder) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+	fmt.Println("DEBUG: custom marshal primitive.D")
+
+	val := (*primitive.D)(ptr)
+	stream.WriteVal(val.Map())
+}
+
+// IsEmpty detect whether primitive.ObjectID is empty.
+func (PrimitiveDEncoder) IsEmpty(ptr unsafe.Pointer) bool {
+	fmt.Println("DEBUG: custom marshal primitive.D")
+
+	return false
+}
