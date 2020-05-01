c101f389985a6d8d7d9c9b7a083d8e48913ccf8c
tengge <930372551@qq.com>
2020-05-01 12:23:50 +0800

    build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,9 +5,7 @@
 :: For more information, please visit: https://github.com/tengge1/ShadowEditor
 :: You can also visit: https://gitee.com/tengge1/ShadowEditor
 ::
-:: Download and install the third-party dependencies both for golang and nodejs.
-:: You should install `golang` and `nodejs` first. In windows, you also need to
-:: install `MinGW` to build cgo packages written in `C++`.
+:: Build both the server and the web client.
 
 @echo off
 
@@ -21,24 +19,44 @@ for %%I in ("%~dp0.") do for %%J in ("%%~dpI.") do set ROOT_DIR=%%~dpnxJ
 set SERVER_DIR=%ROOT_DIR%\server
 :: The WEB_DIR is the web dir.
 set WEB_DIR=%ROOT_DIR%\web
+set BUILD_DIR=%ROOT_DIR%\build
 
 :: Print the dir information.
 echo "root dir:" %ROOT_DIR%
 echo "server dir:" %SERVER_DIR%
 echo "web dir:" %WEB_DIR%
 
-:: install the golang dependencies.
+:: Create build dir if it does not exist.
+if not exist %BUILD_DIR% (
+  cd %ROOT_DIR%
+  echo "enter" %ROOT_DIR%
+  mkdir %BUILD_DIR%
+)
+
+:: Build the golang server.
 echo "enter" %SERVER_DIR%
 cd %SERVER_DIR%
+echo "build server..."
 go env -w GO111MODULE=on
-echo "run `go install`"
-go install
+go build -o ../build/ShadowEditor.exe
+echo "copy config.toml to the build directory"
+copy .\config.toml ..\build
 
-:: install the nodejs dependencies.
-echo "enter" %WEB_DIR%
+:: Copy files to the build folder.
+echo 'enter' %WEB_DIR%
 cd %WEB_DIR%
-echo "run `npm install`"
-call npm install
+echo "copy files..."
+xcopy /e /q /y %WEB_DIR%\assets %BUILD_DIR%\public\assets\
+xcopy /e /q /y %WEB_DIR%\locales %BUILD_DIR%\public\locales\
+copy favicon.ico ..\build\public\favicon.ico
+copy index.html ..\build\public\index.html
+copy manifest.json ..\build\public\manifest.json
+copy sw.js ..\build\public\sw.js
+copy view.html ..\build\public\view.html
+
+:: Build the nodejs web client.
+echo "build web client..."
+call npm run build
 
 :: Restore current dir.
 echo "restore current work directory"
