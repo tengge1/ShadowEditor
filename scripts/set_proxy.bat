676f4f0fe10f85034953db74d5eff2154e55a73e
liteng <liteng@w-ew.com>
2020-05-07 11:59:25 +0800

    win set proxy

--- scripts/set_proxy.bat
+++ scripts/set_proxy.bat
@@ -4,9 +4,18 @@
 ::
 :: For more information, please visit: https://github.com/tengge1/ShadowEditor
 :: You can also visit: https://gitee.com/tengge1/ShadowEditor
-::
+
+@echo off
+
+:: For some well-known reasons, we can not install packages from golang.org in china;
+:: and install packages from github.com is extremely slow
+
+echo set go proxy
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn
+
 :: For some well-known reasons, it is slow to install packages from https://www.npmjs.com/ in china.
 :: So, we can set a proxy to make it faster to install third-party dependencies.
 
-.\set_go_proxy.bat
-.\set_npm_proxy.bat
+echo set nodejs proxy
+npm config set registry https://registry.npm.taobao.org/
