07906c35160808b9abed5d073c817ba7f76dc21f
tengge <930372551@qq.com>
2020-05-18 21:29:07 +0800

    update authority

--- server/server/authority.go
+++ server/server/authority.go
@@ -120,3 +120,107 @@ type AuthorityModel struct {
 	// Name is authority name.
 	Name string
 }
+
+// Authority means an authority.
+type Authority string
+
+const (
+	// Administrator means the user has all the authorities.
+	Administrator Authority = "Administrator"
+	// Login means the user has to log in.
+	Login Authority = "Login"
+	// ListAnimation means the user can get the animation list.
+	ListAnimation Authority = "List Animation"
+	// AddAnimation means the user can upload animation.
+	AddAnimation Authority = "Add Animation"
+	// EditAnimation means the user can edit the animation name and thumbnail who uploaded.
+	EditAnimation Authority = "Edit Animation"
+	// DeleteAnimation means the user can delete the animation who uploaded.
+	DeleteAnimation Authority = "Delete Animation"
+	// ListAudio means the user can get the audio list.
+	ListAudio Authority = "List Audio"
+	// AddAudio means the user can upload audio.
+	AddAudio Authority = "Add Audio"
+	// EditAudio means the user can edit audio name and thumbnail who uploaded.
+	EditAudio Authority = "Edit Audio"
+	// DeleteAudio means the user can delete audio who uploaded.
+	DeleteAudio Authority = "Delete Audio"
+	// ListCategory means the user can get his category list.
+	ListCategory Authority = "List Category"
+	// SaveCategory means the user can add new category.
+	SaveCategory Authority = "Save Category"
+	// DeleteCategory means the user can delete the category who created.
+	DeleteCategory Authority = "Delete Category"
+	// ListCharacter means the user can get the character list.
+	ListCharacter Authority = "List Character"
+	// EditCharacter means the user can edit character name and thumbnail who uploaded.
+	EditCharacter Authority = "Edit Character"
+	// SaveCharacter means the user can save new character who created.
+	SaveCharacter Authority = "Save Character"
+	// DeleteCharacter means the user can delete character who created.
+	DeleteCharacter Authority = "Delete Character"
+	// ListMap means the user can get the texture list.
+	ListMap Authority = "List Map"
+	// AddMap means the user can add new texture.
+	AddMap Authority = "Add Map"
+	// EditMap means the user can edit texture name and thumbnail who uploaded.
+	EditMap Authority = "Edit Map"
+	// DeleteMap means the user can delete the texture who uploaded.
+	DeleteMap Authority = "Delete Map"
+	// ListMaterial means the user can get the material list.
+	ListMaterial Authority = "List Material"
+	// EditMaterial means the user can edit material name and thumbnail who saved.
+	EditMaterial Authority = "Edit Material"
+	// SaveMaterial means the user can save a new material.
+	SaveMaterial Authority = "Save Material"
+	// DeleteMaterial means the user can delete the material who saved.
+	DeleteMaterial Authority = "Delete Material"
+	// ListMesh means the user can get the mesh list.
+	ListMesh Authority = "List Mesh"
+	// AddMesh means the user can upload new mesh.
+	AddMesh Authority = "Add Mesh"
+	// EditMesh means the user can edit mesh name and thumbnail who uploaded.
+	EditMesh Authority = "Edit Mesh"
+	// DeleteMesh means the user can delete mesh who uploaded.
+	DeleteMesh Authority = "Delete Mesh"
+	// ListParticle means the user can get the particle list.
+	ListParticle Authority = "List Particle"
+	// EditParticle means the user can edit particle name and thumbnail who saved.
+	EditParticle Authority = "Edit Particle"
+	// SaveParticle means the user can save new particle.
+	SaveParticle Authority = "Save Particle"
+	// DeleteParticle means the user can delete particle who saved.
+	DeleteParticle Authority = "Delete Particle"
+	// ListPrefab means the user can get prefab list.
+	ListPrefab Authority = "List Prefab"
+	// EditPrefab means the user can edit prefab name and thumbnail.
+	EditPrefab Authority = "Edit Prefab"
+	// SavePrefab means the user can save new prefab.
+	SavePrefab Authority = "SavePrefab"
+	// DeletePrefab means the user can delete prefab.
+	DeletePrefab Authority = "Delete Prefab"
+	// EditScene means the user can edit scene name and thumbnail who saved.
+	EditScene Authority = "Edit Scene"
+	// SaveScene means the user can save new scene.
+	SaveScene Authority = "Save Scene"
+	// PublishScene means the user can publish new scene to static content.
+	PublishScene Authority = "Publish Scene"
+	// DeleteScene means the user can delete scenes who saved.
+	DeleteScene Authority = "Delete Scene"
+	// ListScreenshot means the user can get the screenshot list.
+	ListScreenshot Authority = "List Screenshot"
+	// AddScreenshot means the user can save new screenshot.
+	AddScreenshot Authority = "Add Screenshot"
+	// EditScreenshot means the user can edit screenshot name and thumbnail who saved.
+	EditScreenshot Authority = "Edit Screenshot"
+	// DeleteScreenshot means the user can delete screenshot who saved.
+	DeleteScreenshot Authority = "Delete Screenshot"
+	// ListVideo means the user can get the video list who saved.
+	ListVideo Authority = "List Video"
+	// AddVideo means the user can record new video and upload.
+	AddVideo Authority = "Add Video"
+	// EditVideo means the user can edit video name and thumbnail who saved.
+	EditVideo Authority = "Edit Video"
+	// DeleteVideo means the user can delete video who uploaded.
+	DeleteVideo Authority = "Delete Video"
+)
