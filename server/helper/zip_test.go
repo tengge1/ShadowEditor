2911579d0d48a42f143bfa7dd202a832ff37cfad
tengge <930372551@qq.com>
2020-05-24 15:02:44 +0800

    update zip

--- server/helper/zip_test.go
+++ server/helper/zip_test.go
@@ -9,19 +9,45 @@ package helper
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestUnZip(t *testing.T) {
-	file := "../test/test.zip"
-	tmp, err := ioutil.TempDir("/tmp", "unzip")
+// Use functions borrowed from copy_test.go to test Zip and Unzip.
+
+func TestZip(t *testing.T) {
+	// source dir
+	sourceDirName, err := ioutil.TempDir("", "")
 	if err != nil {
 		t.Error(err)
 	}
-	err = UnZip(file, tmp)
+	if err := prepareTestTree(sourceDirName); err != nil {
+		t.Error(err)
+	}
+	t.Logf("sourceDirName: %v", sourceDirName)
+
+	// create zip file
+	dest, err := ioutil.TempFile(os.TempDir(), "*.zip")
 	if err != nil {
 		t.Error(err)
-		return
 	}
-	t.Log(tmp)
+	dest.Close()
+	destPath := dest.Name()
+
+	if err := Zip(sourceDirName, destPath); err != nil {
+		t.Error(err)
+	}
+
+	// unzip
+	unzipDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := UnZip(destPath, unzipDir); err != nil {
+		t.Error(err)
+	}
+
+	// check unzip files
+	checkTestTree(unzipDir, t)
 }
