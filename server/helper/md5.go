13cd636285bb7a4764d3e0d7ca62762a616f6380
tengge <930372551@qq.com>
2020-05-24 13:28:32 +0800

    md5 helper

--- server/helper/md5.go
+++ server/helper/md5.go
@@ -13,6 +13,8 @@ import (
 )
 
 // MD5 convert a string to md5 encrypt string.
+//
+// TODO: Using md5 to encrypt passwords is not very secure.
 func MD5(str string) string {
 	h := md5.New()
 	h.Write([]byte(str))
