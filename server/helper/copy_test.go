f7c3abf786d9c0c4ca4feefc5c4b1b64f3d94070
tengge <930372551@qq.com>
2020-05-24 09:23:11 +0800

    update test copy

--- server/helper/copy_test.go
+++ server/helper/copy_test.go
@@ -19,6 +19,7 @@ type FileInfo struct {
 	Name     string
 	Type     Type
 	Children []FileInfo
+	Content  string // content when is a file
 }
 
 var (
@@ -31,76 +32,120 @@ var (
 					Name: "Foo1",
 					Type: DirType,
 				}, {
-					Name: "Foo2.txt",
-					Type: FileType,
+					Name:    "Foo2.txt",
+					Type:    FileType,
+					Content: "Hello, world.",
 				},
 			},
 		}, {
-			Name: "Bar.txt",
-			Type: FileType,
+			Name:    "Bar.txt",
+			Type:    FileType,
+			Content: "The quick brown fox jumps over the lazy dog.",
 		},
 	}
 )
 
 func TestCopyDirectory(t *testing.T) {
+	// source dir
 	sourceDirName, err := ioutil.TempDir("", "")
 	if err != nil {
 		t.Error(err)
-		return
 	}
-	t.Logf("sourceDirName: %v", sourceDirName)
-	err = prepareTestTree(sourceDirName)
-	if err != nil {
+	if err := prepareTestTree(sourceDirName); err != nil {
 		t.Error(err)
 	}
+	t.Logf("sourceDirName: %v", sourceDirName)
+
+	// dest dir
 	destDirName, err := ioutil.TempDir("", "")
 	if err != nil {
 		t.Error(err)
-		return
 	}
 	t.Logf("destDirName: %v", destDirName)
 
-	err = CopyDirectory(sourceDirName, destDirName)
-	if err != nil {
+	// copy dir
+	if err = CopyDirectory(sourceDirName, destDirName); err != nil {
 		t.Error(err)
-		return
 	}
+
+	// verify dest dir
+	checkTestTree(destDirName, t)
 }
 
+// prepareTestTree create directories and files descibe by `Tree`.
 func prepareTestTree(root string) error {
-	_, err := os.Stat(root)
-	if !os.IsExist(err) {
+	if _, err := os.Stat(root); os.IsNotExist(err) {
 		os.MkdirAll(root, 0755)
 	}
-	return CreateNode(root, Tree)
+	return createNode(root, Tree)
 }
 
-func CreateNode(parent string, infos []FileInfo) error {
+// createNode create a directory or file describe by `FileInfo`.
+func createNode(parent string, infos []FileInfo) error {
 	for _, info := range infos {
 		path := filepath.Join(parent, info.Name)
-		if info.Type == DirType {
-			_, err := os.Stat(path)
-			if !os.IsExist(err) {
+		if info.Type == DirType { // info is a dir
+			if _, err := os.Stat(path); os.IsNotExist(err) {
 				os.Mkdir(path, 0755)
 			}
-			if info.Children != nil {
-				err := CreateNode(path, info.Children)
-				if err != nil {
+			if info.Children != nil && len(info.Children) > 0 {
+				if err := createNode(path, info.Children); err != nil {
 					return err
 				}
 			}
-		} else if info.Type == FileType {
-			_, err := os.Stat(path)
-			if !os.IsExist(err) {
-				file, err := os.Create(path)
-				if err != nil {
+		} else if info.Type == FileType { // info is a file
+			if _, err := os.Stat(path); os.IsNotExist(err) {
+				if err := ioutil.WriteFile(path, []byte(info.Content), 0755); err != nil {
 					return err
 				}
-				file.Close()
 			}
 		} else {
 			return fmt.Errorf("unknown FileInfo type: %v", info.Type)
 		}
 	}
 	return nil
+}
+
+// checkTestTree verify if copy rightly.
+func checkTestTree(root string, t *testing.T) {
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		t.Errorf("%v is not created", root)
+	}
+	checkNode(root, Tree, t)
+}
+
+// checkNode verify a directory or file is copy rightly as described by FileInfo.
+func checkNode(parent string, infos []FileInfo, t *testing.T) {
+	for _, info := range infos {
+		path := filepath.Join(parent, info.Name)
+		if info.Type == DirType { // info is a dir
+			stat, err := os.Stat(path)
+			if os.IsNotExist(err) {
+				t.Errorf("%v is not created", path)
+			}
+			if !stat.IsDir() {
+				t.Errorf("expect a dir, got a file")
+			}
+			if info.Children != nil && len(info.Children) > 0 {
+				checkNode(path, info.Children, t)
+			}
+		} else if info.Type == FileType { // info is a file
+			stat, err := os.Stat(path)
+			if os.IsNotExist(err) {
+				t.Errorf("%v is not created", path)
+			}
+			if stat.IsDir() {
+				t.Errorf("expect a file, got a dir")
+			}
+			bytes, err := ioutil.ReadFile(path)
+			if err != nil {
+				t.Error(err)
+			}
+			if content := string(bytes); content != info.Content {
+				t.Errorf("expect %v, got %v", info.Content, content)
+			}
+		} else {
+			t.Errorf("unknown FileInfo type: %v", info.Type)
+		}
+	}
 }
