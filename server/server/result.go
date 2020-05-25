689ec01f47223fdb5998c48004507de1b323ae4b
tengge <930372551@qq.com>
2020-05-25 21:18:01 +0800

    update result

--- server/server/result.go
+++ server/server/result.go
@@ -9,7 +9,7 @@ package server
 
 // Result present a server handler result.
 type Result struct {
-	// The Response Code 200 - ok; 300 -error
+	// The Response Code: 200 - ok; 300 -error; 301 - not authorized.
 	Code int `json:"Code" bson:"Code"`
 	// The Response Message
 	Msg string `json:"Msg" bson:"Msg"`
