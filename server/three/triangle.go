3c43445623b73975ac5096c06450041bbe64b508
tengge <930372551@qq.com>
2020-06-10 21:49:45 +0800

    triangle

--- server/three/triangle.go
+++ server/three/triangle.go
@@ -10,323 +10,247 @@
 
 package three
 
-var _v0 = Vector3{};
-var _v1 = Vector3{};
-var _v2 = Vector3{};
-var _v3 = Vector3{};
+import "math"
 
-var _vab = Vector3{};
-var _vac = Vector3{};
-var _vbc = Vector3{};
-var _vap = Vector3{};
-var _vbp = Vector3{};
-var _vcp = Vector3{};
+var _v0 = Vector3{}
+var _v1 = Vector3{}
+var _v2 = Vector3{}
+var _v3 = Vector3{}
 
-func NewTriangle( a, b, c Vector3) *Triangle {
-	return &Triangle{a,b,c}
+var _vab = Vector3{}
+var _vac = Vector3{}
+var _vbc = Vector3{}
+var _vap = Vector3{}
+var _vbp = Vector3{}
+var _vcp = Vector3{}
+
+// NewTriangle :
+func NewTriangle(a, b, c Vector3) *Triangle {
+	return &Triangle{a, b, c}
 }
 
+// Triangle :
 type Triangle struct {
 	A Vector3
 	B Vector3
 	C Vector3
 }
 
-getNormal: function ( a, b, c, target ) {
+// GetNormal :
+func GetNormal(a, b, c, target Vector3) *Vector3 {
+	target.SubVectors(c, b)
+	_v0.SubVectors(a, b)
+	target.Cross(_v0)
 
-	if ( target === undefined ) {
-
-		console.warn( 'THREE.Triangle: .getNormal() target is now required' );
-		target = new Vector3();
-
+	targetLengthSq := target.LengthSq()
+	if targetLengthSq > 0 {
+		return target.MultiplyScalar(1 / math.Sqrt(targetLengthSq))
 	}
-
-	target.subVectors( c, b );
-	_v0.subVectors( a, b );
-	target.cross( _v0 );
-
-	var targetLengthSq = target.lengthSq();
-	if ( targetLengthSq > 0 ) {
-
-		return target.multiplyScalar( 1 / Math.sqrt( targetLengthSq ) );
-
-	}
-
-	return target.set( 0, 0, 0 );
-
-},
-
-// static/instance method to calculate barycentric coordinates
-// based on: http://www.blackpawn.com/texts/pointinpoly/default.html
-getBarycoord: function ( point, a, b, c, target ) {
-
-	_v0.subVectors( c, a );
-	_v1.subVectors( b, a );
-	_v2.subVectors( point, a );
-
-	var dot00 = _v0.dot( _v0 );
-	var dot01 = _v0.dot( _v1 );
-	var dot02 = _v0.dot( _v2 );
-	var dot11 = _v1.dot( _v1 );
-	var dot12 = _v1.dot( _v2 );
-
-	var denom = ( dot00 * dot11 - dot01 * dot01 );
-
-	if ( target === undefined ) {
-
-		console.warn( 'THREE.Triangle: .getBarycoord() target is now required' );
-		target = new Vector3();
-
-	}
-
-	// collinear or singular triangle
-	if ( denom === 0 ) {
-
-		// arbitrary location outside of triangle?
-		// not sure if this is the best idea, maybe should be returning undefined
-		return target.set( - 2, - 1, - 1 );
-
-	}
-
-	var invDenom = 1 / denom;
-	var u = ( dot11 * dot02 - dot01 * dot12 ) * invDenom;
-	var v = ( dot00 * dot12 - dot01 * dot02 ) * invDenom;
-
-	// barycentric coordinates must always sum to 1
-	return target.set( 1 - u - v, v, u );
-
-},
-
-containsPoint: function ( point, a, b, c ) {
-
-	Triangle.getBarycoord( point, a, b, c, _v3 );
-
-	return ( _v3.x >= 0 ) && ( _v3.y >= 0 ) && ( ( _v3.x + _v3.y ) <= 1 );
-
-},
-
-getUV: function ( point, p1, p2, p3, uv1, uv2, uv3, target ) {
-
-	this.getBarycoord( point, p1, p2, p3, _v3 );
-
-	target.set( 0, 0 );
-	target.addScaledVector( uv1, _v3.x );
-	target.addScaledVector( uv2, _v3.y );
-	target.addScaledVector( uv3, _v3.z );
-
-	return target;
-
-},
-
-isFrontFacing: function ( a, b, c, direction ) {
-
-	_v0.subVectors( c, b );
-	_v1.subVectors( a, b );
-
-	// strictly front facing
-	return ( _v0.cross( _v1 ).dot( direction ) < 0 ) ? true : false;
-
+	return target.Set(0, 0, 0)
 }
 
-// instance
+// GetBarycoord :
+// static/instance method to calculate barycentric coordinates
+// based on: http://www.blackpawn.com/texts/pointinpoly/default.html
+func GetBarycoord(point, a, b, c, target Vector3) *Vector3 {
+	_v0.SubVectors(c, a)
+	_v1.SubVectors(b, a)
+	_v2.SubVectors(point, a)
 
-set: function ( a, b, c ) {
+	dot00 := _v0.Dot(_v0)
+	dot01 := _v0.Dot(_v1)
+	dot02 := _v0.Dot(_v2)
+	dot11 := _v1.Dot(_v1)
+	dot12 := _v1.Dot(_v2)
 
-	this.a.copy( a );
-	this.b.copy( b );
-	this.c.copy( c );
+	denom := dot00*dot11 - dot01*dot01
 
-	return this;
-
-},
-
-setFromPointsAndIndices: function ( points, i0, i1, i2 ) {
-
-	this.a.copy( points[ i0 ] );
-	this.b.copy( points[ i1 ] );
-	this.c.copy( points[ i2 ] );
-
-	return this;
-
-},
-
-clone: function () {
-
-	return new this.constructor().copy( this );
-
-},
-
-copy: function ( triangle ) {
-
-	this.a.copy( triangle.a );
-	this.b.copy( triangle.b );
-	this.c.copy( triangle.c );
-
-	return this;
-
-},
-
-getArea: function () {
-
-	_v0.subVectors( this.c, this.b );
-	_v1.subVectors( this.a, this.b );
-
-	return _v0.cross( _v1 ).length() * 0.5;
-
-},
-
-getMidpoint: function ( target ) {
-
-	if ( target === undefined ) {
-
-		console.warn( 'THREE.Triangle: .getMidpoint() target is now required' );
-		target = new Vector3();
-
+	// collinear or singular triangle
+	if denom == 0 {
+		// arbitrary location outside of triangle?
+		// not sure if t is the best idea, maybe should be returning undefined
+		return target.Set(-2, -1, -1)
 	}
 
-	return target.addVectors( this.a, this.b ).add( this.c ).multiplyScalar( 1 / 3 );
+	invDenom := 1 / denom
+	u := (dot11*dot02 - dot01*dot12) * invDenom
+	v := (dot00*dot12 - dot01*dot02) * invDenom
 
-},
+	// barycentric coordinates must always sum to 1
+	return target.Set(1-u-v, v, u)
+}
 
-getNormal: function ( target ) {
+// ContainsPoint :
+func ContainsPoint(point, a, b, c Vector3) bool {
+	GetBarycoord(point, a, b, c, _v3)
+	return _v3.X >= 0 && _v3.Y >= 0 && (_v3.X+_v3.Y) <= 1
+}
 
-	return Triangle.getNormal( this.a, this.b, this.c, target );
+// GetUV :
+func GetUV(point, p1, p2, p3 Vector3, uv1, uv2, uv3, target Vector2) *Vector2 {
+	GetBarycoord(point, p1, p2, p3, _v3)
 
-},
+	target.Set(0, 0)
+	target.AddScaledVector(uv1, _v3.X)
+	target.AddScaledVector(uv2, _v3.Y)
+	target.AddScaledVector(uv3, _v3.Z)
 
-getPlane: function ( target ) {
+	return &target
+}
 
-	if ( target === undefined ) {
+// IsFrontFacing :
+func IsFrontFacing(a, b, c, direction Vector3) bool {
+	_v0.SubVectors(c, b)
+	_v1.SubVectors(a, b)
 
-		console.warn( 'THREE.Triangle: .getPlane() target is now required' );
-		target = new Plane();
+	// strictly front facing
+	return _v0.Cross(_v1).Dot(direction) < 0
+}
 
-	}
+// Set :
+func (t Triangle) Set(a, b, c Vector3) *Triangle {
+	t.A.Copy(a)
+	t.B.Copy(b)
+	t.C.Copy(c)
+	return &t
+}
 
-	return target.setFromCoplanarPoints( this.a, this.b, this.c );
+// SetFromPointsAndIndices :
+func (t Triangle) SetFromPointsAndIndices(points []Vector3, i0, i1, i2 int) *Triangle {
+	t.A.Copy(points[i0])
+	t.B.Copy(points[i1])
+	t.C.Copy(points[i2])
+	return &t
+}
 
-},
+// Clone :
+func (t Triangle) Clone() *Triangle {
+	return NewTriangle(t.A, t.B, t.C).Copy(t)
+}
 
-getBarycoord: function ( point, target ) {
+// Copy :
+func (t Triangle) Copy(triangle Triangle) *Triangle {
+	t.A.Copy(triangle.A)
+	t.B.Copy(triangle.B)
+	t.C.Copy(triangle.C)
+	return &t
+}
 
-	return Triangle.getBarycoord( point, this.a, this.b, this.c, target );
+// GetArea :
+func (t Triangle) GetArea() float64 {
+	_v0.SubVectors(t.C, t.B)
+	_v1.SubVectors(t.A, t.B)
+	return _v0.Cross(_v1).Length() * 0.5
+}
 
-},
+// GetMidpoint :
+func (t Triangle) GetMidpoint(target Vector3) *Vector3 {
+	return target.AddVectors(t.A, t.B).Add(t.C).MultiplyScalar(1 / 3)
+}
 
-getUV: function ( point, uv1, uv2, uv3, target ) {
+// GetNormal :
+func (t Triangle) GetNormal(target Vector3) *Vector3 {
+	return GetNormal(t.A, t.B, t.C, target)
+}
 
-	return Triangle.getUV( point, this.a, this.b, this.c, uv1, uv2, uv3, target );
+// GetPlane :
+func (t Triangle) GetPlane(target Plane) *Plane {
+	return target.SetFromCoplanarPoints(t.A, t.B, t.C)
+}
 
-},
+// GetBarycoord :
+func (t Triangle) GetBarycoord(point, target Vector3) *Vector3 {
+	return GetBarycoord(point, t.A, t.B, t.C, target)
+}
 
-containsPoint: function ( point ) {
+// GetUV :
+func (t Triangle) GetUV(point Vector3, uv1, uv2, uv3, target Vector2) *Vector2 {
+	return GetUV(point, t.A, t.B, t.C, uv1, uv2, uv3, target)
+}
 
-	return Triangle.containsPoint( point, this.a, this.b, this.c );
+// ContainsPoint :
+func (t Triangle) ContainsPoint(point Vector3) bool {
+	return ContainsPoint(point, t.A, t.B, t.C)
+}
 
-},
+// IsFrontFacing :
+func (t Triangle) IsFrontFacing(direction Vector3) bool {
+	return IsFrontFacing(t.A, t.B, t.C, direction)
+}
 
-isFrontFacing: function ( direction ) {
+// IntersectsBox :
+func (t Triangle) IntersectsBox(box Box3) bool {
+	return box.IntersectsTriangle(t)
+}
 
-	return Triangle.isFrontFacing( this.a, this.b, this.c, direction );
-
-},
-
-intersectsBox: function ( box ) {
-
-	return box.intersectsTriangle( this );
-
-},
-
-closestPointToPoint: function ( p, target ) {
-
-	if ( target === undefined ) {
-
-		console.warn( 'THREE.Triangle: .closestPointToPoint() target is now required' );
-		target = new Vector3();
-
-	}
-
-	var a = this.a, b = this.b, c = this.c;
-	var v, w;
+// ClosestPointToPoint :
+func (t Triangle) ClosestPointToPoint(p, target Vector3) *Vector3 {
+	a, b, c := t.A, t.B, t.C
+	var v, w float64
 
 	// algorithm thanks to Real-Time Collision Detection by Christer Ericson,
 	// published by Morgan Kaufmann Publishers, (c) 2005 Elsevier Inc.,
 	// under the accompanying license; see chapter 5.1.5 for detailed explanation.
 	// basically, we're distinguishing which of the voronoi regions of the triangle
 	// the point lies in with the minimum amount of redundant computation.
-
-	_vab.subVectors( b, a );
-	_vac.subVectors( c, a );
-	_vap.subVectors( p, a );
-	var d1 = _vab.dot( _vap );
-	var d2 = _vac.dot( _vap );
-	if ( d1 <= 0 && d2 <= 0 ) {
-
+	_vab.SubVectors(b, a)
+	_vac.SubVectors(c, a)
+	_vap.SubVectors(p, a)
+	d1 := _vab.Dot(_vap)
+	d2 := _vac.Dot(_vap)
+	if d1 <= 0 && d2 <= 0 {
 		// vertex region of A; barycentric coords (1, 0, 0)
-		return target.copy( a );
-
+		return target.Copy(a)
 	}
 
-	_vbp.subVectors( p, b );
-	var d3 = _vab.dot( _vbp );
-	var d4 = _vac.dot( _vbp );
-	if ( d3 >= 0 && d4 <= d3 ) {
-
+	_vbp.SubVectors(p, b)
+	d3 := _vab.Dot(_vbp)
+	d4 := _vac.Dot(_vbp)
+	if d3 >= 0 && d4 <= d3 {
 		// vertex region of B; barycentric coords (0, 1, 0)
-		return target.copy( b );
-
+		return target.Copy(b)
 	}
 
-	var vc = d1 * d4 - d3 * d2;
-	if ( vc <= 0 && d1 >= 0 && d3 <= 0 ) {
-
-		v = d1 / ( d1 - d3 );
+	vc := d1*d4 - d3*d2
+	if vc <= 0 && d1 >= 0 && d3 <= 0 {
+		v = d1 / (d1 - d3)
 		// edge region of AB; barycentric coords (1-v, v, 0)
-		return target.copy( a ).addScaledVector( _vab, v );
-
+		return target.Copy(a).AddScaledVector(_vab, v)
 	}
 
-	_vcp.subVectors( p, c );
-	var d5 = _vab.dot( _vcp );
-	var d6 = _vac.dot( _vcp );
-	if ( d6 >= 0 && d5 <= d6 ) {
-
+	_vcp.SubVectors(p, c)
+	d5 := _vab.Dot(_vcp)
+	d6 := _vac.Dot(_vcp)
+	if d6 >= 0 && d5 <= d6 {
 		// vertex region of C; barycentric coords (0, 0, 1)
-		return target.copy( c );
-
+		return target.Copy(c)
 	}
 
-	var vb = d5 * d2 - d1 * d6;
-	if ( vb <= 0 && d2 >= 0 && d6 <= 0 ) {
-
-		w = d2 / ( d2 - d6 );
+	vb := d5*d2 - d1*d6
+	if vb <= 0 && d2 >= 0 && d6 <= 0 {
+		w = d2 / (d2 - d6)
 		// edge region of AC; barycentric coords (1-w, 0, w)
-		return target.copy( a ).addScaledVector( _vac, w );
-
+		return target.Copy(a).AddScaledVector(_vac, w)
 	}
 
-	var va = d3 * d6 - d5 * d4;
-	if ( va <= 0 && ( d4 - d3 ) >= 0 && ( d5 - d6 ) >= 0 ) {
-
-		_vbc.subVectors( c, b );
-		w = ( d4 - d3 ) / ( ( d4 - d3 ) + ( d5 - d6 ) );
+	va := d3*d6 - d5*d4
+	if va <= 0 && (d4-d3) >= 0 && (d5-d6) >= 0 {
+		_vbc.SubVectors(c, b)
+		w = (d4 - d3) / ((d4 - d3) + (d5 - d6))
 		// edge region of BC; barycentric coords (0, 1-w, w)
-		return target.copy( b ).addScaledVector( _vbc, w ); // edge region of BC
-
+		return target.Copy(b).AddScaledVector(_vbc, w) // edge region of BC
 	}
 
 	// face region
-	var denom = 1 / ( va + vb + vc );
+	denom := 1 / (va + vb + vc)
 	// u = va * denom
-	v = vb * denom;
-	w = vc * denom;
+	v = vb * denom
+	w = vc * denom
 
-	return target.copy( a ).addScaledVector( _vab, v ).addScaledVector( _vac, w );
+	return target.Copy(a).AddScaledVector(_vab, v).AddScaledVector(_vac, w)
+}
 
-},
-
-equals: function ( triangle ) {
-
-	return triangle.a.equals( this.a ) && triangle.b.equals( this.b ) && triangle.c.equals( this.c );
-
+// Equals :
+func (t Triangle) Equals(triangle Triangle) bool {
+	return triangle.A.Equals(t.A) && triangle.B.Equals(t.B) && triangle.C.Equals(t.C)
 }
