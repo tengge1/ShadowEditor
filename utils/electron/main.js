f518339a43fbce57106b57d42b74906c954052ce
tengge <930372551@qq.com>
2020-06-15 21:11:15 +0800

    electron chinese

--- utils/electron/main.js
+++ utils/electron/main.js
@@ -1,6 +1,44 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const iconv = require('iconv-lite');
 
-function createWindow() {
+function startMongoDB() {
+
+}
+
+function startServer() {
+
+}
+
+function start() {
+    let appPath = app.getPath('exe')
+    console.log(appPath)
+    // // 获取上一层的目录 app 是当前目录名称 需要给去掉
+    // let path = appPath.replace(/\\app\\studio.exe/, '')
+
+    const exec = require('child_process').exec
+
+    // console.log(appPath);
+    // console.log(path)
+
+    // // 执行命令行，如果命令不需要路径，或就是项目根目录，则不需要cwd参数：
+    var workerProcess = exec('build/ShadowEditor.exe', { cwd: 'build', encoding: 'GBK' })
+    // // 不受child_process默认的缓冲区大小的使用方法，没参数也要写上{}：workerProcess = exec(cmdStr, {})
+
+    // // 打印正常的后台可执行程序输出
+    workerProcess.stdout.on('data', function (data) {
+        console.log('stdout: ' + data)
+    })
+
+    // // 打印错误的后台可执行程序输出
+    workerProcess.stderr.on('data', buf => {
+        console.log(iconv.decode(buf, 'GBK'));
+    })
+
+    // // 退出之后的输出
+    workerProcess.on('close', function (code) {
+        console.log('out code：' + code)
+    })
+
     let win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -13,4 +51,4 @@ function createWindow() {
     win.loadFile('index.html')
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(start)
