bb80572ff145070f44eae61e97cbf818b6153f90
tengge <930372551@qq.com>
2020-06-25 09:51:00 +0800

    install_dev.js

--- scripts/install_dev.js
+++ scripts/install_dev.js
@@ -19,24 +19,24 @@ const cmds = [
     'go get -u golang.org/x/tools/cmd/goimports',
     'go get -u golang.org/x/lint/golint',
     'go get -u golang.org/x/tools/cmd/godoc',
-    'go get -u -v golang.org/x/tools/gopls',
+    'go get -u golang.org/x/tools/gopls',
 
-    'go get -u -v github.com/uudashr/gopkgs/v2/cmd/gopkgs',
-    'go get -u -v github.com/acroca/go-symbols',
-    'go get -u -v github.com/cweill/gotests/...',
-    'go get -u -v github.com/fatih/gomodifytags',
-    'go get -u -v github.com/josharian/impl',
-    'go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct',
-    'go get -u -v github.com/haya14busa/goplay/cmd/goplay',
-    'go get -u -v github.com/godoctor/godoctor',
-    'go get -u -v github.com/go-delve/delve/cmd/dlv',
-    'go get -u -v github.com/zmb3/gogetdoc',
-    'go get -u -v github.com/rogpeppe/godef',
-    'go get -u -v github.com/ramya-rao-a/go-outline',
-    'go get -u -v github.com/sqs/goreturns',
-    'go get -u -v github.com/tylerb/gotype-live',
-    'go get -u -v github.com/sourcegraph/go-langserver',
-    'go get -u -v github.com/stamblerre/gocode'
+    'go get -u github.com/uudashr/gopkgs/v2/cmd/gopkgs',
+    'go get -u github.com/acroca/go-symbols',
+    'go get -u github.com/cweill/gotests/...',
+    'go get -u github.com/fatih/gomodifytags',
+    'go get -u github.com/josharian/impl',
+    'go get -u github.com/davidrjenni/reftools/cmd/fillstruct',
+    'go get -u github.com/haya14busa/goplay/cmd/goplay',
+    'go get -u github.com/godoctor/godoctor',
+    'go get -u github.com/go-delve/delve/cmd/dlv',
+    'go get -u github.com/zmb3/gogetdoc',
+    'go get -u github.com/rogpeppe/godef',
+    'go get -u github.com/ramya-rao-a/go-outline',
+    'go get -u github.com/sqs/goreturns',
+    'go get -u github.com/tylerb/gotype-live',
+    'go get -u github.com/sourcegraph/go-langserver',
+    'go get -u github.com/stamblerre/gocode'
 ];
 
 /**
@@ -61,7 +61,11 @@ function main() {
 
     // install the golang development dependencies
     cmds.forEach(n => {
-        console.log(exec(n));
+        try {
+            console.log(exec(n));
+        } catch {
+            console.warn(`install ${n} failed`);
+        }
     });
 
     console.log(`leave ${serverDir}`);
