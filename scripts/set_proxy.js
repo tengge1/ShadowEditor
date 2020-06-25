37e68c87aa32a3eae16888b9bb8fbcbdb8bbab98
tengge <930372551@qq.com>
2020-06-25 08:59:06 +0800

    node set proxy

--- scripts/set_proxy.js
+++ scripts/set_proxy.js
@@ -12,23 +12,36 @@ const process = require('child_process');
 
 // IMPORTANT: This script is for Chinese users only!
 
-const cmds = [
+/**
+ * Execute a command
+ * @param {String} cmd bat of shell command
+ * @param {Boolean} showCmd whether to print the command
+ * @returns the result of the command
+ */
+function exec(cmd, showCmd = true) {
+    showCmd && console.log(cmd);
+    return process.execSync(cmd).toString().trimRight('\n');
+}
+
+/**
+ * The main function
+ */
+function main() {
     // For some well-known reasons, we can not install packages from golang.org in china;
     // and install packages from github.com is extremely slow.
     // So, we can set a proxy to make it faster to install third-party dependencies.
-    'echo set go proxy',
-    'go env -w GO111MODULE=on',
-    'go env -w GOPROXY=https://goproxy.cn',
+    console.log('set go proxy');
+    exec('go env -w GO111MODULE=on');
+    exec('go env -w GOPROXY=https://goproxy.cn');
 
     // For some well-known reasons, it is slow to install packages from https://www.npmjs.com/ in china.
     // So, we can set a proxy to make it faster to install third-party dependencies.
-    'echo set nodejs proxy',
-    'npm config set registry https://registry.npm.taobao.org/',
+    console.log('set nodejs proxy');
+    console.log('npm config set registry https://registry.npm.taobao.org/');
 
     // Output the current `go_proxy` and `node_proxy`.
-    'echo current go proxy: $(go env GOPROXY)',
-    'echo current nodejs proxy: $(npm config get registry)'
-];
+    console.log(`current go proxy: ${exec('go env GOPROXY', false)}`);
+    console.log(`current nodejs proxy: ${exec('npm config get registry', false)}`);
+}
 
-var buf = process.execSync('go env GOPROXY');
-console.log(buf.toString());
+main()
