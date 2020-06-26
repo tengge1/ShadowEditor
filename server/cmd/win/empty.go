300ff3f4fe5b3e686e9fda3412620365eb5847b4
tengge <930372551@qq.com>
2020-06-27 00:28:26 +0800

    build on ubuntu

---
+++ server/cmd/win/empty.go
@@ -0,0 +1,12 @@
+// Copyright 2017-2020 The ShadowEditor Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+//
+// For more information, please visit: https://github.com/tengge1/ShadowEditor
+// You can also visit: https://gitee.com/tengge1/ShadowEditor
+
+package win
+
+// This is an empty file for ubuntu only. DO NOT delete.
+// If all file in this package is built on Windows, it will output an error:
+// main.go:14:2: build constraints exclude all Go files in ShadowEditor/server/cmd/win
