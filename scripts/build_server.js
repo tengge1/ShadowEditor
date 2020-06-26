5c9b50d560e96388d4370471c4d1912cb12a2632
tengge <930372551@qq.com>
2020-06-26 14:59:08 +0800

    build-server

--- scripts/build_server.js
+++ scripts/build_server.js
@@ -8,25 +8,14 @@
  * You can also visit: https://gitee.com/tengge1/ShadowEditor
  */
 
-const subprocess = require('child_process');
 const path = require('path');
 const os = require('os');
-
-/**
- * Execute a command
- * @param {String} cmd bat of shell command
- * @param {Boolean} showCmd whether to print the command
- * @returns the result of the command
- */
-function exec(cmd, showCmd = true) {
-    showCmd && console.log(cmd);
-    return subprocess.execSync(cmd).toString().trimRight('\n');
-}
+const exec = require('./exec');
 
 /**
  * The main function
  */
-function main() {
+async function main() {
     const rootDir = process.cwd(); // The root dir that contains `README.md`.
     const serverDir = path.join(rootDir, 'server'); // The golang server dir.
 
@@ -35,9 +24,9 @@ function main() {
     process.chdir(serverDir);
     console.log(`build server...`);
     if (os.platform() === 'win32') {
-        exec('go build -o ../build/ShadowEditor.exe');
+        await exec('go', ['build', '-o', '../build/ShadowEditor.exe']);
     } else {
-        exec('go build -o ../build/ShadowEditor');
+        await exec('go', ['build', '-o', '../build/ShadowEditor']);
     }
     console.log(`leave ${serverDir}`);
     process.chdir(rootDir);
