d0cb76ba5398eb32038290aa81661a637d0332f9
tengge <930372551@qq.com>
2020-07-04 09:20:27 +0800

    add banner

--- web/src/command/Command.js
+++ web/src/command/Command.js
@@ -1,4 +1,13 @@
-﻿/**
+﻿/*
+ * Copyright 2017-2020 The ShadowEditor Authors. All rights reserved.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file.
+ * 
+ * For more information, please visit: https://github.com/tengge1/ShadowEditor
+ * You can also visit: https://gitee.com/tengge1/ShadowEditor
+ */
+/**
  * 命令基类
  * @author dforrer / https://github.com/dforrer
  * Developed as part of a project at University of Applied Sciences and Arts Northwestern Switzerland (www.fhnw.ch)
--- web/src/controls/OrthographicCameraControls.js
+++ web/src/controls/OrthographicCameraControls.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2017-2020 The ShadowEditor Authors. All rights reserved.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file.
+ * 
+ * For more information, please visit: https://github.com/tengge1/ShadowEditor
+ * You can also visit: https://gitee.com/tengge1/ShadowEditor
+ */
 /**
  * 正交相机控制器
  * @author tengge / https://github.com/tengge1
