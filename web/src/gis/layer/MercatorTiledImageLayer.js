d74418023f6de9bda19f865dda71a70fd2b58169
tengge <930372551@qq.com>
2020-08-04 20:12:56 +0800

    remove grid helper

--- web/src/gis/layer/MercatorTiledImageLayer.js
+++ web/src/gis/layer/MercatorTiledImageLayer.js
@@ -80,63 +80,7 @@ MercatorTiledImageLayer.prototype.createTile = function (sector, level, row, col
 
 // Overridden from TiledImageLayer to unproject the retrieved image prior to creating a texture for it.
 MercatorTiledImageLayer.prototype.createTexture = function (dc, tile, image) {
-    var srcCanvas = dc.canvas2D,
-        srcContext = dc.ctx2D,
-        srcImageData,
-        destCanvas = this.destCanvas,
-        destContext = this.destContext,
-        destImageData = destContext.createImageData(image.width, image.height),
-        sector = tile.sector,
-        tMin = WWMath.gudermannianInverse(sector.minLatitude),
-        tMax = WWMath.gudermannianInverse(sector.maxLatitude),
-        lat, g, srcRow, kSrc, kDest, sy, dy;
-
-    srcCanvas.width = image.width;
-    srcCanvas.height = image.height;
-    destCanvas.width = image.width;
-    destCanvas.height = image.height;
-
-    // Draw the original image to a canvas so image data can be had for it.
-    destContext.drawImage(image, 0, 0, image.width, image.height);
-    // srcImageData = srcContext.getImageData(0, 0, image.width, image.height);
-
-    // If it's a blank image, mark it as permanently absent.
-    // if (this.detectBlankImages && this.isBlankImage(image, srcImageData)) {
-    //     this.absentResourceList.markResourceAbsentPermanently(tile.imagePath);
-    //     return null;
-    // }
-
-    // Unproject the retrieved image.
-    // for (var n = 0; n < 1; n++) {
-    //     for (var y = 0; y < image.height; y++) {
-    //         sy = 1 - y / (image.height - 1);
-    //         lat = sy * sector.deltaLatitude() + sector.minLatitude;
-    //         g = WWMath.gudermannianInverse(lat);
-    //         dy = 1 - (g - tMin) / (tMax - tMin);
-    //         dy = WWMath.clamp(dy, 0, 1);
-    //         srcRow = Math.floor(dy * (image.height - 1));
-    //         for (var x = 0; x < image.width; x++) {
-    //             kSrc = 4 * (x + srcRow * image.width);
-    //             kDest = 4 * (x + y * image.width);
-
-    //             destImageData.data[kDest] = srcImageData.data[kSrc];
-    //             destImageData.data[kDest + 1] = srcImageData.data[kSrc + 1];
-    //             destImageData.data[kDest + 2] = srcImageData.data[kSrc + 2];
-    //             destImageData.data[kDest + 3] = srcImageData.data[kSrc + 3];
-    //         }
-    //     }
-    // }
-
-    //destContext.putImageData(destImageData, 0, 0);
-    destContext.strokeStyle = '#ffff00';
-    destContext.strokeRect(0, 0, 256, 256);
-    destContext.font = "48px sans-serif";
-    destContext.fillStyle = '#ff0000';
-    destContext.textAlign = 'center';
-    destContext.textBaseline = 'middle';
-    destContext.fillText(`${tile.row},${tile.column}`, 128, 128);
-
-    return TiledImageLayer.prototype.createTexture.call(this, dc, tile, destCanvas);
+    return TiledImageLayer.prototype.createTexture.call(this, dc, tile, image);
 };
 
 // Determines whether a retrieved image is blank.
