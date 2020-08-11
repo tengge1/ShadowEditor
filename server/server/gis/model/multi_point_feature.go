2078030f4f7b7caa0ab64b68310535b50ccaf638
tengge <930372551@qq.com>
2020-08-11 21:18:40 +0800

    multi_point_feature

---
+++ server/server/gis/model/multi_point_feature.go
@@ -0,0 +1,24 @@
+// Copyright 2017-2020 The ShadowEditor Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+//
+// For more information, please visit: https://github.com/tengge1/ShadowEditor
+// You can also visit: https://gitee.com/tengge1/ShadowEditor
+
+package model
+
+// MultiPointFeature represents a multi-point.
+type MultiPointFeature struct {
+	// Type should always be `Feature`.
+	Type       string                 `json:"type"`
+	ID         string                 `json:"id"`
+	Geometry   MultiPointGeometry     `json:"geometry"`
+	Properties map[string]interface{} `json:"properties"`
+}
+
+// MultiPointGeometry is the geometry of a MultiPointFeature.
+type MultiPointGeometry struct {
+	// Type should always be `MultiPoint`;
+	Type        GeometryType `json:"type"`
+	Coordinates [][]float64  `json:"coordinates"`
+}
