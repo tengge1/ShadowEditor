e9a8791b7cc48bf782e4e40d18902d9cf3e3a3b9
tengge <930372551@qq.com>
2020-08-11 21:22:07 +0800

    multi_polygon_feature

---
+++ server/server/gis/model/multi_polygon_feature.go
@@ -0,0 +1,24 @@
+// Copyright 2017-2020 The ShadowEditor Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+//
+// For more information, please visit: https://github.com/tengge1/ShadowEditor
+// You can also visit: https://gitee.com/tengge1/ShadowEditor
+
+package model
+
+// MultiPolygonFeature represents a MultiPolygon.
+type MultiPolygonFeature struct {
+	// Type should always be `Feature`.
+	Type       string                 `json:"type"`
+	ID         string                 `json:"id"`
+	Geometry   MultiPolygonGeometry   `json:"geometry"`
+	Properties map[string]interface{} `json:"properties"`
+}
+
+// MultiPolygonGeometry is the geometry of a MultiPolygonFeature.
+type MultiPolygonGeometry struct {
+	// Type should always be `MultiPolygon`.
+	Type        GeometryType    `json:"type"`
+	Coordinates [][][][]float64 `json:"coordinates"`
+}
