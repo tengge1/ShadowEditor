79330cf1fcf16aabb60094c69373fa92f2a8812c
tengge <930372551@qq.com>
2020-08-25 22:34:41 +0800

    update shader

--- web/test/WebWorldWind/src/shaders/glsl/surface_tile_vertex.glsl
+++ web/test/WebWorldWind/src/shaders/glsl/surface_tile_vertex.glsl
@@ -29,13 +29,43 @@ uniform sampler2D heightmap;
 varying vec2 texSamplerCoord;
 varying vec2 texMaskCoord;
 
-int add(int x, int y, int z) {
-    return x + y + z;
-}
+#define EARTH_RADIUS 6378137.0
+#define MIN_LATITUDE -180.0
+#define MAX_LATITUDE 180.0
+#define MIN_LONGITUDE -180.0
+#define MAX_LONGITUDE 180.0
+#define PI 3.141592653589793
 
 void main() {
-    add(column, row, level);
-    gl_Position = mvpMatrix * vertexPoint;
+// 每个瓦片位置
+    float size = pow(2.0, float(level));
+    float dlon = (MAX_LONGITUDE - MIN_LONGITUDE) / size;
+    float dlat = (MAX_LATITUDE - MIN_LATITUDE) / size;
+
+    float left = MIN_LONGITUDE + dlon * float(column);
+    float top = MAX_LATITUDE - dlat * float(row);
+    float right = left + dlon;
+    float bottom = top - dlat;
+
+    // 瓦片上每个小格位置
+    // +0.5的原因是：position范围是-0.5到0.5
+    float lon = left + (right - left) * (0.5 + vertexPoint.x);
+    float lat = top - (top - bottom) * (0.5 + vertexPoint.y);
+
+    lon = lon * PI / 180.0;
+    lat = lat * PI / 180.0;
+
+    // 墨卡托投影反算
+    lat = 2.0 * atan(exp(lat)) - PI / 2.0;
+
+    vec4 transformed = vec4(
+        EARTH_RADIUS * cos(lat) * cos(lon),
+        EARTH_RADIUS * sin(lat),
+        -EARTH_RADIUS * cos(lat) * sin(lon),
+        1.0
+    );
+
+    gl_Position = mvpMatrix * transformed;
     /* Transform the vertex texture coordinate into sampler texture coordinates. */
     texSamplerCoord = (texSamplerMatrix * vertexTexCoord).st;
     /* Transform the vertex texture coordinate into mask texture coordinates. */
