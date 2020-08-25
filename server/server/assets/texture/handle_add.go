49f57b8c465826a9d3bf7f9e443b825359e3fade
daijun <daijun@potevio.com>
2020-08-25 21:04:35 +0800

    fix 天空球上传图片type判断错误

--- server/server/assets/texture/handle_add.go
+++ server/server/assets/texture/handle_add.go
@@ -138,7 +138,7 @@ func Add(w http.ResponseWriter, r *http.Request) {
 	}
 
 	doc["TotalPinYin"] = pinyin.TotalPinYin
-
+	textureType := r.FormValue("type")
 	if len(files) == 6 { // Cute Texture
 		doc["Type"] = Cube
 
@@ -155,7 +155,7 @@ func Add(w http.ResponseWriter, r *http.Request) {
 	} else if strings.ToLower(filepath.Ext(file.Filename)) == ".mp4" { // 视频贴图
 		doc["Type"] = Video
 		doc["Url"] = fmt.Sprintf("%v/%v", savePath, fileName)
-	} else if fileType == "skyBall" {
+	} else if textureType == "skyBall" {
 		doc["Type"] = SkyBall
 		doc["Url"] = fmt.Sprintf("%v/%v", savePath, fileName)
 	} else {
