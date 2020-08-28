9e54893c87f09d43e72d570f99f657ba2d5c2c7c
tengge <930372551@qq.com>
2020-08-28 21:19:11 +0800

    three.js projection

--- web/test/WebWorldWind/src/WorldWindow.js
+++ web/test/WebWorldWind/src/WorldWindow.js
@@ -563,55 +563,67 @@ WorldWindow.prototype.resize = function () {
 };
 
 // Internal. Intentionally not documented.
-WorldWindow.prototype.computeViewingTransform = function (projection, modelview) {
-    modelview.setToIdentity();
-    this.worldWindowController.applyLimits();
-    var globe = this.globe;
-    var navigator = this.navigator;
-    var lookAtPosition = new Position(navigator.lookAtLocation.latitude, navigator.lookAtLocation.longitude, 0);
-    modelview.multiplyByLookAtModelview(lookAtPosition, navigator.range, navigator.heading, navigator.tilt, navigator.roll, globe);
+WorldWindow.prototype.computeViewingTransform = function () {
+    var mat4 = new THREE.Matrix4();
+    return function (projection, modelview) {
+        modelview.setToIdentity();
+        this.worldWindowController.applyLimits();
+        var globe = this.globe;
+        var navigator = this.navigator;
+        var lookAtPosition = new Position(navigator.lookAtLocation.latitude, navigator.lookAtLocation.longitude, 0);
+        modelview.multiplyByLookAtModelview(lookAtPosition, navigator.range, navigator.heading, navigator.tilt, navigator.roll, globe);
 
-    if (projection) {
-        projection.setToIdentity();
-        var globeRadius = WWMath.max(globe.equatorialRadius, globe.polarRadius),
-            eyePoint = modelview.extractEyePoint(new Vec3(0, 0, 0)),
-            eyePos = globe.computePositionFromPoint(eyePoint[0], eyePoint[1], eyePoint[2], new Position(0, 0, 0)),
-            eyeHorizon = WWMath.horizonDistanceForGlobeRadius(globeRadius, eyePos.altitude),
-            atmosphereHorizon = WWMath.horizonDistanceForGlobeRadius(globeRadius, 160000),
-            viewport = this.viewport;
+        if (projection) {
+            projection.setToIdentity();
+            var globeRadius = WWMath.max(globe.equatorialRadius, globe.polarRadius),
+                eyePoint = modelview.extractEyePoint(new Vec3(0, 0, 0)),
+                eyePos = globe.computePositionFromPoint(eyePoint[0], eyePoint[1], eyePoint[2], new Position(0, 0, 0)),
+                eyeHorizon = WWMath.horizonDistanceForGlobeRadius(globeRadius, eyePos.altitude),
+                atmosphereHorizon = WWMath.horizonDistanceForGlobeRadius(globeRadius, 160000),
+                viewport = this.viewport;
 
-        // Set the far clip distance to the smallest value that does not clip the atmosphere.
-        // TODO adjust the clip plane distances based on the navigator's orientation - shorter distances when the
-        // TODO horizon is not in view
-        // TODO parameterize the object altitude for horizon distance
-        var farDistance = eyeHorizon + atmosphereHorizon;
-        if (farDistance < 1e3)
-            farDistance = 1e3;
+            // Set the far clip distance to the smallest value that does not clip the atmosphere.
+            // TODO adjust the clip plane distances based on the navigator's orientation - shorter distances when the
+            // TODO horizon is not in view
+            // TODO parameterize the object altitude for horizon distance
+            var farDistance = eyeHorizon + atmosphereHorizon;
+            if (farDistance < 1e3)
+                farDistance = 1e3;
 
-        // Compute the near clip distance in order to achieve a desired depth resolution at the far clip distance.
-        // This computed distance is limited such that it does not intersect the terrain when possible and is never
-        // less than a predetermined minimum (usually one). The computed near distance automatically scales with the
-        // resolution of the WebGL depth buffer.
-        var nearDistance = WWMath.perspectiveNearDistanceForFarDistance(farDistance, 10, this.depthBits);
+            // Compute the near clip distance in order to achieve a desired depth resolution at the far clip distance.
+            // This computed distance is limited such that it does not intersect the terrain when possible and is never
+            // less than a predetermined minimum (usually one). The computed near distance automatically scales with the
+            // resolution of the WebGL depth buffer.
+            var nearDistance = WWMath.perspectiveNearDistanceForFarDistance(farDistance, 10, this.depthBits);
 
-        // Prevent the near clip plane from intersecting the terrain.
-        var distanceToSurface = eyePos.altitude - globe.elevationAtLocation(eyePos.latitude, eyePos.longitude);
-        if (distanceToSurface > 0) {
-            var maxNearDistance = WWMath.perspectiveNearDistance(viewport.width, viewport.height, distanceToSurface);
-            if (nearDistance > maxNearDistance) {
-                nearDistance = maxNearDistance;
+            // Prevent the near clip plane from intersecting the terrain.
+            var distanceToSurface = eyePos.altitude - globe.elevationAtLocation(eyePos.latitude, eyePos.longitude);
+            if (distanceToSurface > 0) {
+                var maxNearDistance = WWMath.perspectiveNearDistance(viewport.width, viewport.height, distanceToSurface);
+                if (nearDistance > maxNearDistance) {
+                    nearDistance = maxNearDistance;
+                }
             }
-        }
 
-        if (nearDistance < 1) {
-            nearDistance = 1;
+            if (nearDistance < 1) {
+                nearDistance = 1;
+            }
+            
+            var width = nearDistance;
+            var height = width * viewport.height / viewport.width;
+            mat4.makePerspective(
+                -width / 2,
+                width / 2,
+                height / 2,
+                -height / 2,
+                nearDistance,
+                farDistance
+            );
+            mat4.transpose(); // WebWorldWind是行优先矩阵
+            projection.copy(mat4.elements);
         }
-
-        // Compute the current projection matrix based on this navigator's perspective properties and the current
-        // WebGL viewport.
-        projection.setToPerspectiveProjection(viewport.width, viewport.height, nearDistance, farDistance);
-    }
-};
+    };
+}();
 
 // Internal. Intentionally not documented.
 WorldWindow.prototype.computePixelMetrics = function (projection) {
