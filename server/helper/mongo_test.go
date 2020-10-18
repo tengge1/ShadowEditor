87b41164663288806aadca0d943728976722b8ab
liteng <930372551@qq.com>
2020-10-18 15:32:30 +0800

    upgrade mongo helper

--- server/helper/mongo_test.go
+++ server/helper/mongo_test.go
@@ -8,6 +8,7 @@
 package helper
 
 import (
+	"go.mongodb.org/mongo-driver/mongo"
 	"testing"
 	"time"
 
@@ -114,4 +115,164 @@ func TestMongo(t *testing.T) {
 type Person struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
+}
+
+func TestTraction(t *testing.T) {
+	config, err := GetConfig("../config.toml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// test data
+	dbName := "Test" + time.Now().Format("150405")
+	collectionName := "PersonTest"
+	xiaoming := Person{"xiaoqiang", 30}
+
+	// NewMongo
+	db, err := NewMongo(config.Database.Connection, dbName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := db.CreateCollection(collectionName); err != nil {
+		t.Error(err)
+	}
+
+	err = db.UseSession(func(sessionContext mongo.SessionContext) error {
+		// StartTransaction
+		if err := sessionContext.StartTransaction(); err != nil {
+			t.Error(err)
+			return err
+		}
+
+		if _, err := db.InsertOne(collectionName, xiaoming); err != nil {
+			t.Error(err)
+			return err
+		}
+
+		filter := bson.M{
+			"name": "xiaoqiang",
+		}
+
+		var person Person
+		find, err := db.FindOne(collectionName, filter, &person)
+		if err != nil {
+			t.Error(err)
+			return err
+		}
+		if find {
+			t.Errorf("expect false, get %v", find)
+		}
+
+		// CommitTransaction
+		if err := sessionContext.CommitTransaction(sessionContext); err != nil {
+			t.Error(err)
+			return err
+		}
+
+		find, err = db.FindOne(collectionName, filter, &person)
+		if err != nil {
+			t.Error(err)
+		}
+		if !find {
+			t.Errorf("expect true, get %v", find)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMongo_CollectionExists(t *testing.T) {
+	config, err := GetConfig("../config.toml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	dbName := "Test"
+
+	db, err := NewMongo(config.Database.Connection, dbName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// collection is not existed
+	collectionName := "Collection" + time.Now().Format("150405")
+
+	existed, err := db.CollectionExists(collectionName)
+	if err != nil {
+		t.Error(err)
+	}
+	if existed == true {
+		t.Errorf("expect false, get %v", existed)
+	}
+
+	// collection is existed
+	if err := db.CreateCollection(collectionName); err != nil {
+		t.Error(err)
+	}
+
+	existed, err = db.CollectionExists(collectionName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if existed == false {
+		t.Errorf("expect true, get %v", existed)
+	}
+}
+
+// Descriptor test struct descriptor in mongodb
+type Descriptor struct {
+	Foo   int `json:"foo1"`
+	Bar   int `bson:"bar2"`
+	Hello int `json:"bar3" bson:"foo4"`
+	World int
+}
+
+func TestMongo_Descriptor(t *testing.T) {
+	config, err := GetConfig("../config.toml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	db, err := NewMongo(config.Database.Connection, "GISCoreTest")
+	if err != nil {
+		t.Error(err)
+	}
+	collectionName := "Descriptor"
+	exists, err := db.CollectionExists(collectionName)
+	if exists {
+		db.DropCollection(collectionName)
+	}
+
+	descriptor := Descriptor{1, 2, 3, 4}
+
+	if _, err := db.InsertOne(collectionName, descriptor); err != nil {
+		t.Error(err)
+	}
+
+	doc := bson.M{}
+	find, err := db.FindOne(collectionName, bson.M{}, &doc)
+	if err != nil {
+		t.Error(err)
+	}
+	if !find {
+		t.Errorf("expect true, get false")
+	}
+	if _, ok := doc["foo"]; !ok {
+		t.Errorf("foo is not found")
+	}
+	if _, ok := doc["bar2"]; !ok {
+		t.Errorf("bar2 is not found")
+	}
+	if _, ok := doc["foo4"]; !ok {
+		t.Errorf("foo4 is not found")
+	}
+	if _, ok := doc["world"]; !ok {
+		t.Errorf("world is not found")
+	}
 }
