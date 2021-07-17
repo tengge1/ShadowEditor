642fc7dc5bd1abffdcaab7c942a5d5e6c9c214c7
tengge1 <930372551@qq.com>
2021-07-17 22:10:59 +0800

    update clean script

--- scripts/clean.js
+++ scripts/clean.js
@@ -15,24 +15,28 @@ const fs = require('fs-extra');
  */
 function main() {
     const list = [
+        'build/certificate',
         'build/desktop',
         'build/logs',
         'build/mongo',
         'build/public/assets',
         'build/public/build',
         'build/public/locales',
+        'build/public/models',
         'build/public/temp',
         'build/public/favicon.ico',
         'build/public/index.html',
         'build/public/manifest.json',
         'build/public/sw.js',
         'build/public/view.html',
+        'build/toml',
         'build/config.toml',
         'build/logs.txt',
         'build/main.js',
         'build/package.json',
         'build/ShadowEditor',
-        'build/ShadowEditor.exe'
+        'build/ShadowEditor.exe',
+        'build/Server.exe'
     ];
 
     list.forEach(n => {
