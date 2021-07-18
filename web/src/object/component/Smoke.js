30089e94892203b144e15defaffb58c9b9e99fcb
tengge1 <930372551@qq.com>
2021-07-18 21:15:23 +0800

    fix smoke

--- web/src/object/component/Smoke.js
+++ web/src/object/component/Smoke.js
@@ -22,66 +22,68 @@ import fragmentShader from './shader/smoke_fragment.glsl';
  * @param {THREE.WebGLRenderer} renderer 渲染器
  * @param {Object} options 选项
  */
-function Smoke(camera, renderer, options = {}) {
-    var particleCount = options.particleCount || 32;
-    var size = options.size || 3;
-    var lifetime = options.lifetime || 10;
+class Smoke extends THREE.Points {
+    constructor(camera, renderer, options = {}) {
+        var particleCount = options.particleCount || 32;
+        var size = options.size || 3;
+        var lifetime = options.lifetime || 10;
 
-    // 几何体
-    var geometry = new THREE.BufferGeometry();
+        // 几何体
+        var geometry = new THREE.BufferGeometry();
 
-    var position = new Float32Array(particleCount * 3);
-    var shift = new Float32Array(particleCount);
+        var position = new Float32Array(particleCount * 3);
+        var shift = new Float32Array(particleCount);
 
-    for (var i = 0; i < particleCount; i++) {
-        position[i * 3 + 0] = THREE.Math.randFloat(-0.5, 0.5);
-        position[i * 3 + 1] = 2.4;
-        position[i * 3 + 3] = THREE.Math.randFloat(-0.5, 0.5);
-        shift[i] = Math.random() * 1;
+        for (var i = 0; i < particleCount; i++) {
+            position[i * 3 + 0] = THREE.Math.randFloat(-0.5, 0.5);
+            position[i * 3 + 1] = 2.4;
+            position[i * 3 + 3] = THREE.Math.randFloat(-0.5, 0.5);
+            shift[i] = Math.random() * 1;
+        }
+
+        geometry.setAttribute('position', new THREE.BufferAttribute(position, 3));
+        geometry.setAttribute('shift', new THREE.BufferAttribute(shift, 1));
+
+        // 材质
+        var texture = new THREE.TextureLoader().load('assets/textures/VolumetricFire/smoke.png');
+
+        var uniforms = {
+            time: {type: 'f', value: 0},
+            size: {type: 'f', value: size},
+            texture2: {type: 't', value: texture},
+            lifetime: {type: 'f', value: lifetime},
+            projection: {
+                type: 'f',
+                value: Math.abs(renderer.domElement.height / (2 * Math.tan(THREE.Math.degToRad(camera.fov))))
+            }
+        };
+
+        var material = new THREE.ShaderMaterial({
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader,
+            uniforms: uniforms,
+            blending: THREE.AdditiveBlending,
+            transparent: true,
+            depthWrite: false
+        });
+
+        super(geometry, material);
+
+        this.sortParticles = true;
+
+        this.name = _t('Smoke');
+
+        Object.assign(this.userData, {
+            type: 'Smoke',
+            particleCount: particleCount,
+            size: size,
+            lifetime: lifetime
+        });
     }
 
-    geometry.setAttribute('position', new THREE.BufferAttribute(position, 3));
-    geometry.setAttribute('shift', new THREE.BufferAttribute(shift, 1));
-
-    // 材质
-    var texture = new THREE.TextureLoader().load('assets/textures/VolumetricFire/smoke.png');
-
-    var uniforms = {
-        time: { type: 'f', value: 0 },
-        size: { type: 'f', value: size },
-        texture: { type: 't', value: texture },
-        lifetime: { type: 'f', value: lifetime },
-        projection: { type: 'f', value: Math.abs(renderer.domElement.height / (2 * Math.tan(THREE.Math.degToRad(camera.fov)))) }
-    };
-
-    var material = new THREE.ShaderMaterial({
-        vertexShader: vertexShader,
-        fragmentShader: fragmentShader,
-        uniforms: uniforms,
-        blending: THREE.AdditiveBlending,
-        transparent: true,
-        depthWrite: false
-    });
-
-    THREE.Points.call(this, geometry, material);
-
-    this.sortParticles = true;
-
-    this.name = _t('Smoke');
-
-    Object.assign(this.userData, {
-        type: 'Smoke',
-        particleCount: particleCount,
-        size: size,
-        lifetime: lifetime
-    });
+    update(elapsed) {
+        this.material.uniforms.time.value = elapsed;
+    }
 }
-
-Smoke.prototype = Object.create(THREE.Points.prototype);
-Smoke.prototype.constructor = Smoke;
-
-Smoke.prototype.update = function (elapsed) {
-    this.material.uniforms.time.value = elapsed;
-};
 
 export default Smoke;
--- web/src/object/component/shader/smoke_fragment.glsl
+++ web/src/object/component/shader/smoke_fragment.glsl
@@ -1,9 +1,9 @@
-uniform sampler2D texture;
+uniform sampler2D texture2;
 varying float progress;
 
 void main() {
 
   vec3 color = vec3( 1. );
-  gl_FragColor = texture2D( texture, gl_PointCoord ) * vec4( color, .3 * ( 1. - progress ) );
+  gl_FragColor = texture2D( texture2, gl_PointCoord ) * vec4( color, .3 * ( 1. - progress ) );
 
 }
